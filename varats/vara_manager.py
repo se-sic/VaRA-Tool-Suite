d20652c86d072fcfc444b6304488ec4cf3ca220f
Florian Sattler <vuld3r@gmail.com>
2018-05-09 22:14:05 +0200

    first work on the gui to init VaRA

---
+++ varats/vara_manager.py
@@ -0,0 +1,21 @@
+"""
+This module handels the status of VaRA.
+Setting up the tooling, keeping it up to date,
+and providing necessary information.
+"""
+
+def download_vara():
+    """
+    Downloads VaRA an all other necessary repos from github.
+    """
+    pass
+
+def initialize_vara():
+    pass
+
+# TODO: needs enum
+def build_vara(build_type):
+    """
+    Builds VaRA
+    """
+    pass
