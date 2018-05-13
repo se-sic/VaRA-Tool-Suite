377026c4ccecdd73abd1f4f6af4e544d6bf4974b
Florian Sattler <vuld3r@gmail.com>
2018-05-13 15:12:06 +0200

    refactored MainWindow to be inheritance based

--- varats/gui/main_window.py
+++ varats/gui/main_window.py
@@ -10,45 +10,38 @@ from varats.gui.buildsetup_window import BuildSetup
 from PyQt5.QtWidgets import QMainWindow
 
 
-class MainWindow(object):
+class MainWindow(QMainWindow, Ui_MainWindow):
     """
     Manages the GUI state and manages the different active views.
     """
 
     def __init__(self):
-        self.ui_mw = Ui_MainWindow()
-        self.main_window = QMainWindow()
+        super(self.__class__, self).__init__()
         self.views = []
         self.bwg = None
 
-        self.setup_ui()
-
-    def setup_ui(self):
-        """
-        Setup main GUI
-        """
-        self.ui_mw.setupUi(self.main_window)
-        self.ui_mw.actionExampleView.triggered.connect(self._spawn_exampleview)
-        self.ui_mw.actionCR_BarView.triggered.connect(self._spawn_cr_bar_view)
+        self.setupUi(self)
+        self.actionExampleView.triggered.connect(self._spawn_exampleview)
+        self.actionCR_BarView.triggered.connect(self._spawn_cr_bar_view)
 
         # Signals for menubar
-        self.ui_mw.actionVaRA_Setup.triggered.connect(self._spawn_vara_setup)
+        self.actionVaRA_Setup.triggered.connect(self._spawn_vara_setup)
 
-        self.ui_mw.tabWidget.tabCloseRequested.connect(self.__remove_tab)
+        self.tabWidget.tabCloseRequested.connect(self.__remove_tab)
 
-        self.main_window.show()
+        self.show()
 
     def _spawn_exampleview(self):
         new_tab = ExampleView()
         self.views.append(new_tab)
 
-        self.ui_mw.tabWidget.addTab(new_tab, "ExampleView")
+        self.tabWidget.addTab(new_tab, "ExampleView")
 
     def _spawn_cr_bar_view(self):
         new_tab = CRBarView()
         self.views.append(new_tab)
 
-        self.ui_mw.tabWidget.addTab(new_tab, "CR-BarView")
+        self.tabWidget.addTab(new_tab, "CR-BarView")
 
     def _spawn_vara_setup(self):
         """
@@ -58,8 +51,8 @@ class MainWindow(object):
         self.bwg.show()
 
     def __remove_tab(self, index):
-        tab = self.ui_mw.tabWidget.widget(index)
+        tab = self.tabWidget.widget(index)
         if tab is not None:
             self.views.remove(tab)
 
-            self.ui_mw.tabWidget.removeTab(index)
+            self.tabWidget.removeTab(index)
