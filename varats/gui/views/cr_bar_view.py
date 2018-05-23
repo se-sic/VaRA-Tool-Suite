8e5591b51b68a134f204eff39c051af3f491619f
Florian Sattler <vuld3r@gmail.com>
2018-05-23 12:01:56 +0200

    Added slider impl
    CR Bar view now supports multiple files.

--- varats/gui/views/cr_bar_view.py
+++ varats/gui/views/cr_bar_view.py
@@ -19,7 +19,9 @@ class CRBarView(QWidget, Ui_Form):
     def __init__(self):
         super(CRBarView, self).__init__()
 
-        self.commit_report = None
+        self.commit_reports = []
+        self.current_report = None
+        self.loading_files = 0
 
         self.setupUi(self)
         self.plot_up.set_cf_plot(True)
@@ -31,49 +33,84 @@ class CRBarView(QWidget, Ui_Form):
         self.check_cf_graph.stateChanged.connect(self.enable_cf_plot)
         self.check_df_graph.stateChanged.connect(self.enable_df_plot)
 
+        self.fileSlider.sliderReleased.connect(self._slider_moved)
+
     def load_commit_report(self):
         """
         Load new CommitReport from file_path.
         """
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        file_path, _ = QFileDialog.getOpenFileName(
+        file_paths, _ = QFileDialog.getOpenFileNames(
             self, "Load CommitReport file", "",
             "Yaml Files (*.yaml *.yml);;All Files (*)", options=options)
 
-        if not isfile(file_path):
-            err = QMessageBox()
-            err.setIcon(QMessageBox.Warning)
-            err.setWindowTitle("File not found.")
-            err.setText("Could not find selected file.")
-            err.setStandardButtons(QMessageBox.Ok)
-            err.exec_()
-            return
+        for file_path in file_paths:
+            if not isfile(file_path):
+                err = QMessageBox()
+                err.setIcon(QMessageBox.Warning)
+                err.setWindowTitle("File not found.")
+                err.setText("Could not find selected file.")
+                err.setStandardButtons(QMessageBox.Ok)
+                err.exec_()
+                return
 
-        if not (file_path.endswith(".yaml")
-                or file_path.endswith(".yml")):
-            err = QMessageBox()
-            err.setIcon(QMessageBox.Warning)
-            err.setWindowTitle("Wrong File ending.")
-            err.setText("File seems not to be a yaml file.")
-            err.setStandardButtons(QMessageBox.Ok)
-            err.exec_()
-            return
+            if not (file_path.endswith(".yaml")
+                    or file_path.endswith(".yml")):
+                err = QMessageBox()
+                err.setIcon(QMessageBox.Warning)
+                err.setWindowTitle("Wrong File ending.")
+                err.setText("File seems not to be a yaml file.")
+                err.setStandardButtons(QMessageBox.Ok)
+                err.exec_()
+                return
 
-        if self.commit_report is None or \
-                (self.commit_report is not None
-                 and self.commit_report.path != file_path):
-            self.statusLabel.setText("Loading file...")
+        for file_path in file_paths:
+            skip = False
+            for current_report in self.commit_reports:
+                # skip files that were loaded bevor
+                if current_report.path == file_path:
+                    skip = True
+                    continue
+            if skip:
+                continue
+            self.loading_files += 1
+            self.statusLabel.setText("Loading files... " +
+                                     str(self.loading_files))
             VDM.load_data_class(file_path, CommitReport,
                                 self._set_new_commit_report)
 
     def _set_new_commit_report(self, commit_report):
-        self.commit_report = commit_report
+        self.loading_files -= 1
+
+        if commit_report not in self.commit_reports:
+            self.commit_reports.append(commit_report)
+            self._adjust_slider()
+
+        if self.loading_files is 0:
+            self.statusLabel.setText("")
+        else:
+            self.statusLabel.setText("Loading files... " +
+                                     str(self.loading_files))
+
+    def _draw_plots(self):
+        if self.current_report is None:
+            return
+
         if self.check_cf_graph.isChecked():
-            self.plot_up.update_plot(self.commit_report)
+            self.plot_up.update_plot(self.current_report)
         if self.check_df_graph.isChecked():
-            self.plot_down.update_plot(self.commit_report)
-        self.statusLabel.setText("")
+            self.plot_down.update_plot(self.current_report)
+
+    def _adjust_slider(self):
+        self.fileSlider.setMaximum(len(self.commit_reports) - 1)
+        self._slider_moved()
+
+    def _slider_moved(self):
+        if len(self.commit_reports) >= 1:
+            self.current_report = self\
+                .commit_reports[self.fileSlider.value()]
+            self._draw_plots()
 
     def enable_cf_plot(self, state: int):
         """
@@ -82,8 +119,8 @@ class CRBarView(QWidget, Ui_Form):
         if state is 0:  # turned off
             self.plot_up.hide()
         else:
-            if self.commit_report is not None:
-                self.plot_up.update_plot(self.commit_report)
+            if self.current_report is not None:
+                self.plot_up.update_plot(self.current_report)
             self.plot_up.show()
 
     def enable_df_plot(self, state: int):
@@ -93,6 +130,6 @@ class CRBarView(QWidget, Ui_Form):
         if state is 0:  # turned off
             self.plot_down.hide()
         else:
-            if self.commit_report is not None:
-                self.plot_down.update_plot(self.commit_report)
+            if self.current_report is not None:
+                self.plot_down.update_plot(self.current_report)
             self.plot_down.show()
