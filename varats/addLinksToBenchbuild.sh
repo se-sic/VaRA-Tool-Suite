3fcf22f3ad01b63e280de8feb668cb6cb40f2b1c
Florian Sattler <vuld3r@gmail.com>
2018-06-18 16:32:04 +0200

    Changed success message

--- varats/addLinksToBenchbuild.sh
+++ varats/addLinksToBenchbuild.sh
@@ -5,6 +5,6 @@ LIB_DIRECTORY_PROJ=$LIB_DIRECTORY_BENCH"/projects"
 LIB_DIRECTORY_EXP=$LIB_DIRECTORY_BENCH"/experiments"
 CUR_FOLDER_PROJ=$(pwd)"/varats/vara-projects"
 CUR_FOLDER_EXP=$(pwd)"/varats/vara-experiments"
-echo "Everything worked fine. !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 ln -s $CUR_FOLDER_PROJ $LIB_DIRECTORY_PROJ
 ln -s $CUR_FOLDER_EXP $LIB_DIRECTORY_EXP
+echo "Linked VaRA experiments and projects into BenchBuild."
