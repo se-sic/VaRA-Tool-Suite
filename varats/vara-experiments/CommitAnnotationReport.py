f0d73fe737c1a2dec98bf69b1164a27b5239da0f
Tobias Hellmich <hellmichtobias@icloud.com>
2018-07-07 08:29:55 +0200

    Tidy up CommitAnnotationReport
    - Reorder steps
    - Use the already existing prepare script

--- varats/vara-experiments/CommitAnnotationReport.py
+++ varats/vara-experiments/CommitAnnotationReport.py
@@ -2,16 +2,9 @@ from benchbuild.experiment import Experiment
 from benchbuild import extensions as ext
 from benchbuild.extensions import RunWithTime, RuntimeExtension
 from benchbuild.settings import CFG
-from benchbuild.utils.downloader import Git
-from benchbuild.utils.actions import Clean, Step, Configure, Build
-from benchbuild.utils.cmd import make
-from benchbuild.utils.run import run
+from benchbuild.utils.actions import Step
 from plumbum import local
-from plumbum.path.utils import delete
-import logging
 from os import path
-
-LOG = logging.getLogger(__name__)
 
 EnvVars = {
     "LLVM_COMPILER": "clang",
@@ -20,7 +13,6 @@ EnvVars = {
     "CC": "wllvm",
     "CXX": "wllvm++",
     "WLLVM_OUTPUT_FILE": path.join(CFG["tmp_dir"].value(),"wllvm.log"),
-    "LLVM_COMPILER_PATH": "/home/hellmich/git/llvm/build/dev/bin",
     "LLVM_CC_NAME": "clang",
     "LLVM_CXX_NAME": "clang++"
 }
@@ -46,29 +38,18 @@ class CommitAnnotationReport(Experiment):
         project.runtime_extension = ext.RunWithTime(RuntimeExtension(project,
                 self, config={'jobs': int(CFG["jobs"].value())}))
 
+        project.EnvVars = EnvVars
         project_src = path.join(project.builddir, project.src_dir)
 
         def evaluate_preparation():
-            SRC_URL = "https://github.com/se-passau/VaRA.git"
-            Git(SRC_URL, "vara")
-            scripts_src = CFG["benchbuild_prefix"].value() + "/vara/tools/marker-region"
-
-            if not path.exists(scripts_src):
-                LOG.error("Following path does not exist", scripts_src)
-                return ""
+            with local.cwd("/"):
+                scripts_src = path.join(local.path(str(CFG["env"]["path"].value()[0])).up(3), "tools/VaRA/tools/marker-region")
 
             prepare = local[scripts_src + "/prepare.sh"]
-            clang_bins = EnvVars["LLVM_COMPILER_PATH"]
-
-            if not path.exists(clang_bins):
-                LOG.error("Following path does not exist", clang_bins)
-                return ""
-            
-            project.EnvVars = EnvVars
             project.src_dir = path.join(project.src_dir, "out")
 
             with local.cwd(project_src):
-                prepare("-c", clang_bins, "-t", scripts_src)
+                prepare("-c", CFG["env"]["path"].value(), "-t", scripts_src)
 
         def evaluate_extraction():
             extract = local["extract-bc"]
@@ -77,21 +58,14 @@ class CommitAnnotationReport(Experiment):
                     extract(project.name)
 
         def evaluate_analysis():
-            opt = local[path.join(EnvVars["LLVM_COMPILER_PATH"], "opt")]
-            run_cmd = opt["-vara-CFR", "-view-TFA", "-yaml-out-file=source.yaml",
-                path.join(project_src, "out", project.name + ".bc")]
+            opt = local[path.join(str(CFG["env"]["path"].value()[0]), "opt")]
+            yamlAdd = "-yaml-out-file=" + project.name + ".yaml"
+            run_cmd = opt["-vara-CFR", yamlAdd, path.join(project_src, "out", project.name + ".bc")]
             with local.cwd(CFG["tmp_dir"].value()):
                 run_cmd()
 
         actns = self.default_runtime_actions(project)
-        for actn in actns:
-            if "CLEAN" in actn.NAME:
-                actns.remove(actn)
-
-        actns.append(Prepare(self, evaluate_preparation))
-        actns.append(Configure(project))
-        actns.append(Build(project))
-        actns.append(Extract(self, evaluate_extraction))
-        actns.append(Analyse(self, evaluate_analysis))
-        actns.append(Clean(project))
+        actns.insert(len(actns)-4, Prepare(self, evaluate_preparation))
+        actns.insert(len(actns)-1, Extract(self, evaluate_extraction))
+        actns.insert(len(actns)-1, Analyse(self, evaluate_analysis))
         return actns
