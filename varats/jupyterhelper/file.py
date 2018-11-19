1c1c23df2308e38d2d7b498ba03c2cdd00579e01
Florian Sattler <vuld3r@gmail.com>
2018-11-19 14:00:56 +0100

    Initial work to allow jupyter to use vara (#20)
    resolves se-passau/VaRA#301

---
+++ varats/jupyterhelper/file.py
@@ -0,0 +1,17 @@
+"""
+This module provides different jupyther helpers to allow easier interaction
+with varas file handling APIs.
+"""
+
+from varats.data.data_manager import VDM
+from varats.data.commit_report import CommitReport
+
+
+def load_commit_report(file_path: str) -> CommitReport:
+    """
+    Load a CommitReport from a file.
+
+    Attributes:
+        file_path (str): Full path to the file
+    """
+    return VDM.load_data_class_sync(file_path, CommitReport)
