fc14e773585710879b327e5a6303c8ce686315e9
Florian Sattler <vuld3r@gmail.com>
2018-11-30 16:13:17 +0100

    Automatically generate working BenchBuild config (#25)

--- varats/gui/main_window.py
+++ varats/gui/main_window.py
@@ -2,9 +2,9 @@
 VaRA-TS MainWindow
 """
 
-from os import path, getcwd
+from os import path
 
-from varats.settings import CFG, save_config
+from varats.settings import CFG, save_config, generate_benchbuild_config
 from varats.gui.ui_MainWindow import Ui_MainWindow
 from varats.gui.views.example_view import ExampleView
 from varats.gui.views.cr_bar_view import CRBarView
@@ -18,7 +18,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     """
 
     def __init__(self):
-        super(self.__class__, self).__init__()
+        super(MainWindow, self).__init__()
         self.views = []
         self.bwg = None
 
@@ -71,31 +71,9 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                                                   + "/benchbuild"
         create_missing_folders()
 
-        # Local to lazy initialize BenchBuild config
-        from benchbuild.settings import CFG as BB_CFG
-        projects_conf = BB_CFG["plugins"]["projects"]
-        projects_conf.value[:] = []
-        # If we want later to use default BB projects
-        # projects_conf.value[:] = [ x for x in projects_conf.value
-        #                           if not x.endswith('gzip')]
-        projects_conf.value[:] += ['varats.projects.c_projects.gzip']
-
-        BB_CFG["env"] = {
-            # TODO (sattlerf): add path to vara install here
-            "path": "",
-        }
-
-        def replace_bb_cwd_path(cfg_varname, cfg_node=BB_CFG):
-            cfg_node[cfg_varname] = str(CFG["benchbuild_root"]) +\
-                str(cfg_node[cfg_varname])[len(getcwd()):]
-
-        replace_bb_cwd_path("build_dir")
-        replace_bb_cwd_path("tmp_dir")
-        replace_bb_cwd_path("test_dir")
-        replace_bb_cwd_path("node_dir", BB_CFG["slurm"])
-
-        bb_config_path = str(CFG["benchbuild_root"]) + "/.benchbuild.yml"
-        BB_CFG.store(bb_config_path)
+        generate_benchbuild_config(CFG,
+                                   str(CFG["benchbuild_root"]) +
+                                   "/.benchbuild.yml")
 
     def __remove_tab(self, index):
         tab = self.tabWidget.widget(index)
