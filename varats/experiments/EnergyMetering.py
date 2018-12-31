5e6cebff5a50fd9c4330a931c6267bf7f3d4a699
Tobias Hellmich <tobias.hellmich@tele2.de>
2018-12-31 11:55:55 +0100

    Improve experiment EnergyMetering

--- varats/experiments/EnergyMetering.py
+++ varats/experiments/EnergyMetering.py
@@ -4,7 +4,7 @@ import benchbuild.utils.actions as actions
 from benchbuild.experiment import Experiment
 from benchbuild.extensions import compiler, run, time
 from benchbuild.settings import CFG
-from benchbuild.utils.cmd import opt
+from benchbuild.utils.download import Git
 from plumbum import local
 
 CFG["em"] = {
@@ -18,25 +18,36 @@ CFG["em"] = {
     }
 }
 
-class Energy_Metering(actions.Step):
+class Download_EM(actions.Step):
     NAME = "Energy_Metering"
     DESCRIPTION = "Measures the energy."
 
+class Analyse_EM(actions.Step):
+    NAME = "Energy_Metering"
+    DESCRIPTION = "Measures the energy."
 
 class EnergyMetering(Experiment):
-    NAME = "GitBlameAnnotationReport"
+    NAME = "EnergyMetering"
 
     def actions_for_project(self, project):
-        project.runtime_extension = run.RuntimeExtension(project, self) << time.RunWithTime()
+        project.runtime_extension = run.RuntimeExtension(project, self) \
+                                    << time.RunWithTime()
         project.compiler_extension = compiler.RunCompiler(project, self)
 
-        def evaluate_energy_metering():
-            input = local.path(project.builddir)
+        def download_em():
+            with local.cwd(project.builddir):
+                Git("https://github.com/se-passau/EnergyMetering.git",
+                    self.NAME)
+
+        def evaluate_em():
+            input = local.path(project.builddir) / project.SRC_FILE
             output_std = local.path(str(CFG["em"]["results"].value))
             output_rep = local.path(str(CFG["em"]["results_rep"].value))
             config = input / "config"
-            em_initial = local["em.sh"]
-            em_output = local[output / "em.sh"]
+
+            em_initial = local[local.path(project.builddir) / self.NAME /
+                               "em.sh"]
+            em_output = local[output_std / "em.sh"]
 
             prepare = em_initial["prepare", "-d", output_std, "-c", config]
             prepare()
@@ -44,17 +55,20 @@ class EnergyMetering(Experiment):
             start = em_output["start"]
             start()
 
-            if os.path.exists(local.path(input / "repeat.map"))
+            if os.path.exists(local.path(input / "repeat.map")):
                 repeat = em_initial["-d", output_rep, "-r", output_std]
                 repeat()
                 start_rep = em_output["start"]
                 start_rep()
 
-            em_output["process-summary", "-i", input / "summary-info", "-s", "summary.txt", "-D", "deviations.txt"]
-            em_output["process-summary", "-i", input / "summary-info", "-s", "measurements.csv", "-D", "deviations.csv", "-C"]
+            em_output["process-summary", "-i", input / "summary-info", "-s",
+                      "summary.txt", "-D", "deviations.txt"]
+            em_output["process-summary", "-i", input / "summary-info", "-s",
+                      "measurements.csv", "-D", "deviations.csv", "-C"]
 
         analysis_actions = []
         analysis_actions.append(actions.Compile(project))
-        analysis_actions.append(Energy_Metering(self, evaluate_energy_metering))
+        analysis_actions.append(Download_EM(self, download_em))
+        analysis_actions.append(Analyse_EM(self, evaluate_em))
         analysis_actions.append(actions.Clean(project))
         return analysis_actions
