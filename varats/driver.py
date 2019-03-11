6c96bc40fa96985dc0968bdeed4ddc87b26274da
Florian Sattler <vuld3r@gmail.com>
2019-03-11 13:52:00 +0100

    Graph plot updates

--- varats/driver.py
+++ varats/driver.py
@@ -19,6 +19,7 @@ from varats.gui.main_window import MainWindow
 from varats.gui.buildsetup_window import BuildSetup
 from varats.vara_manager import setup_vara, BuildType
 from varats.tools.commit_map import generate_commit_map
+from varats.data.commit_report import CommitMap
 
 from PyQt5.QtWidgets import QApplication, QMessageBox
 
@@ -159,27 +160,48 @@ def gen_fosd_graph():
 
     from pathlib import Path
 
+    commit_map = CommitMap("/home/vulder/vara/c_map")
+
     reports = []
     result_dir = Path("/home/vulder/vara/fosd_results/")
     for file_path in result_dir.iterdir():
         if file_path.stem.startswith("gzip"):
             print("Loading file: ", file_path)
             reports.append(load_commit_report(file_path))
+
+    def sorter(report):
+        return commit_map.short_time_id(report.head_commit)
+
+    reports = sorted(reports, key=sorter)
+
     # Sort with commit map
     commits = []
-    interactions = []
+    df_interactions = []
+    cf_interactions = []
     head_interactions = []
 
     for report in reports:
             commits.append(report.head_commit)
-            interactions.append(report.number_of_df_interactions())
+            df_interactions.append(report.number_of_df_interactions())
+            cf_interactions.append(report.number_of_cf_interactions())
             hi = report.number_of_head_df_interactions()
             head_interactions.append(hi[0] + hi[1])
 
-    df=pd.DataFrame({'x': commits, 'Interactions': interactions,
+    df=pd.DataFrame({'x': commits, 'DFInteractions': df_interactions,
+                     'CFInteractions': cf_interactions,
                      "HEAD Interactions": head_interactions})
-    plt.plot('x', 'Interactions', data=df, color='red')
-    plt.plot('x', 'HEAD Interactions', data=df, color='green')
+
+    ax = plt.subplot()
+
+    for label in (ax.get_xticklabels() + ax.get_yticklabels()):
+            # label.set_fontname('Arial')
+            label.set_fontsize(14)
+
+    plt.plot('x', 'DFInteractions', data=df, color='red')
+    plt.plot('x', 'CFInteractions', data=df, color='blue')
+    plt.xlabel("Revisions", **{'size': '14'})
+    plt.ylabel("Interactions", **{'size': '14'})
+    # plt.plot('x', 'HEAD Interactions', data=df, color='green')
     plt.show()
 
 
