eb606e6d035ac793219d3a91bac52b62a64e357e
Florian Sattler <vuld3r@gmail.com>
2019-05-27 00:56:05 +0200

    Fixed issue

--- run_extra_tests.sh
+++ run_extra_tests.sh
@@ -11,8 +11,8 @@ fi
 COVERAGE='coverage run -p'
 
 # Smoke tests
-$COVERAGE `which vara-buildsetup` -c
-$COVERAGE `which vara-cs` gen paper_configs/test/ half_norm gzip/
-$COVERAGE `which vara-cs` ext paper_configs/test/gzip_0.case_study simple_add gzip/ --extra-revs 0dd8313ea7bce --merge-stage 3
-$COVERAGE `which vara-cs` ext paper_configs/test/gzip_0.case_study distrib_add gzip/ --distribution uniform --num-rev 5
-$COVERAGE `which vara-cs` status
+$COVERAGE $(which vara-buildsetup) -c
+$COVERAGE $(which vara-cs) gen paper_configs/test/ half_norm gzip/
+$COVERAGE $(which vara-cs) ext paper_configs/test/gzip_0.case_study simple_add gzip/ --extra-revs 0dd8313ea7bce --merge-stage 3
+$COVERAGE $(which vara-cs) ext paper_configs/test/gzip_0.case_study distrib_add gzip/ --distribution uniform --num-rev 5
+$COVERAGE $(which vara-cs) status
