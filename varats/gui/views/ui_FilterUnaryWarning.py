46b9de5db6f8efec28d020de03e038297930141e
Julian Breiteneicher <git-contact@brebeluk.de>
2019-07-04 23:14:03 +0200

    Incorporate feedback (Change warning text)

--- varats/gui/views/ui_FilterUnaryWarning.py
+++ varats/gui/views/ui_FilterUnaryWarning.py
@@ -11,11 +11,12 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_FilterUnaryWarning(object):
     def setupUi(self, FilterUnaryWarning):
         FilterUnaryWarning.setObjectName("FilterUnaryWarning")
-        FilterUnaryWarning.resize(340, 121)
+        FilterUnaryWarning.resize(716, 121)
         self.verticalLayout = QtWidgets.QVBoxLayout(FilterUnaryWarning)
         self.verticalLayout.setObjectName("verticalLayout")
         self.uiWarningLabel = QtWidgets.QLabel(FilterUnaryWarning)
         self.uiWarningLabel.setEnabled(True)
+        self.uiWarningLabel.setWordWrap(True)
         self.uiWarningLabel.setObjectName("uiWarningLabel")
         self.verticalLayout.addWidget(self.uiWarningLabel)
 
@@ -25,7 +26,7 @@ class Ui_FilterUnaryWarning(object):
     def retranslateUi(self, FilterUnaryWarning):
         _translate = QtCore.QCoreApplication.translate
         FilterUnaryWarning.setWindowTitle(_translate("FilterUnaryWarning", "Form"))
-        self.uiWarningLabel.setText(_translate("FilterUnaryWarning", "<html><head/><body><p><span style=\" font-weight:600;\">This filter applies to the source and the target region of an interaction</span></p></body></html>"))
+        self.uiWarningLabel.setText(_translate("FilterUnaryWarning", "<html><head/><body><p><span style=\" font-weight:600;\">Currently this filter applies to source and target, therefore, only interactions where both satisfy the filter are included.</span></p></body></html>"))
 
 
 if __name__ == "__main__":
