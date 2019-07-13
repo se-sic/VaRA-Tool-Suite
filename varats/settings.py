8fe59f8dc64bbc99fdea35017c099b4baf697baa
Simon <simon.labrenz@outlook.de>
2019-07-13 15:14:15 +0200

    Move taint cpp files to own project.

--- varats/settings.py
+++ varats/settings.py
@@ -183,6 +183,7 @@ def generate_benchbuild_config(vara_cfg: s.Configuration,
     ]
     projects_conf.value[:] += ['varats.projects.cpp_projects.doxygen']
     projects_conf.value[:] += ['varats.projects.test_projects.basic_tests']
+    projects_conf.value[:] += ['varats.projects.test_projects.taint_tests']
 
     # Experiments for VaRA
     projects_conf = BB_CFG["plugins"]["experiments"]
@@ -191,7 +192,7 @@ def generate_benchbuild_config(vara_cfg: s.Configuration,
         'varats.experiments.git_blame_annotation_report',
         'varats.experiments.marker_tester',
         'varats.experiments.just_compile',
-        'varats.experiments.taint_propagation'
+        #'varats.experiments.taint_propagation'
     ]
 
     # Slurm Cluster Configuration
