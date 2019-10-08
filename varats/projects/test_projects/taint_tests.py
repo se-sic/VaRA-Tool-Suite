cba77f010b07ab72ee0610f6a3334e0188841f39
Simon <simon.labrenz@outlook.de>
2019-10-08 17:13:26 +0200

    Add the taint test project containing getenv calls,

---
+++ varats/projects/test_projects/taint_tests.py
@@ -0,0 +1,61 @@
+"""
+Compile a collection of representing examples for the taint analysis.
+"""
+from plumbum import local
+
+from benchbuild.utils.run import run
+from benchbuild.utils.compiler import cxx
+from benchbuild.utils.download import with_git
+import benchbuild.project as prj
+
+
+@with_git(
+    "https://github.com/se-passau/vara-perf-tests.git",
+    limit=1,
+    refspec="origin/f-taintTests")
+class TaintTests(prj.Project):  # type: ignore
+    """
+    Taint tests:
+        Different small test files for taint propagation
+    """
+
+    NAME = 'taint-tests'
+    GROUP = 'test_projects'
+    DOMAIN = 'testing'
+    VERSION = 'HEAD'
+
+    SRC_FILE = "vara-perf-tests"
+
+    CPP_FILES = [
+        "arrayTaintPropagation.cpp",
+        "byValueArgPassing.cpp",
+        "coercedArgPassing.cpp",
+        "coercedReturnValuePassing.cpp",
+        "controlFlowDependency.cpp",
+        "operatorTaintPropagation.cpp",
+        "pointerTaintPropagation1.cpp",
+        "pointerTaintPropagation2.cpp",
+        "pointerTaintPropagation3.cpp",
+        "regularArgPassing.cpp",
+        "regularReturnValuePassing.cpp",
+        "returnValueMapping.cpp",
+        "switchFallthrough.cpp",
+        "unionTaintPropagation.cpp",
+        "variableLengthArgForwarding.cpp",
+        "variableLengthArgPassing.cpp"
+    ]
+
+    BIN_NAMES = [file_name.replace('.cpp', '')
+                 for file_name in CPP_FILES]
+
+    def run_tests(self, runner: run) -> None:
+        pass
+
+    def compile(self) -> None:
+        self.download()
+
+        clang = cxx(self)
+        with local.cwd(self.SRC_FILE):
+            for file in self.CPP_FILES:
+                run(clang["{name}/{file}".format(name=self.NAME, file=file),
+                          "-o", file.replace('.cpp', '')])
