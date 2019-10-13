3f824953c885e76fe5d45bbe016677fb4822dcc9
Simon Labrenz <17525831+labrenz@users.noreply.github.com>
2019-10-13 21:29:47 +0200

    Adds phasar env tracing experiment (#136)
    Add a new experiment that executes phasar's environment variable tracing analysis.

---
+++ varats/data/reports/env_trace_report.py
@@ -0,0 +1,27 @@
+"""
+Report that simply takes the output of an phasar analysis.
+"""
+
+from varats.data.report import BaseReport, FileStatusExtension
+
+
+class EnvTraceReport(BaseReport):
+    """
+    The phasar report produces json files with the output for each file.
+    """
+
+    SHORTHAND = "ENV-TRACE"
+    FILETYPE = "json"
+
+    @staticmethod
+    def get_file_name(project_name: str, binary_name: str,
+                      project_version: str, project_uuid: str,
+                      extension_type: FileStatusExtension,
+                      file_ext: str = ".json") -> str:
+        """
+        Generates a filename for a dataflow analysis with json file type.
+        """
+        return BaseReport.get_file_name(EnvTraceReport.SHORTHAND, project_name,
+                                        binary_name, project_version,
+                                        project_uuid, extension_type,
+                                        file_ext)
