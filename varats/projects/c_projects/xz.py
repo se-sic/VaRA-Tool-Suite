ac977e329e92f65d5be4522f4d3753c49b936fb4
Sebastian Böhm <boehmseb@cs.uni-saarland.de>
2019-11-05 16:12:14 +0100

    Adds revision range based compilation to case study xz

--- varats/projects/c_projects/xz.py
+++ varats/projects/c_projects/xz.py
@@ -3,12 +3,13 @@ Project file for xz.
 """
 from benchbuild.project import Project
 from benchbuild.settings import CFG
-from benchbuild.utils.cmd import make, autoreconf, cp
+from benchbuild.utils.cmd import make, autoreconf, cp, git
 from benchbuild.utils.compiler import cc
 from benchbuild.utils.download import with_git
 from benchbuild.utils.run import run
 
 from plumbum import local
+from varats.utils.project_util import get_all_revisions_between
 
 from varats.paper.paper_config import project_filter_generator
 
@@ -36,13 +37,24 @@ class Xz(Project):  # type: ignore
     def compile(self) -> None:
         self.download()
 
+        # commit 46133fb47d6da1f0dec27ae23db1d633bc72e9e3 introduced
+        # cmake as build system
+        with local.cwd(self.SRC_FILE):
+            version_id = git("rev-parse", "HEAD").strip()
+            cmake_revisions = get_all_revisions_between(
+                "5d018dc03549c1ee4958364712fb0c94e1bf2741", "fda4724d8114fccfa31c1839c15479f350c2fb4c")
+
         self.cflags += ["-fPIC"]
 
         clang = cc(self)
         with local.cwd(self.SRC_FILE):
             with local.env(CC=str(clang)):
                 run(autoreconf["--install"])
-                run(local["./configure"])
+
+                if version_id in cmake_revisions:
+                    run(local["./configure", "--enable-dynamic=yes"])
+                else:
+                    run(local["./configure"])
             run(make["-j", int(CFG["jobs"])])
 
             # Copy hidden binary file to root for extraction
