ecb75a884aab6882ab983934c4af68f33fe99520
Florian Sattler <vuld3r@gmail.com>
2019-11-14 15:51:13 +0100

    Fixes default

--- varats/settings.py
+++ varats/settings.py
@@ -89,7 +89,7 @@ CFG['experiment'] = {
         "This option is replaced by file_status_blacklist = [Success]"
     },
     "file_status_blacklist": {
-        "default": [Success, Blocked],
+        "default": ['Success', 'Blocked'],
         "desc":
         "Do not include revision with these file status for benchbuild "
         "processing"
