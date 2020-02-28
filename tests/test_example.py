d6d0b086c9bd12a65049fd0e4292d417f9afc2bd
Florian Sattler <vuld3r@gmail.com>
2020-02-28 09:50:33 +0100

    Small fixes

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,23 +1,42 @@
+"""
+Test example file that can be used as orientation.
+"""
 import unittest
 import mock
 
+
 class ExampleTestCase(unittest.TestCase):
+    """
+    An example for how to create a test case.
+    """
 
     def test_foo(self):
-        pass
+        """
+        How create a test
+        """
 
 
 def function_under_test(file_path):
-    with open(file_path) as f:
-        txt = f.read()
+    """
+    Example function with IO to tests.
+    """
+    with open(file_path) as opened_filed:
+        txt = opened_filed.read()
         print(txt)
         return txt
 
 
 class MyTestCase(unittest.TestCase):
+    """
+    An example for how to create a test case that needs mocking because of file
+    IO.
+    """
 
     @mock.patch("builtins.open", create=True)
     def test_function_under_test(self, mock_open):
+        """
+        Test function with mocked method for file access.
+        """
         mock_open.side_effect = [
             mock.mock_open(read_data="FileContents").return_value,
         ]
--- tests/test_vara_manager.py
+++ tests/test_vara_manager.py
@@ -1,3 +1,7 @@
+"""
+Test VaRA llvm project enums that abstract over the different llvm repositories
+needed to build VaRA.
+"""
 import unittest
 from pathlib import Path
 
@@ -11,6 +15,9 @@ class TestLLVMProjects(unittest.TestCase):
     """
 
     def test_get_vara_values(self):
+        """
+        Tests if we can get the correct values from the vara project enums.
+        """
         vara_project = LLVMProjects.vara
 
         self.assertEqual(vara_project.project_name, "VaRA")
@@ -19,6 +26,9 @@ class TestLLVMProjects(unittest.TestCase):
         self.assertEqual(vara_project.path, Path("tools/VaRA"))
 
     def test_vara_projects_iter(self):
+        """
+        Tests if the VaRA project iterator works.
+        """
         vara_projects_iter = iter(VaRAProjectsIter())
         self.assertEqual(next(vara_projects_iter), LLVMProjects.llvm)
         self.assertEqual(next(vara_projects_iter), LLVMProjects.clang)
@@ -26,11 +36,14 @@ class TestLLVMProjects(unittest.TestCase):
         self.assertRaises(StopIteration, next, vara_projects_iter)
 
     def test_vara_extra_projects_iter(self):
+        """
+        Tests if the VaRA only extra projects iterator works.
+        """
         vara_extra_projects_iter = iter(VaRAExtraProjectsIter())
-        self.assertEqual(
-            next(vara_extra_projects_iter), LLVMProjects.clang_extra)
-        self.assertEqual(
-            next(vara_extra_projects_iter), LLVMProjects.compiler_rt)
+        self.assertEqual(next(vara_extra_projects_iter),
+                         LLVMProjects.clang_extra)
+        self.assertEqual(next(vara_extra_projects_iter),
+                         LLVMProjects.compiler_rt)
         self.assertEqual(next(vara_extra_projects_iter), LLVMProjects.lld)
         self.assertEqual(next(vara_extra_projects_iter), LLVMProjects.phasar)
         self.assertRaises(StopIteration, next, vara_extra_projects_iter)
