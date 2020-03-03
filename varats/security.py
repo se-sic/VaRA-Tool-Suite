5623d81f8e76bfbf9a5cc877df7038e825e964ba
Marc Jose <marc@marc-jose.de>
2020-03-03 19:17:11 +0100

    Better formatting

--- varats/security.py
+++ varats/security.py
@@ -11,11 +11,11 @@ def list_cve_for_projects(vendor: str, product: str) -> None:
     """
     List all CVE's for the given vendor/product combination.
     """
-    print(f"Listing CVE's for {vendor}/{product}")
+    print(f"Listing CVE's for {vendor}/{product}:")
     try:
         cve_list = CVE.find_all_cve(vendor=vendor, product=product)
         for cve in cve_list:
             cve_url = f"https://cve.mitre.org/cgi-bin/cvename.cgi?name={cve}"
-            print(f"{cve} -> {cve_url}")
+            print(f"{cve.cve_id:20} {cve_url}")
     except ValueError:
         print('No entries found.')
