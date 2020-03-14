974342e671b5eb4e3180e0fb30479b09f2d02c6d
Sebastian Böhm <boehmseb@cs.uni-saarland.de>
2020-03-14 11:58:43 +0100

    Another try on fixing autodoc warnings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -75,4 +75,4 @@ add_module_names = True
 
 # -- Extension configuration -------------------------------------------------
 
-autodoc_mock_imports = ["plumbum.cmd", "benchbuild.utils.cmd", ]
+autodoc_mock_imports = ["plumbum", "benchbuild"]
