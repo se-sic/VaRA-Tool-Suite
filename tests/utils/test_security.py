557e0e94307a90895707e10d8d7b1d0f0c2f78af
Sebastian Böhm <boehmseb@cs.uni-saarland.de>
2020-03-19 12:16:15 +0100

    Refactor test_security.py

--- tests/utils/test_security.py
+++ tests/utils/test_security.py
@@ -14,6 +14,26 @@ class TestSecurity(unittest.TestCase):
     Security tests.
     """
 
+    REFERENCE_CVE_DATA: dict = {
+        'cve_id':
+            'CVE-2014-0160',
+        'score':
+            5.0,
+        'published':
+            datetime.strptime('2014-04-07 22:55:00', '%Y-%m-%d %H:%M:%S'),
+        'vector':
+            frozenset(['AV:N', 'AC:L', 'Au:N', 'C:P', 'I:N', 'A:N'])
+    }
+
+    REFERENCE_CWE_DATA: dict = {
+        'cwe_id': 'CWE-478',
+        'name': 'Missing Default Case in Switch Statement',
+        'description':
+            'The code does not have a default case in a switch statement, '
+            'which might lead to complex logical errors and resultant '
+            'weaknesses.'
+    }
+
     def test_find_single_cve(self):
         """
         Check if the Heartbleed's CVE-2014-0160 can be properly retrieved and
@@ -22,21 +42,10 @@ class TestSecurity(unittest.TestCase):
         """
         cve: CVE = find_cve('CVE-2014-0160')
 
-        reference_data: dict = {
-            'cve_id':
-                'CVE-2014-0160',
-            'score':
-                5.0,
-            'published':
-                datetime.strptime('2014-04-07 22:55:00', '%Y-%m-%d %H:%M:%S'),
-            'vector':
-                frozenset(['AV:N', 'AC:L', 'Au:N', 'C:P', 'I:N', 'A:N'])
-        }
-
-        self.assertTrue(cve.cve_id == reference_data['cve_id'])
-        self.assertTrue(cve.score == reference_data['score'])
-        self.assertTrue(cve.published == reference_data['published'])
-        self.assertTrue(cve.vector == reference_data['vector'])
+        self.assertTrue(cve.cve_id == self.REFERENCE_CVE_DATA['cve_id'])
+        self.assertTrue(cve.score == self.REFERENCE_CVE_DATA['score'])
+        self.assertTrue(cve.published == self.REFERENCE_CVE_DATA['published'])
+        self.assertTrue(cve.vector == self.REFERENCE_CVE_DATA['vector'])
 
     def test_find_all_cve(self):
         """
@@ -47,22 +56,13 @@ class TestSecurity(unittest.TestCase):
         cve_list: tp.FrozenSet[CVE] = find_all_cve('openssl', 'openssl')
         self.assertTrue(len(cve_list) != 0)
 
-        reference_data: dict = {
-            'cve_id':
-                'CVE-2014-0160',
-            'score':
-                5.0,
-            'published':
-                datetime.strptime('2014-04-07 22:55:00', '%Y-%m-%d %H:%M:%S'),
-            'vector': ['AV:N', 'AC:L', 'Au:N', 'C:P', 'I:N', 'A:N']
-        }
-
         found: bool = False
         for cve in cve_list:
-            if cve.cve_id == reference_data['cve_id']:
-                self.assertTrue(cve.score == reference_data['score'])
-                self.assertTrue(cve.published == reference_data['published'])
-                self.assertTrue(cve.vector == reference_data['vector'])
+            if cve.cve_id == self.REFERENCE_CVE_DATA['cve_id']:
+                self.assertTrue(cve.score == self.REFERENCE_CVE_DATA['score'])
+                self.assertTrue(
+                    cve.published == self.REFERENCE_CVE_DATA['published'])
+                self.assertTrue(cve.vector == self.REFERENCE_CVE_DATA['vector'])
                 found = True
                 break
         self.assertTrue(found)
@@ -74,18 +74,10 @@ class TestSecurity(unittest.TestCase):
         """
         self.assertTrue(len(CWE_LIST) != 0)
 
-        reference_data: dict = {
-            'cwe_id': 'CWE-478',
-            'name': 'Missing Default Case in Switch Statement',
-            'description':
-                'The code does not have a default case in a switch statement, '
-                'which might lead to complex logical errors and resultant '
-                'weaknesses.'
-        }
-
-        self.assertTrue(find_cwe(cwe_id=reference_data['cwe_id']))
-        self.assertTrue(find_cwe(cwe_name=reference_data['name']))
-        self.assertTrue(find_cwe(cwe_description=reference_data['description']))
+        self.assertTrue(find_cwe(cwe_id=self.REFERENCE_CWE_DATA['cwe_id']))
+        self.assertTrue(find_cwe(cwe_name=self.REFERENCE_CWE_DATA['name']))
+        self.assertTrue(
+            find_cwe(cwe_description=self.REFERENCE_CWE_DATA['description']))
 
     def test_find_all_cwe(self):
         """
@@ -94,23 +86,13 @@ class TestSecurity(unittest.TestCase):
         """
         cwe_list: tp.FrozenSet[CWE] = find_all_cwe()
         self.assertTrue(len(cwe_list) != 0)
-        print(cwe_list)
-
-        reference_data: dict = {
-            'cwe_id': 'CWE-478',
-            'name': 'Missing Default Case in Switch Statement',
-            'description':
-                'The code does not have a default case in a switch statement, '
-                'which might lead to complex logical errors and resultant '
-                'weaknesses.'
-        }
 
         found: bool = False
         for cwe in cwe_list:
-            if cwe.cwe_id == reference_data['cwe_id']:
-                self.assertTrue(cwe.name == reference_data['name'])
+            if cwe.cwe_id == self.REFERENCE_CWE_DATA['cwe_id']:
+                self.assertTrue(cwe.name == self.REFERENCE_CWE_DATA['name'])
                 self.assertTrue(
-                    cwe.description == reference_data['description'])
+                    cwe.description == self.REFERENCE_CWE_DATA['description'])
                 found = True
                 break
         self.assertTrue(found)
