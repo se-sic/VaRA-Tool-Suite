086d899fced9643dcaf9f99019a06629842bcf22
Sebastian Böhm <boehmseb@cs.uni-saarland.de>
2020-04-09 12:10:42 +0200

    Revert CVE tests to really fetch data.
    This is needed because the used CVE-API seems quite unstable

--- tests/data/test_cve.py
+++ tests/data/test_cve.py
@@ -5,7 +5,6 @@ Test the security utilities eg CVE, CWE stuff.
 from datetime import datetime
 import unittest
 import typing as tp
-from unittest import mock
 
 from varats.data.provider.cve.cve import (CVE, CWE, find_cve, find_all_cve,
                                           find_cwe, find_all_cwe)
@@ -15,17 +14,6 @@ class TestSecurity(unittest.TestCase):
     """
     Security tests.
     """
-
-    CVE_PAYLOAD = {
-        "Published": "2014-04-07T22:55:00",
-        "cvss": 5.0,
-        "cvss-vector": "AV:N/AC:L/Au:N/C:P/I:N/A:N",
-        "id": "CVE-2014-0160",
-        "references": [],
-        "summary": "Some summary"
-    }
-
-    ALL_CVE_PAYLOAD = {"results": [CVE_PAYLOAD], "total": 1}
 
     REFERENCE_CVE_DATA = {
         'cve_id':
@@ -47,15 +35,12 @@ class TestSecurity(unittest.TestCase):
             'weaknesses.'
     }
 
-    @mock.patch('varats.data.provider.cve.cve.__fetch_cve_data')
-    def test_find_single_cve(self, mock_fetch_cve_data):
+    def test_find_single_cve(self):
         """
         Check if the Heartbleed's CVE-2014-0160 can be properly retrieved and
         parsed.
         https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-2601
         """
-        mock_fetch_cve_data.return_value = self.CVE_PAYLOAD
-
         cve: CVE = find_cve('CVE-2014-0160')
 
         self.assertTrue(cve.cve_id == self.REFERENCE_CVE_DATA['cve_id'])
@@ -63,17 +48,14 @@ class TestSecurity(unittest.TestCase):
         self.assertTrue(cve.published == self.REFERENCE_CVE_DATA['published'])
         self.assertTrue(cve.vector == self.REFERENCE_CVE_DATA['vector'])
 
-    @mock.patch('varats.data.provider.cve.cve.__fetch_cve_data')
-    def test_find_all_cve(self, mock_fetch_cve_data):
+    def test_find_all_cve(self):
         """
         Get all OpenSSL CVE's and check if the Heartbleed CVE-2014-0160 is
         contained.
         @https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-2601
         """
-        mock_fetch_cve_data.return_value = self.ALL_CVE_PAYLOAD
-
         cve_list: tp.FrozenSet[CVE] = find_all_cve('openssl', 'openssl')
-        self.assertTrue(len(cve_list) == self.ALL_CVE_PAYLOAD["total"])
+        self.assertTrue(len(cve_list) > 0)
 
         found: bool = False
         for cve in cve_list:
