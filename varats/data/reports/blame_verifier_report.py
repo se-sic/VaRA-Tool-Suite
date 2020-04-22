b3821b0a6cf60df86b4a3d739ef923d26532dbeb
Simon Lichtnecker <cormensratio@posteo.de>
2020-04-22 11:55:01 +0200

    Adds a impl skeleton for the BlameVerifierReport

---
+++ varats/data/reports/blame_verifier_report.py
@@ -0,0 +1,37 @@
+from varats.data.report import BaseReport, MetaReport, FileStatusExtension
+
+
+class BlameVerifierReport(BaseReport):
+    """
+    TODO: Add further description (Currently empty report)
+    Full BlameVerifier report.
+    """
+
+    SHORTHAND = "BVR"
+    FILE_TYPE = "yaml"
+
+    @staticmethod
+    def get_file_name(project_name: str,
+                      binary_name: str,
+                      project_version: str,
+                      project_uuid: str,
+                      extension_type: FileStatusExtension,
+                      file_ext: str = "yaml") -> str:
+        """
+        Generates a filename for a commit report with 'yaml'
+        as file extension.
+
+        Args:
+            project_name: name of the project for which the report was generated
+            binary_name: name of the binary for which the report was generated
+            project_version: version of the analyzed project, i.e., commit hash
+            project_uuid: benchbuild uuid for the experiment run
+            extension_type: to specify the status of the generated report
+            file_ext: file extension of the report file
+
+        Returns:
+            name for the report file that can later be uniquely identified
+        """
+        return MetaReport.get_file_name(BlameVerifierReport.SHORTHAND, project_name,
+                                        binary_name, project_version,
+                                        project_uuid, extension_type, file_ext)
