7e0195b858fe82afe4951bf7d0902b43511e95cc
Sebastian Böhm <boehmseb@cs.uni-saarland.de>
2020-04-29 16:52:59 +0200

    Adds a file viewing utility (#267)
    Resolves se-passau/VaRA#489

--- varats/utils/cli_util.py
+++ varats/utils/cli_util.py
@@ -27,6 +27,51 @@ def cli_yn_choice(question: str, default: str = 'y') -> bool:
     return choice.strip().lower() in values
 
 
+ListType = tp.TypeVar("ListType")
+
+
+def cli_list_choice(question: str,
+                    choices: tp.List[ListType],
+                    choice_to_str: tp.Callable[[ListType], str],
+                    on_choice_callback: tp.Callable[[ListType], None],
+                    start_label: int = 0,
+                    default: int = 0,
+                    repeat: bool = False) -> None:
+    """
+    Ask the user to select an item from a list on the cli.
+
+    Args:
+        question: the question to ask the user
+        choices: the choices the user has
+        choice_to_str: a function converting a choice to a string
+        on_choice_callback: action to perform when a choice has been made
+        start_label: the number label of the first choice
+        default: the default choice that is taken if no input is given
+        repeat: whether to ask for another choice after ``on_choice_callback``
+                has finished
+    """
+    if repeat:
+        prompt = f"{question} or enter 'q' to quit (default={default}): "
+    else:
+        prompt = f"{question} (default={default}): "
+
+    max_idx_digits = len(str(len(choices) - 1))
+    for idx, choice in enumerate(choices, start=start_label):
+        idx_str = f"{idx}.".ljust(max_idx_digits + 1, " ")
+        print(f"{idx_str} {choice_to_str(choice)}")
+
+    user_choice = input(prompt)
+    while not user_choice.startswith("q"):
+        if not user_choice:
+            user_choice = str(default)
+        if user_choice.isdigit(
+        ) and start_label <= int(user_choice) < start_label + len(choices):
+            on_choice_callback(choices[int(user_choice) - start_label])
+        if not repeat:
+            return
+        user_choice = input(prompt)
+
+
 def initialize_logger_config() -> None:
     """
     Initializes the logging framework with a basic config, allowing the user to
