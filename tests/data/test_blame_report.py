0fb9e1a807e489ef6511b28d28cf8c7f3320f1d3
Florian Sattler <vuld3r@gmail.com>
2020-05-04 18:33:17 +0200

    Adds functionality to diff BlameReports
    Adds a BlameReportDiff class that represents the diff between two
    BlameReports and shows all interactions that changed.

--- tests/data/test_blame_report.py
+++ tests/data/test_blame_report.py
@@ -8,18 +8,21 @@ from pathlib import Path
 
 import yaml
 
-from varats.data.reports.blame_report import (BlameReport,
+from varats.data.reports.blame_report import (BlameReport, BlameReportDiff,
                                               BlameResultFunctionEntry,
                                               BlameInstInteractions,
                                               generate_degree_tuples)
 
-YAML_DOC_1 = """---
+FAKE_REPORT_PATH = (
+    "BR-xz-xz-fdbc0cfa71_63959faf-66d9-41e0-8dbb-abeee2c255eb_success.yaml")
+
+YAML_DOC_HEADER = """---
 DocType:         BlameReport
 Version:         1
 ...
 """
 
-YAML_DOC_2 = """---
+YAML_DOC_BR_1 = """---
 result-map:
   adjust_assignment_expression:
     demangled-name:  adjust_assignment_expression
@@ -42,25 +45,92 @@ result-map:
 ...
 """
 
+YAML_DOC_BR_2 = """---
+result-map:
+  adjust_assignment_expression:
+    demangled-name:  adjust_assignment_expression
+    insts:           []
+  bool_exec:
+    demangled-name:  bool_exec
+    insts:
+      - base-hash:       48f8ed5347aeb9d54e7ea041b1f8d67ffe74db33
+        interacting-hashes:
+          - a387695a1a2e52dcb1c5b21e73d2fd5a6aadbaf9
+        amount:          19
+      - base-hash:       48f8ed5347aeb9d54e7ea041b1f8d67ffe74db33
+        interacting-hashes:
+          - a387695a1a2e52dcb1c5b21e73d2fd5a6aadbaf9
+          - e8999a84efbd9c3e739bff7af39500d14e61bfbc
+        amount:          7
+  _Z7doStuffii:
+    demangled-name:  'doStuff(int, int)'
+    insts:
+      - base-hash:       48f8ed5347aeb9d54e7ea041b1f8d67ffe74db33
+        interacting-hashes:
+          - a387695a1a2e52dcb1c5b21e73d2fd5a6aadbaf9
+        amount:          3
+  _Z7doStuffdd:
+    demangled-name:  'doStuff(double, double)'
+    insts:
+      - base-hash:       48f8ed5347aeb9d54e7ea041b1f8d67ffe74db33
+        interacting-hashes:
+          - a387695a1a2e52dcb1c5b21e73d2fd5a6aadbaf9
+        amount:          2
+...
+"""
+
+YAML_DOC_BR_3 = """---
+result-map:
+  adjust_assignment_expression:
+    demangled-name:  adjust_assignment_expression
+    insts:           []
+  _Z7doStuffii:
+    demangled-name:  'doStuff(int, int)'
+    insts:           []
+...
+"""
+
+YAML_DOC_BR_4 = """---
+result-map:
+  adjust_assignment_expression:
+    demangled-name:  adjust_assignment_expression
+    insts:           []
+  bool_exec:
+    demangled-name:  bool_exec
+    insts:
+      - base-hash:       48f8ed5347aeb9d54e7ea041b1f8d67ffe74db33
+        interacting-hashes:
+          - a387695a1a2e52dcb1c5b21e73d2fd5a6aadbaf9
+          - e8999a84efbd9c3e739bff7af39500d14e61bfbc
+        amount:          5
+  _Z7doStuffii:
+    demangled-name:  'doStuff(int, int)'
+    insts:           []
+...
+"""
+
 
 class TestBlameInstInteractions(unittest.TestCase):
     """
     Test if a blame inst interactions are correctly reconstruction from yaml.
     """
+
     @classmethod
     def setUpClass(cls):
         """
         Load and parse function infos from yaml file.
         """
         with mock.patch("builtins.open",
-                        new=mock.mock_open(read_data=YAML_DOC_2)):
+                        new=mock.mock_open(read_data=YAML_DOC_BR_1)):
             with open("fake_file_path") as yaml_file:
                 yaml_doc = yaml.safe_load(yaml_file)
                 insts_iter = iter(yaml_doc['result-map']['bool_exec']['insts'])
-                cls.blame_interaction_1 = BlameInstInteractions(
-                    next(insts_iter))
-                cls.blame_interaction_2 = BlameInstInteractions(
-                    next(insts_iter))
+                cls.blame_interaction_1 = (
+                    BlameInstInteractions.create_blame_inst_interactions(
+                        next(insts_iter)))
+                cls.blame_interaction_2 = (
+                    BlameInstInteractions.create_blame_inst_interactions(
+                        next(insts_iter)))
 
     def test_base_hash(self):
         """
@@ -96,20 +166,23 @@ class TestResultFunctionEntry(unittest.TestCase):
     """
     Test if a result function entry is correctly reconstruction from yaml.
     """
+
     @classmethod
     def setUpClass(cls):
         """
         Load and parse function infos from yaml file.
         """
         with mock.patch("builtins.open",
-                        new=mock.mock_open(read_data=YAML_DOC_2)):
+                        new=mock.mock_open(read_data=YAML_DOC_BR_1)):
             with open("fake_file_path") as yaml_file:
                 yaml_doc = yaml.safe_load(yaml_file)
-                cls.func_entry_c = BlameResultFunctionEntry(
-                    'bool_exec', yaml_doc['result-map']['bool_exec'])
+                cls.func_entry_c = (
+                    BlameResultFunctionEntry.create_blame_result_function_entry(
+                        'bool_exec', yaml_doc['result-map']['bool_exec']))
 
-                cls.func_entry_cxx = BlameResultFunctionEntry(
-                    '_Z7doStuffii', yaml_doc['result-map']['_Z7doStuffii'])
+                cls.func_entry_cxx = (
+                    BlameResultFunctionEntry.create_blame_result_function_entry(
+                        '_Z7doStuffii', yaml_doc['result-map']['_Z7doStuffii']))
 
     def test_name(self):
         """
@@ -147,13 +220,15 @@ class TestBlameReport(unittest.TestCase):
     """
     Test if a blame report is correctly reconstructed from yaml.
     """
+
     @classmethod
     def setUpClass(cls):
         """
         Load and parse function infos from yaml file.
         """
         with mock.patch("builtins.open",
-                        new=mock.mock_open(read_data=YAML_DOC_1 + YAML_DOC_2)):
+                        new=mock.mock_open(read_data=YAML_DOC_HEADER +
+                                           YAML_DOC_BR_1)):
             loaded_report = BlameReport(Path('fake_file_path'))
             cls.report = loaded_report
 
@@ -191,17 +266,198 @@ class TestBlameReport(unittest.TestCase):
         self.assertEqual(next(func_entry_iter).name, '_Z7doStuffii')
 
 
+class TestBlameReportDiff(unittest.TestCase):
+    """
+    Test if diffs between BlameReports are correctly computed.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Load different blame_reports.
+        """
+        cls.reports = []
+        for report_yaml in [
+                YAML_DOC_BR_1, YAML_DOC_BR_2, YAML_DOC_BR_3, YAML_DOC_BR_4
+        ]:
+            with mock.patch("builtins.open",
+                            new=mock.mock_open(read_data=YAML_DOC_HEADER +
+                                               report_yaml)):
+                cls.reports.append(BlameReport(Path(FAKE_REPORT_PATH)))
+
+    def test_add_function_between_reports(self):
+        """
+        Checks if the diff containts functions that where added between
+        reports.
+        """
+        diff = BlameReportDiff(self.reports[1], self.reports[0])
+        new_func = diff.get_blame_result_function_entry('_Z7doStuffdd')
+
+        # Check if new function is correctly added to diff
+        self.assertEqual(new_func.name, '_Z7doStuffdd')
+        self.assertEqual(new_func.demangled_name, 'doStuff(double, double)')
+        self.assertEqual(len(new_func.interactions), 1)
+        self.assertEqual(new_func.interactions[0].base_commit,
+                         '48f8ed5347aeb9d54e7ea041b1f8d67ffe74db33')
+        self.assertEqual(len(new_func.interactions[0].interacting_commits), 1)
+        self.assertEqual(new_func.interactions[0].interacting_commits[0],
+                         'a387695a1a2e52dcb1c5b21e73d2fd5a6aadbaf9')
+        self.assertEqual(new_func.interactions[0].amount, 2)
+
+    def test_remove_function_between_reports(self):
+        """
+        Checks if the diff containts functions that where removed between
+        reports.
+        """
+        diff = BlameReportDiff(self.reports[2], self.reports[0])
+        del_func = diff.get_blame_result_function_entry('bool_exec')
+
+        # Check if deleted function is correctly added to diff
+        self.assertEqual(del_func.name, 'bool_exec')
+        self.assertEqual(del_func.demangled_name, 'bool_exec')
+        self.assertEqual(len(del_func.interactions), 2)
+        # Check first interaction
+        self.assertEqual(del_func.interactions[0].base_commit,
+                         '48f8ed5347aeb9d54e7ea041b1f8d67ffe74db33')
+        self.assertEqual(len(del_func.interactions[0].interacting_commits), 1)
+        self.assertEqual(del_func.interactions[0].interacting_commits[0],
+                         'a387695a1a2e52dcb1c5b21e73d2fd5a6aadbaf9')
+        self.assertEqual(del_func.interactions[0].amount, 22)
+
+        # Check second interaction
+        self.assertEqual(del_func.interactions[1].base_commit,
+                         '48f8ed5347aeb9d54e7ea041b1f8d67ffe74db33')
+        self.assertEqual(len(del_func.interactions[1].interacting_commits), 2)
+        self.assertEqual(del_func.interactions[1].interacting_commits[0],
+                         'a387695a1a2e52dcb1c5b21e73d2fd5a6aadbaf9')
+        self.assertEqual(del_func.interactions[1].interacting_commits[1],
+                         'e8999a84efbd9c3e739bff7af39500d14e61bfbc')
+        self.assertEqual(del_func.interactions[1].amount, 5)
+
+    def test_add_interaction(self):
+        """
+        Checks if the diff containts interactions that where added between
+        reports.
+        """
+        diff = BlameReportDiff(self.reports[1], self.reports[0])
+        changed_func = diff.get_blame_result_function_entry('_Z7doStuffii')
+
+        # Check if changed function is correctly added to diff
+        self.assertEqual(changed_func.name, '_Z7doStuffii')
+        self.assertEqual(changed_func.demangled_name, 'doStuff(int, int)')
+        self.assertEqual(len(changed_func.interactions), 1)
+        # Check first interaction
+        self.assertEqual(changed_func.interactions[0].base_commit,
+                         '48f8ed5347aeb9d54e7ea041b1f8d67ffe74db33')
+        self.assertEqual(len(changed_func.interactions[0].interacting_commits),
+                         1)
+        self.assertEqual(changed_func.interactions[0].interacting_commits[0],
+                         'a387695a1a2e52dcb1c5b21e73d2fd5a6aadbaf9')
+        self.assertEqual(changed_func.interactions[0].amount, 3)
+
+    def test_remove_interaction(self):
+        """
+        Checkfs if the diff contains interactions that where removed between
+        reports.
+        """
+        diff = BlameReportDiff(self.reports[3], self.reports[0])
+        del_func = diff.get_blame_result_function_entry('bool_exec')
+
+        # Check if deleted function is correctly added to diff
+        self.assertEqual(del_func.name, 'bool_exec')
+        self.assertEqual(del_func.demangled_name, 'bool_exec')
+        self.assertEqual(len(del_func.interactions), 1)
+        # Check first interaction
+        self.assertEqual(del_func.interactions[0].base_commit,
+                         '48f8ed5347aeb9d54e7ea041b1f8d67ffe74db33')
+        self.assertEqual(len(del_func.interactions[0].interacting_commits), 1)
+        self.assertEqual(del_func.interactions[0].interacting_commits[0],
+                         'a387695a1a2e52dcb1c5b21e73d2fd5a6aadbaf9')
+        self.assertEqual(del_func.interactions[0].amount, 22)
+
+    def test_increase_interaction_amount(self):
+        """
+        Checks if interactions where the amount increased between reports are
+        shown.
+        """
+        diff = BlameReportDiff(self.reports[1], self.reports[0])
+        changed_func = diff.get_blame_result_function_entry('bool_exec')
+
+        # Check if deleted function is correctly added to diff
+        self.assertEqual(changed_func.name, 'bool_exec')
+        self.assertEqual(changed_func.demangled_name, 'bool_exec')
+        self.assertEqual(len(changed_func.interactions), 2)
+
+        # Check second interaction, that was increased
+        self.assertEqual(changed_func.interactions[1].base_commit,
+                         '48f8ed5347aeb9d54e7ea041b1f8d67ffe74db33')
+        self.assertEqual(len(changed_func.interactions[1].interacting_commits),
+                         2)
+        self.assertEqual(changed_func.interactions[1].interacting_commits[0],
+                         'a387695a1a2e52dcb1c5b21e73d2fd5a6aadbaf9')
+        self.assertEqual(changed_func.interactions[1].interacting_commits[1],
+                         'e8999a84efbd9c3e739bff7af39500d14e61bfbc')
+        self.assertEqual(changed_func.interactions[1].amount, 2)
+
+    def test_decreased_interaction_amount(self):
+        """
+        Checks if interactions where the amount decreased between reports are
+        shown.
+        """
+        diff = BlameReportDiff(self.reports[1], self.reports[0])
+        changed_func = diff.get_blame_result_function_entry('bool_exec')
+
+        # Check if deleted function is correctly added to diff
+        self.assertEqual(changed_func.name, 'bool_exec')
+        self.assertEqual(changed_func.demangled_name, 'bool_exec')
+        self.assertEqual(len(changed_func.interactions), 2)
+
+        # Check first interaction, that was decreased
+        self.assertEqual(changed_func.interactions[0].base_commit,
+                         '48f8ed5347aeb9d54e7ea041b1f8d67ffe74db33')
+        self.assertEqual(len(changed_func.interactions[0].interacting_commits),
+                         1)
+        self.assertEqual(changed_func.interactions[0].interacting_commits[0],
+                         'a387695a1a2e52dcb1c5b21e73d2fd5a6aadbaf9')
+        self.assertEqual(changed_func.interactions[0].amount, 3)
+
+    def test_function_not_in_diff(self):
+        """
+        Checks that only functions that changed are in the diff.
+        """
+        # Report 2
+        diff = BlameReportDiff(self.reports[1], self.reports[0])
+        self.assertTrue(diff.has_function('bool_exec'))
+        self.assertTrue(diff.has_function('_Z7doStuffii'))
+        self.assertTrue(diff.has_function('_Z7doStuffdd'))
+        self.assertFalse(diff.has_function('adjust_assignment_expression'))
+
+        # Report 3
+        diff_2 = BlameReportDiff(self.reports[2], self.reports[0])
+        self.assertTrue(diff_2.has_function('bool_exec'))
+        self.assertFalse(diff_2.has_function('adjust_assignment_expression'))
+        self.assertFalse(diff_2.has_function('_Z7doStuffii'))
+
+        # Report 4
+        diff_3 = BlameReportDiff(self.reports[3], self.reports[0])
+        self.assertTrue(diff_3.has_function('bool_exec'))
+        self.assertFalse(diff_3.has_function('adjust_assignment_expression'))
+        self.assertFalse(diff_3.has_function('_Z7doStuffii'))
+
+
 class TestBlameReportHelperFunctions(unittest.TestCase):
     """
     Test if a blame report is correctly reconstruction from yaml.
     """
+
     @classmethod
     def setUpClass(cls):
         """
         Load and parse function infos from yaml file.
         """
         with mock.patch("builtins.open",
-                        new=mock.mock_open(read_data=YAML_DOC_1 + YAML_DOC_2)):
+                        new=mock.mock_open(read_data=YAML_DOC_HEADER +
+                                           YAML_DOC_BR_1)):
             loaded_report = BlameReport(Path('fake_file_path'))
             cls.report = loaded_report
 
