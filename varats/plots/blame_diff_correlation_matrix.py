cb77c443eb93695854d2c81e469b2580e8115d09
Florian Sattler <vuld3r@gmail.com>
2020-05-07 16:24:03 +0200

    Add correlation to plot

--- varats/plots/blame_diff_correlation_matrix.py
+++ varats/plots/blame_diff_correlation_matrix.py
@@ -8,11 +8,28 @@ import typing as tp
 
 import matplotlib.pyplot as plt
 import seaborn as sns
+from scipy.stats import pearsonr, spearmanr
 
 from varats.data.databases.blame_diff_metrics_database import (
     BlameDiffMetricsDatabase)
 from varats.data.reports.commit_report import CommitMap
 from varats.plots.plot import Plot
+
+
+def annotate_correlation(x, y, ax=None, **kws) -> None:
+    """
+    Plot the correlation coefficient in the top right hand corner of a plot.
+    """
+    ax = ax or plt.gca()
+    pearson_rho, _ = pearsonr(x, y)
+    ax.annotate(f'$\mathit{{\\rho_p}}$ = {pearson_rho:.2f}',
+                xy=(.6, .9),
+                xycoords=ax.transAxes)
+
+    spearman_rho, _ = spearmanr(x, y)
+    ax.annotate(f'$\mathit{{\\rho_s}}$ = {spearman_rho:.2f}',
+                xy=(.6, .77),
+                xycoords=ax.transAxes)
 
 
 class BlameDiffCorrelationMatrix(Plot):
@@ -44,9 +61,10 @@ class BlameDiffCorrelationMatrix(Plot):
 
         df.drop(df[df.churn_total == 0].index, inplace=True)
 
-        sns.pairplot(
-            df, hue="year",
+        g = sns.pairplot(
+            df,  #hue="year",
             diag_kind="hist")  # , vars=["churn_total", "diff_ci_total"])
+        g.map_lower(annotate_correlation)
         # grid = sns.PairGrid(df,
         #                     x_vars=["churn_total", "diff_ci_total"],
         #                     y_vars=["churn_total", "diff_ci_total"])
