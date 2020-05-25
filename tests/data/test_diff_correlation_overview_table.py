c600ed1adc727c023d9807cde99495bb694d446b
Sebastian Böhm <boehmseb@cs.uni-saarland.de>
2020-05-25 16:14:23 +0200

    Add automatic tmp_dir handling

--- tests/data/test_diff_correlation_overview_table.py
+++ tests/data/test_diff_correlation_overview_table.py
@@ -1,54 +1,22 @@
 import unittest
 
+import pytest
+
 from tests.test_utils import replace_config, TEST_INPUTS_DIR
-from varats.data.reports.commit_report import CommitMap
 from varats.tables import diff_correlation_overview_table
-from varats.tools.commit_map import get_commit_map
-
-
-def mocked_get_commit_map(project_name: str) -> CommitMap:
-    return {
-        "gzip": get_commit_map("xz", TEST_INPUTS_DIR / "cmaps" / "gzip.cmap"),
-        "xz": get_commit_map("xz", TEST_INPUTS_DIR / "cmaps" / "xz.cmap"),
-    }[project_name]
 
 
 class TestDiffCorrelationOverviewTable(unittest.TestCase):
 
+    @pytest.mark.slow
     @replace_config()
-    def test_table_tex_output(self, config):
+    def test_table_tex_output2(self, config):
         config["paper_config"]["current_config"
                               ] = "test_diff_correlation_overview_table"
-        # monkey-patch the table's get_commit_map()
-        get_commit_map_orig = diff_correlation_overview_table.get_commit_map
-        diff_correlation_overview_table.get_commit_map = mocked_get_commit_map
-
         table = diff_correlation_overview_table.DiffCorrelationOverviewTable(
         ).tabulate()
 
         with open(
             TEST_INPUTS_DIR / "tables" / "b_diff_correlation_overview.tex"
         ) as expected:
-            self.assertEqual(table, expected.read())
-
-        # restore monkey patch
-        diff_correlation_overview_table.get_commit_map = get_commit_map_orig
-
-    def test_table_tex_output2(self):
-        with replace_config() as config:
-            config["paper_config"]["current_config"
-                                  ] = "test_diff_correlation_overview_table"
-            # monkey-patch the table's get_commit_map()
-            get_commit_map_orig = diff_correlation_overview_table.get_commit_map
-            diff_correlation_overview_table.get_commit_map = mocked_get_commit_map
-
-            table = diff_correlation_overview_table.DiffCorrelationOverviewTable(
-            ).tabulate()
-
-            with open(
-                TEST_INPUTS_DIR / "tables" / "b_diff_correlation_overview.tex"
-            ) as expected:
-                self.assertEqual(table, expected.read())
-
-            # restore monkey patch
-            diff_correlation_overview_table.get_commit_map = get_commit_map_orig
+            assert table == expected.read()
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,7 @@
 """Module for test utility functions."""
 import contextlib
 import os
+import tempfile
 import typing as tp
 from copy import deepcopy
 from functools import wraps
@@ -13,29 +14,32 @@ import varats.settings as settings
 TEST_INPUTS_DIR = Path(os.path.dirname(__file__)) / 'TEST_INPUTS'
 
 
-def get_test_config() -> benchbuild.utils.settings.Configuration:
+def get_test_config(tmp_path: Path) -> benchbuild.utils.settings.Configuration:
     """
     Get a vara config suitable for testing.
 
     Configs returned by this function are meant to be passed to
     :func:`replace_config()`.
 
+    Args:
+        tmp_path: path to put files that may be written during test execution
+
     Returns:
         a (deep)copy of the current vara config suitable for testing
     """
     test_config = deepcopy(settings._CFG)
 
+    # setup test input dir
     test_config["config_file"] = None
-    test_config["data_cache"] = TEST_INPUTS_DIR / "data_cache"
     test_config["result_dir"] = TEST_INPUTS_DIR / "results"
-
     test_config["paper_config"]["folder"] = TEST_INPUTS_DIR / "paper_config"
     test_config["paper_config"]["current_config"] = None
 
-    # Do not pollute test environment. Tests should use temp dirs.
-    test_config["plots"]["plot_dir"] = None
-    test_config["tables"]["table_dir"] = None
-    test_config["artefacts"]["artefacts_dir"] = None
+    # let output config options point to test env.
+    test_config["data_cache"] = tmp_path / "data_cache"
+    test_config["plots"]["plot_dir"] = tmp_path / "plots"
+    test_config["tables"]["table_dir"] = tmp_path / "tables"
+    test_config["artefacts"]["artefacts_dir"] = tmp_path / "artefacts"
 
     return test_config
 
@@ -44,22 +48,30 @@ class _replace_config():
 
     def __init__(
         self,
+        tmp_path: tp.Optional[Path],
         config: tp.Optional[benchbuild.utils.settings.Configuration] = None
     ) -> None:
-        self.old_config = get_test_config()
+        if not tmp_path:
+            self.tmp_path = tempfile.TemporaryDirectory()
+            tmp_path = Path(self.tmp_path.name)
+
+        self.old_config = settings._CFG
         if config:
             self.new_config = config
         else:
-            self.new_config = deepcopy(self.old_config)
+            self.new_config = get_test_config(tmp_path)
 
     @contextlib.contextmanager
     def decoration_helper(self, args: tp.Any, kwargs: tp.Any) -> tp.Any:
         settings._CFG = self.new_config
+        settings.create_missing_folders()
         args += (self.new_config,)
         try:
             yield args, kwargs
         finally:
             settings._CFG = self.old_config
+            if self.tmp_path:
+                self.tmp_path.cleanup()
 
     def __call__(self, func) -> tp.Any:
 
@@ -72,13 +84,17 @@ class _replace_config():
 
     def __enter__(self):
         settings._CFG = self.new_config
+        settings.create_missing_folders()
         return self.new_config
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         settings._CFG = self.old_config
+        if self.tmp_path:
+            self.tmp_path.cleanup()
 
 
 def replace_config(
+    tmp_path: tp.Optional[Path] = None,
     config: tp.Optional[benchbuild.utils.settings.Configuration] = None
 ) -> tp.Any:
     """
@@ -95,10 +111,13 @@ def replace_config(
     after the `as`.
 
     Args:
+        tmp_path: path to put files that may be written during test execution.
+                  If absent, the wrapper will create a temporary directory that
+                  is deleted after restoring the config.
         config: if given, use this config instead of a copy of the current one
 
     Returns:
         the wrapped function
     """
 
-    return _replace_config(config)
+    return _replace_config(tmp_path, config)
