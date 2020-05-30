66b99c606df2c03a6a14e3e026f7b7797a083cb7
Simon Lichtnecker <38688755+cormensratio@users.noreply.github.com>
2020-05-30 13:19:21 +0200

    Update tests/data/test_wllvm.py
    Co-authored-by: Florian Sattler <vuld3r@gmail.com>

--- tests/data/test_wllvm.py
+++ tests/data/test_wllvm.py
@@ -44,7 +44,7 @@ class TestExtract(unittest.TestCase):
         mock_bc_file_name = f"{test_project_name}-{test_binary_name}-" \
                             f"{test_project_version}-dbg_O0.bc"
 
-        # Act
+        # When
         actual = Extract.get_bc_file_name(
             test_project_name, test_binary_name, test_project_version,
             test_bc_file_extensions
