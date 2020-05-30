15792cc27af131adf8c44e87fb7c46daff2dcfd4
Simon Lichtnecker <cormensratio@posteo.de>
2020-05-30 13:59:19 +0200

    Moves redundant code in setUpClass()

--- tests/data/test_blame_verifier_report.py
+++ tests/data/test_blame_verifier_report.py
@@ -9,61 +9,55 @@ from varats.data.reports import blame_verifier_report as BVR
 class TestBlameVerifierReport(unittest.TestCase):
     """Test if a blame verifier report is correctly reconstructed from .txt."""
 
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.test_project_name = "testProject"
+        cls.test_binary_name = "testBinary"
+        cls.test_project_version = "testProjectVersion1"
+        cls.test_project_uuid = "00000000-0000-0000-0000-000000000001"
+        cls.test_extension_type = 'success'
+        cls.test_file_ext = ".txt"
+
+        cls.mock_file_name = f"-{cls.test_project_name}" \
+                             f"-{cls.test_binary_name}-" \
+                             f"{cls.test_project_version}_" \
+                             f"{cls.test_project_uuid}_" \
+                             f"{cls.test_extension_type}" \
+                             f"{cls.test_file_ext}"
+
     def test_get_file_name_opt(self):
         """Test if the file name is correctly built with the opt extension."""
         # Given
-        test_shorthand = "BVR_Opt"
-        test_project_name = "testProject"
-        test_binary_name = "testBinary"
-        test_project_version = "testProjectVersion1"
-        test_project_uuid = "00000000-0000-0000-0000-000000000001"
-        test_extension_type = 'success'
-        test_file_ext = ".txt"
-
-        mock_file_name = f"{test_shorthand}-{test_project_name}" \
-                         f"-{test_binary_name}-{test_project_version}_" \
-                         f"{test_project_uuid}_{test_extension_type}" \
-                         f"{test_file_ext}"
+        self.mock_file_name = f"BVR_Opt{self.mock_file_name}"
 
         # When
         actual = BVR.BlameVerifierReportOpt.get_file_name(
-            project_name=test_project_name,
-            binary_name=test_binary_name,
-            project_version=test_project_version,
-            project_uuid=test_project_uuid,
+            project_name=self.test_project_name,
+            binary_name=self.test_binary_name,
+            project_version=self.test_project_version,
+            project_uuid=self.test_project_uuid,
             extension_type=FileStatusExtension.Success,
-            file_ext=test_file_ext
+            file_ext=self.test_file_ext
         )
 
         # Then
-        self.assertEqual(mock_file_name, actual)
+        self.assertEqual(self.mock_file_name, actual)
 
     def test_get_file_name_no_opt(self):
         """Test if the file name is correctly built without the opt
         extension."""
         # Given
-        test_shorthand = "BVR_NoOpt"
-        test_project_name = "testProject"
-        test_binary_name = "testBinary"
-        test_project_version = "testProjectVersion1"
-        test_project_uuid = "00000000-0000-0000-0000-000000000001"
-        test_extension_type = 'failed'
-        test_file_ext = ".txt"
-
-        mock_file_name = f"{test_shorthand}-{test_project_name}" \
-                         f"-{test_binary_name}-{test_project_version}_" \
-                         f"{test_project_uuid}_{test_extension_type}" \
-                         f"{test_file_ext}"
+        self.mock_file_name = f"BVR_NoOpt{self.mock_file_name}"
 
         # When
         actual = BVR.BlameVerifierReportNoOpt.get_file_name(
-            project_name=test_project_name,
-            binary_name=test_binary_name,
-            project_version=test_project_version,
-            project_uuid=test_project_uuid,
-            extension_type=FileStatusExtension.Failed,
-            file_ext=test_file_ext
+            project_name=self.test_project_name,
+            binary_name=self.test_binary_name,
+            project_version=self.test_project_version,
+            project_uuid=self.test_project_uuid,
+            extension_type=FileStatusExtension.Success,
+            file_ext=self.test_file_ext
         )
 
         # Then
-        self.assertEqual(mock_file_name, actual)
+        self.assertEqual(self.mock_file_name, actual)
