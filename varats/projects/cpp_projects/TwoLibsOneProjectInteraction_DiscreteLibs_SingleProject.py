9a1dbc2f32a0c7e3e70ad3f0c4bd4fb249e4ad17
Simon Lichtnecker <cormensratio@posteo.de>
2020-08-24 17:35:08 +0200

    Adapts project to new style

--- varats/projects/cpp_projects/TwoLibsOneProjectInteraction_DiscreteLibs_SingleProject.py
+++ varats/projects/cpp_projects/TwoLibsOneProjectInteraction_DiscreteLibs_SingleProject.py
@@ -2,11 +2,7 @@
 import typing as tp
 
 import benchbuild as bb
-from benchbuild.project import Project
 from benchbuild.utils.cmd import cmake, git, make
-from benchbuild.utils.compiler import cc
-from benchbuild.utils.download import with_git
-from benchbuild.utils.run import run
 from benchbuild.utils.settings import get_number_of_jobs
 from plumbum import local
 
@@ -16,19 +12,17 @@ from varats.utils.project_util import (
     ProjectBinaryWrapper,
     get_all_revisions_between,
     wrap_paths_to_binaries,
+    get_local_project_git_path,
 )
 
 
-class TwoLibsOneProjectInteractionDiscreteLibsSingleProject(Project):
+class TwoLibsOneProjectInteractionDiscreteLibsSingleProject(bb.Project):
     """Class to analyse interactions between two discrete libraries and one
     project."""
 
     NAME = 'TwoLibsOneProjectInteraction_DiscreteLibs_SingleProject'
     GROUP = 'cpp_projects'
     DOMAIN = 'analysis'
-    VERSION = 'HEAD'
-
-    SRC_FILE = NAME + "-{0}".format(VERSION)
 
     SOURCE = [
         bb.source.Git(
@@ -53,29 +47,32 @@ class TwoLibsOneProjectInteractionDiscreteLibsSingleProject(Project):
 
     def compile(self) -> None:
         """Contains instructions on how to build the project."""
-        self.download()
+
+        git_path = get_local_project_git_path(self.NAME)
+        version = self.version_of_primary
 
         # cmake as build system
-        with local.cwd(self.SRC_FILE):
-            version_id = git("rev-parse", "HEAD").strip()
+        with local.cwd(git_path):
             cmake_revisions = get_all_revisions_between(
-                "1cc6a93677f9a9825d3bbe20f1201e6652ba4a7d", "master"
+                "cc6a93677f9a9825d3bbe20f1201e6652ba4a7d", "master"
             )
 
-        if version_id in cmake_revisions:
+        if version in cmake_revisions:
             self.__compile_cmake()
         else:
             self.__compile_make()
 
     def __compile_cmake(self) -> None:
-        clang = cc(self)
-        with local.cwd(self.SRC_FILE):
+        version_source = bb.path(self.source_of_primary)
+        clang = bb.compiler.cc(self)
+        with local.cwd(version_source):
             with local.env(CC=str(clang)):
-                cmake("-G", "Unix Makefiles", ".")
-            run(make["-j", get_number_of_jobs(bb_cfg())])
+                bb.watch(cmake)("-G", "Unix Makefiles", ".")
+            bb.watch(make)("-j", get_number_of_jobs(bb_cfg()))
 
     def __compile_make(self) -> None:
-        clang = cc(self)
-        with local.cwd(self.SRC_FILE):
+        version_source = bb.path(self.source_of_primary)
+        clang = bb.compiler.cc(self)
+        with local.cwd(version_source):
             with local.env(CC=str(clang)):
-                run(make["-j", get_number_of_jobs(bb_cfg())])
+                bb.watch(make)("-j", get_number_of_jobs(bb_cfg()))
