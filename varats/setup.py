69ca984b009c000dce21a037eb4e1611267173a8
Florian Sattler <vuld3r@gmail.com>
2020-08-30 21:18:46 +0200

    Provides an fall back if we cannot determine a version number

--- varats/setup.py
+++ varats/setup.py
@@ -2,7 +2,11 @@ from setuptools import find_namespace_packages, setup
 
 setup(
     name='varats',
-    use_scm_version={'root': '..'},
+    use_scm_version={
+        'root': '..',
+        "relative_to": __file__,
+        "fallback_version": '10.0.0'
+    },
     url='https://github.com/se-passau/vara-tool-suite',
     packages=find_namespace_packages(include=['varats.*']),
     setup_requires=["pytest-runner", "setuptools_scm"],
