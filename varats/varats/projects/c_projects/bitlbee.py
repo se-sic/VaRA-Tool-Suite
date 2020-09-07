1caa203121449d564fc7ae380865ba87a31e825e
Sebastian Böhm <boehmseb@cs.uni-saarland.de>
2020-09-07 13:38:56 +0200

    Remove type ignores

--- varats/varats/projects/c_projects/bitlbee.py
+++ varats/varats/projects/c_projects/bitlbee.py
@@ -50,10 +50,10 @@ class Bitlbee(bb.Project, CVEProviderHook):  # type: ignore
         """Compile the project."""
         bitlbee_source = local.path(self.source_of(self.primary_source))
 
-        compiler = bb.compiler.cc(self)  # type: ignore
+        compiler = bb.compiler.cc(self)
         with local.cwd(bitlbee_source):
             with local.env(CC=str(compiler)):
-                bb.watch(local["./configure"])()  # type: ignore
+                bb.watch(local["./configure"])()
 
             bb.watch(make)("-j", get_number_of_jobs(bb_cfg()))  # type: ignore
 
