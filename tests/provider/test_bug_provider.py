d64e78384b2fafbb4b113a280dc0331815754a6d
Simon <s8sifedi@stud.uni-saarland.de>
2020-09-26 16:04:48 +0200

    adjusted remaining tests to use create_autospec

--- tests/provider/test_bug_provider.py
+++ tests/provider/test_bug_provider.py
@@ -1,6 +1,10 @@
 """Test bug_provider and bug modules."""
 import unittest
-from unittest.mock import Mock
+from unittest.mock import Mock, create_autospec
+
+from github.Issue import Issue
+from github.IssueEvent import IssueEvent
+from github.Label import Label
 
 from varats.provider.bug.bug import _has_closed_a_bug
 
@@ -20,8 +24,8 @@ class TestBugDetectionStrategies(unittest.TestCase):
         irrelevant_label.name = "good first issue"
 
         issue = create_autospec(Issue)
-        issue.labels = [irrelevant_label, bug_label]
         issue.id = 1
+        issue.labels = [irrelevant_label, bug_label]
 
         # sane mock issue event
         issue_event_bug = create_autospec(IssueEvent)
@@ -43,15 +47,14 @@ class TestBugDetectionStrategies(unittest.TestCase):
         """Test identifying issue events closing an issue that is not a bug."""
 
         # mock issue without labels
-        issue = Mock(**{'labels.return_value': [], 'id.return_value': 2})
+        issue = create_autospec(Issue)
+        issue.id = 2
+        issue.labels = []
 
-        issue_event = Mock(
-            **{
-                'event.return_value': "closed",
-                'commit_id.return_value': "1235",
-                'issue.return_value': issue
-            }
-        )
+        issue_event = create_autospec(IssueEvent)
+        issue_event.event = "closed"
+        issue_event.commit_id = "1235"
+        issue_event.issue = issue
 
         self.assertFalse(_has_closed_a_bug(issue_event))
 
@@ -59,28 +62,22 @@ class TestBugDetectionStrategies(unittest.TestCase):
         """Test identifying issue events not closing their issue."""
 
         # issue representing bug
-        bug_label = Mock(**{'name.return_value': "bug"})
-        issue = Mock(
-            **{
-                'labels.return_value': [bug_label],
-                'id.return_value': 3
-            }
-        )
+        bug_label = create_autospec(Label)
+        bug_label.name = "bug"
 
-        issue_event_pinned = Mock(
-            **{
-                'event.return_value': "pinned",
-                'commit_id.return_value': None,
-                'issue.return_value': issue
-            }
-        )
-        issue_event_assigned = Mock(
-            **{
-                'event.return_value': "assigned",
-                'commit_id.return_value': "1236",
-                'issue.return_value': issue
-            }
-        )
+        issue = create_autospec(Issue)
+        issue.id = 3
+        issue.labels = [bug_label]
+
+        issue_event_pinned = create_autospec(IssueEvent)
+        issue_event_pinned.event = "pinned"
+        issue_event_pinned.commit_id = None
+        issue_event_pinned.issue = issue
+
+        issue_event_assigned = create_autospec(IssueEvent)
+        issue_event_assigned.event = "assigned"
+        issue_event_assigned.commit_id = "1236"
+        issue_event_assigned.issue = issue
 
         self.assertFalse(_has_closed_a_bug(issue_event_pinned))
         self.assertFalse(_has_closed_a_bug(issue_event_assigned))
