4244b546f5da431a389f3b4d64038324a829f63e
Simon Lichtnecker <cormensratio@posteo.de>
2020-09-28 13:30:17 +0200

    Adds correct cc and cxx compiler

--- varats/varats/projects/cpp_projects/TwoLibsOneProjectInteraction_DiscreteLibs_SingleProject.py
+++ varats/varats/projects/cpp_projects/TwoLibsOneProjectInteraction_DiscreteLibs_SingleProject.py
@@ -59,7 +59,8 @@ class TwoLibsOneProjectInteraction_DiscreteLibs_SingleProject(
         """Contains instructions on how to build the project."""
 
         version_source = local.path(self.source_of_primary)
-        clang = bb.compiler.cc(self)  # type: ignore
+        c_compiler = bb.compiler.cc(self)  # type: ignore
+        cxx_compiler = bb.compiler.cxx(self)  # type: ignore
         mkdir(version_source / "build")
 
         # As long as multiple VaraTestRepoSources are not working, one has to
@@ -77,6 +78,6 @@ class TwoLibsOneProjectInteraction_DiscreteLibs_SingleProject(
             git("submodule", "update")
 
         with local.cwd(version_source / "build"):
-            with local.env(CC=str(clang)):
+            with local.env(CC=str(c_compiler), CXX=str(cxx_compiler)):
                 bb.watch(cmake)("-G", "Unix Makefiles", "..")  # type: ignore
             bb.watch(make)("-j", get_number_of_jobs(bb_cfg()))  # type: ignore
