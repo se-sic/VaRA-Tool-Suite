50dcd216984b4aa05f7f433a272fcfee66eb7a59
Sebastian Böhm <boehmseb@cs.uni-saarland.de>
2020-11-04 15:13:10 +0100

    containers: JustCompile working

--- varats/varats/containers/containers.py
+++ varats/varats/containers/containers.py
@@ -2,7 +2,9 @@
 
 import logging
 import typing as tp
+from copy import deepcopy
 from enum import Enum
+from tempfile import TemporaryDirectory
 
 from benchbuild.environments.domain.commands import Command, CreateImage
 from benchbuild.environments.domain.declarative import (
@@ -10,6 +12,7 @@ from benchbuild.environments.domain.declarative import (
     ContainerImage,
 )
 from benchbuild.environments.service_layer import messagebus, unit_of_work
+from plumbum import local
 
 from varats.tools.tool_util import get_research_tool
 from varats.utils.settings import bb_cfg, vara_cfg
@@ -25,9 +28,13 @@ __BASE_IMAGES: tp.Dict[ImageBase, ContainerImage] = {
     ImageBase.DEBIAN_10:
         ContainerImage().from_("debian:10").run('apt', 'update').run(
             'apt', 'install', '-y', 'python3', 'python3-dev', 'python3-pip',
-            'musl-dev', 'git', 'gcc', 'libgit2-dev', 'libffi-dev'
+            'musl-dev', 'git', 'gcc', 'libgit2-dev', 'libffi-dev',
+            'libyaml-dev', 'clang'
         )
 }
+
+VARATS_ROOT = "/varats_root/"
+BB_ROOT = "/app/"
 
 
 def _add_varats_layers(layers: ContainerImage) -> ContainerImage:
@@ -62,6 +69,33 @@ def _add_varats_layers(layers: ContainerImage) -> ContainerImage:
     return layers
 
 
+def _add_vara_config(layers: ContainerImage, tmp_dir: str) -> ContainerImage:
+    config = deepcopy(vara_cfg())
+    config_file = tmp_dir + "/.varats.yaml"
+
+    config["config_file"] = VARATS_ROOT + ".varats.yaml"
+    config["result_dir"] = VARATS_ROOT + "results/"
+    config["paper_config"]["folder"] = VARATS_ROOT + "paper_configs/"
+    config["benchbuild_root"] = BB_ROOT
+
+    #TODO: hook for research tool
+
+    config.store(local.path(config_file))
+    layers.copy_([config_file], config["config_file"].value)
+    layers.env(VARATS_CONFIG_FILE=config["config_file"].value)
+
+    return layers
+
+
+def _add_benchbuild_config(layers: ContainerImage) -> ContainerImage:
+    layers.env(
+        BB_VARATS_OUTFILE=f"{VARATS_ROOT}results/",
+        BB_VARATS_RESULT=f"{VARATS_ROOT}BC_files/"
+    )
+
+    return layers
+
+
 def create_base_image(base: ImageBase) -> None:
     """
     Build a base image for the given image base and the current research tool.
@@ -69,27 +103,31 @@ def create_base_image(base: ImageBase) -> None:
     Args:
         base: the image base
     """
-    image = __BASE_IMAGES[base]
-    image_name = f"{base.value}_varats"
+    with TemporaryDirectory() as tmpdir:
+        image = __BASE_IMAGES[base]
+        image_name = f"{base.value}_varats"
 
-    # we need an up-to-date pip version to get the prebuilt pygit2 package
-    # with an up-to-date libgit2
-    image.run('pip3', 'install', '--upgrade', 'pip')
-    add_benchbuild_layers(image)
-    _add_varats_layers(image)
+        # we need an up-to-date pip version to get the prebuilt pygit2 package
+        # with an up-to-date libgit2
+        image.run('pip3', 'install', '--upgrade', 'pip')
+        add_benchbuild_layers(image)
+        _add_varats_layers(image)
 
-    # add research tool if configured
-    configured_research_tool = vara_cfg()["container"]["research_tool"]
-    if configured_research_tool:
-        research_tool = get_research_tool(str(configured_research_tool))
-        image_name += f"_{research_tool.name}"
-        research_tool.add_container_layers(image)
+        # add research tool if configured
+        configured_research_tool = vara_cfg()["container"]["research_tool"]
+        if configured_research_tool:
+            research_tool = get_research_tool(str(configured_research_tool))
+            image_name += f"_{research_tool.name.lower()}"
+            research_tool.add_container_layers(image)
 
-    # TODO: configure varats + bb; result folders
+        _add_vara_config(image, tmpdir)
+        _add_benchbuild_config(image)
+        # image.run('')
 
-    cmd: Command = CreateImage(image_name, image)
-    uow = unit_of_work.ContainerImagesUOW()
-    messagebus.handle(cmd, uow)
+        image.workingdir(BB_ROOT)
+        cmd: Command = CreateImage(image_name, image)
+        uow = unit_of_work.ContainerImagesUOW()
+        messagebus.handle(cmd, uow)
 
 
 def create_base_images() -> None:
@@ -109,9 +147,9 @@ def get_base_image(base: ImageBase) -> ContainerImage:
     Returns:
         the requested base image
     """
-    image_name = base.value
+    image_name = f"{base.value}_varats"
     configured_research_tool = vara_cfg()["container"]["research_tool"]
     if configured_research_tool:
         research_tool = get_research_tool(str(configured_research_tool))
-        image_name += f"_{research_tool.name}"
+        image_name += f"_{research_tool.name.lower()}"
     return ContainerImage().from_(image_name)
