0554275c8a6872524073618420a27f822548756e
Simon Lichtnecker <cormensratio@posteo.de>
2020-11-23 14:20:48 +0100

    Adds differentiation for single and multi lib reports in database

--- varats/varats/data/databases/blame_interaction_degree_database.py
+++ varats/varats/data/databases/blame_interaction_degree_database.py
@@ -59,6 +59,7 @@ class BlameInteractionDegreeDatabase(
     ) -> pd.DataFrame:
         commit_lookup = create_commit_lookup_helper(project_name)
 
+        # TODO: Adapt layout for single and multi lib use cases
         def create_dataframe_layout() -> pd.DataFrame:
             df_layout = pd.DataFrame(columns=cls.COLUMNS)
             df_layout.degree = df_layout.degree.astype('int64')
@@ -77,112 +78,145 @@ class BlameInteractionDegreeDatabase(
         ) -> tp.Tuple[pd.DataFrame, str, str]:
             report = load_blame_report(report_path)
 
-            # TODO: Use categorised degrees and amounts only if the report has
-            #  multiple libs
             # TODO: Find better way to make multi_repo_report check
             multi_repo_report: bool = is_multi_repository_report(report)
 
-            categorised_list_of_degree_occurrences = \
-                generate_lib_dependent_degrees(report)
+            def build_single_lib_dataframe() -> pd.DataFrame:
+                list_of_degree_occurrences = generate_degree_tuples(report)
+                degrees, amounts = map(list, zip(*list_of_degree_occurrences))
+                total = sum(amounts)
 
-            total_amounts_of_all_libs = 0
+                list_of_author_degree_occurrences = \
+                    generate_author_degree_tuples(report, commit_lookup
+                )
+                author_degrees, author_amounts = map(
+                    list, zip(*list_of_author_degree_occurrences)
+                )
+                author_total = sum(author_amounts)
 
-            for base_name, lib_dict \
-                    in categorised_list_of_degree_occurrences.items():
-                for lib_name, tuple_list in lib_dict.items():
-                    for degree_amount_tuple in tuple_list:
-                        total_amounts_of_all_libs += degree_amount_tuple[1]
+                list_of_max_time_deltas = generate_max_time_distribution_tuples(
+                    report, commit_lookup, MAX_TIME_BUCKET_SIZE
+                )
+                max_time_buckets, max_time_amounts = map(
+                    list, zip(*list_of_max_time_deltas)
+                )
+                total_max_time_amounts = sum(max_time_amounts)
 
-            list_of_degree_occurrences = generate_degree_tuples(report)
-            degrees, amounts = map(list, zip(*list_of_degree_occurrences))
-            total = sum(amounts)
+                list_of_avg_time_deltas = generate_avg_time_distribution_tuples(
+                    report, commit_lookup, AVG_TIME_BUCKET_SIZE
+                )
+                avg_time_buckets, avg_time_amounts = map(
+                    list, zip(*list_of_avg_time_deltas)
+                )
+                total_avg_time_amounts = sum(avg_time_amounts)
 
-            list_of_author_degree_occurrences = generate_author_degree_tuples(
-                report, commit_lookup
-            )
-            author_degrees, author_amounts = map(
-                list, zip(*list_of_author_degree_occurrences)
-            )
-            author_total = sum(author_amounts)
+                amount_of_entries = len(
+                    degrees + author_degrees + max_time_buckets +
+                    avg_time_buckets
+                )
 
-            list_of_max_time_deltas = generate_max_time_distribution_tuples(
-                report, commit_lookup, MAX_TIME_BUCKET_SIZE
-            )
-            max_time_buckets, max_time_amounts = map(
-                list, zip(*list_of_max_time_deltas)
-            )
-            total_max_time_amounts = sum(max_time_amounts)
+                return pd.DataFrame({
+                    'revision': [report.head_commit] * amount_of_entries,
+                    'time_id': [commit_map.short_time_id(report.head_commit)] *
+                               amount_of_entries,
+                    'degree_type':
+                        [DegreeType.interaction.value] * len(degrees) +
+                        [DegreeType.author.value] * len(author_degrees) +
+                        [DegreeType.max_time.value] * len(max_time_buckets) +
+                        [DegreeType.avg_time.value] * len(avg_time_buckets),
+                    'degree':
+                        degrees + author_degrees + max_time_buckets +
+                        avg_time_buckets,
+                    'amount':
+                        amounts + author_amounts + max_time_amounts +
+                        avg_time_amounts,
+                    'fraction':
+                        np.concatenate([
+                            np.divide(amounts, total),
+                            np.divide(author_amounts, author_total),
+                            np.divide(max_time_amounts, total_max_time_amounts),
+                            np.divide(avg_time_amounts, total_avg_time_amounts)
+                        ]),
+                },
+                                    index=range(0, amount_of_entries))
 
-            list_of_avg_time_deltas = generate_avg_time_distribution_tuples(
-                report, commit_lookup, AVG_TIME_BUCKET_SIZE
-            )
-            avg_time_buckets, avg_time_amounts = map(
-                list, zip(*list_of_avg_time_deltas)
-            )
-            total_avg_time_amounts = sum(avg_time_amounts)
+            def build_multi_lib_dataframe() -> pd.DataFrame:
+                categorised_list_of_degree_occurrences = \
+                    generate_lib_dependent_degrees(report)
 
-            # TODO: calculate amount_of_entries
-            amount_of_entries = len(
-                degrees + author_degrees + max_time_buckets + avg_time_buckets
-            )
+                total_amounts_of_all_libs = 0
 
-            # TODO: Add degree_type etc. to dataframe rows
-            def build_dataframe_row(
-                base_library: str, inter_library: str, lib_degree: int,
-                lib_amount: int
-            ) -> tp.Dict:
+                # TODO: Find better way to calculate the sum of new amounts
+                for _, lib_dict \
+                        in categorised_list_of_degree_occurrences.items():
+                    for _, tuple_list in lib_dict.items():
+                        for degree_amount_tuple in tuple_list:
+                            total_amounts_of_all_libs += degree_amount_tuple[1]
 
-                data_dict: tp.Dict[str, tp.Any] = {
-                    'revision':
-                        report.head_commit,
-                    'time_id':
-                        commit_map.short_time_id(report.head_commit),
-                    'base_lib':
-                        base_library,
-                    'inter_lib':
-                        inter_library,
-                    'lib_degree':
-                        lib_degree,
-                    'lib_amount':
-                        lib_amount,
-                    'lib_fraction':
-                        np.divide(lib_amount, total_amounts_of_all_libs)
-                }
-                return data_dict
+                def build_dataframe_row(
+                    base_library: str, inter_library: str, lib_degree: int,
+                    lib_amount: int
+                ) -> tp.Dict:
 
-            result_data_dicts: tp.List[tp.Dict] = []
+                    data_dict: tp.Dict[str, tp.Any] = {
+                        'revision':
+                            report.head_commit,
+                        'time_id':
+                            commit_map.short_time_id(report.head_commit),
+                        'base_lib':
+                            base_library,
+                        'inter_lib':
+                            inter_library,
+                        'lib_degree':
+                            lib_degree,
+                        'lib_amount':
+                            lib_amount,
+                        'lib_fraction':
+                            np.divide(lib_amount, total_amounts_of_all_libs)
+                    }
+                    return data_dict
 
-            for base_lib_name, inter_lib_dict \
-                    in categorised_list_of_degree_occurrences.items():
+                result_data_dicts: tp.List[tp.Dict] = []
 
-                for inter_lib_name, degree_amount_tuples in \
-                        inter_lib_dict.items():
+                for base_lib_name, inter_lib_dict \
+                        in categorised_list_of_degree_occurrences.items():
 
-                    inter_degrees, inter_amounts = map(
-                        list, zip(*degree_amount_tuples)
-                    )
-                    # TODO: simplify with zip()
-                    for x in range(len(inter_degrees)):
-                        current_data_dict = build_dataframe_row(
-                            base_lib_name,
-                            inter_lib_name,
-                            tp.cast(tp.List, inter_degrees)[x],
-                            tp.cast(tp.List, inter_amounts)[x],
+                    for inter_lib_name, degree_amount_tuples in \
+                            inter_lib_dict.items():
+
+                        inter_degrees, inter_amounts = map(
+                            list, zip(*degree_amount_tuples)
                         )
-                        result_data_dicts.append(current_data_dict)
+                        # TODO: simplify with zip()
+                        for i in range(len(inter_degrees)):
+                            current_data_dict = build_dataframe_row(
+                                base_lib_name,
+                                inter_lib_name,
+                                tp.cast(tp.List, inter_degrees)[i],
+                                tp.cast(tp.List, inter_amounts)[i],
+                            )
+                            result_data_dicts.append(current_data_dict)
 
-            # TODO: remove testing rows
-            fire_lib_test_one = build_dataframe_row(
-                "fire_lib", "Elementalist", 5, 24
-            )
-            fire_lib_test_two = build_dataframe_row(
-                "fire_lib", "Elementalist", 6, 42
-            )
-            result_data_dicts.append(fire_lib_test_one)
-            result_data_dicts.append(fire_lib_test_two)
+                # TODO: remove testing rows
+                fire_lib_test_one = build_dataframe_row(
+                    "fire_lib", "Elementalist", 5, 21
+                )
+                fire_lib_test_two = build_dataframe_row(
+                    "fire_lib", "Elementalist", 6, 23
+                )
+                result_data_dicts.append(fire_lib_test_one)
+                result_data_dicts.append(fire_lib_test_two)
 
-            df = pd.DataFrame(result_data_dicts)
-            return df, report.head_commit, str(report_path.stat().st_mtime_ns)
+                return pd.DataFrame(result_data_dicts)
+
+            if multi_repo_report:
+                result_dataframe = build_multi_lib_dataframe()
+            else:
+                result_dataframe = build_single_lib_dataframe()
+
+            return result_dataframe, report.head_commit, str(
+                report_path.stat().st_mtime_ns
+            )
 
         report_files = get_processed_revisions_files(
             project_name, BlameReport,
