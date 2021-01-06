0afad8f75e2e5e233369fe7620f4d6713ce3f273
Sebastian Böhm <boehmseb@cs.uni-saarland.de>
2021-01-06 14:30:54 +0100

    Fix broken tests

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -4,14 +4,20 @@ import typing as tp
 
 import attr
 import plumbum as pb
-from benchbuild.source import Variant, BaseSource
+from benchbuild.source import Variant, FetchableSource
 
 
-@attr.s
-class BBTestSource(BaseSource):
+class BBTestSource(FetchableSource):
     """Source test fixture class."""
 
-    test_versions: tp.List[str] = attr.ib()
+    test_versions: tp.List[str]
+
+    def __init__(
+        self, test_versions: tp.List[str], local: str,
+        remote: tp.Union[str, tp.Dict[str, str]]
+    ):
+        super().__init__(local, remote)
+        self.test_versions = test_versions
 
     @property
     def default(self) -> Variant:
