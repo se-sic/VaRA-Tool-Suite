784b30c1c2fbb18f8bb7108fd45e1b5e562f13d9
Simon <nerumlp@gmail.com>
2021-02-22 11:32:50 +0100

    test xz make change

--- varats/varats/projects/c_projects/xz.py
+++ varats/varats/projects/c_projects/xz.py
@@ -100,13 +100,12 @@ class Xz(bb.Project, CVEProviderHook):  # type: ignore
                 short=True
             )
 
-        self.cflags += ["-fPIC"]
+        #self.cflags += ["-fPIC"]
 
         clang = bb.compiler.cc(self)
         with local.cwd(xz_version_source):
             with local.env(CC=str(clang)):
                 bb.watch(autoreconf)("--install")
-                bb.watch(local["./autogen.sh"])
                 configure = bb.watch(local["./configure"])
 
                 if xz_version in revisions_wo_dynamic_linking:
