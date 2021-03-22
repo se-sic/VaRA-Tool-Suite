23388d6238d947d82117a9ceac362094fac77036
Sebastian Böhm <boehmseb@cs.uni-saarland.de>
2021-03-22 11:00:42 +0100

    Add BlameInteractionGraph class.
    The class represents blame interaction data as a graph and provides different views on the data.

---
+++ varats/varats/data/reports/blame_interaction_graph.py
@@ -0,0 +1,70 @@
+"""Module for representing blame interaction data in a graph/network."""
+
+import typing as tp
+
+import networkx as nx
+
+from varats.data.reports.blame_report import (
+    BlameReport,
+    gen_base_to_inter_commit_repo_pair_mapping,
+    BlameReportDiff,
+)
+
+
+class BlameInteractionGraph():
+    """Graph/Network built from blame interaction data."""
+
+    def __init__(
+        self, project_name: str, report: tp.Union[BlameReport, BlameReportDiff]
+    ):
+        self.__report = report
+        self.__interaction_graph: tp.Optional[nx.Graph] = None
+        self.__project_name = project_name
+
+    def __build_interaction_graph(self) -> None:
+        if self.__interaction_graph:
+            return
+
+        interaction_graph = nx.DiGraph()
+        interactions = gen_base_to_inter_commit_repo_pair_mapping(self.__report)
+        commits = {
+            commit for base, inters in interactions.items()
+            for commit in [base, *inters.keys()]
+        }
+        interaction_graph.add_nodes_from([
+            (commit, {
+                "commit_hash": commit.commit_hash[:10],
+            }) for commit in commits
+        ])
+        interaction_graph.add_edges_from([
+            (base, inter, {
+                "amount": amount
+            })
+            for base, inters in interactions.items()
+            for inter, amount in inters.items()
+        ])
+
+        self.__interaction_graph = interaction_graph
+
+    @property
+    def commit_interaction_graph(self) -> nx.Graph:
+        """
+        Return a digraph with commits as nodes and interactions as edges.
+
+        The graph has the following attributes:
+        Nodes:
+          - commit_hash: commit hash of the commit represented by the node
+        Edges:
+          - amount: how often this interaction was found
+
+        Returns:
+            the commit interaction graph
+        """
+        if self.__interaction_graph is None:
+            self.__build_interaction_graph()
+        return self.__interaction_graph.copy(as_view=False)
+
+    @property
+    def author_interaction_graph(self) -> nx.Graph:
+        # TODO
+        pass
