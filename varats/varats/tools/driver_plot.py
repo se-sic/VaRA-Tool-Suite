abc6a4892c6f0644c924c5478fc1ad13a238b0ac
Sebastian Böhm <boehmseb@cs.uni-saarland.de>
2021-04-29 17:41:13 +0200

    Remove old plot driver code

--- varats/varats/tools/driver_plot.py
+++ varats/varats/tools/driver_plot.py
@@ -6,21 +6,13 @@ subcommand for each generator. For this to work, plot generators must be placed
 in the module `varats.plot.plots`.
 """
 
-import argparse
 import logging
 import typing as tp
-from pathlib import Path
 
 import click as click
 
 from varats.data.discover_reports import initialize_reports
-from varats.plot.plots import (
-    PlotRegistry,
-    build_plots,
-    PlotGenerator,
-    CommonPlotOptions,
-    PlotConfig,
-)
+from varats.plot.plots import PlotGenerator, CommonPlotOptions, PlotConfig
 from varats.plots.discover_plots import initialize_plots
 from varats.projects.discover_projects import initialize_projects
 from varats.utils.cli_util import initialize_cli_tool, add_cli_options
@@ -63,85 +55,6 @@ for generator_name, generator_cls in PlotGenerator.GENERATORS.items():
     # wrap command with options specified in the generator class
     command = add_cli_options(command_template, *generator_cls.OPTIONS)
     main.command(generator_name)(command)
-
-
-#  Old code begins here --------------------------------------------------------
-def old_main() -> None:
-    """
-    Main function for the graph generator.
-
-    `vara-plot`
-    """
-    initialize_cli_tool()
-    initialize_projects()
-    initialize_reports()
-    initialize_plots()
-    parser = argparse.ArgumentParser("vara-plot")
-    parser.add_argument(
-        "plot_type",
-        help="Plot to generate." + PlotRegistry.get_plot_types_help_string()
-    )
-    parser.add_argument(
-        "-r", "--result-output", help="Set the output folder for plot files"
-    )
-    parser.add_argument("-p", "--project", help="Project name")
-    parser.add_argument(
-        "-c", "--cmap", help="Path to commit map", default=None, type=Path
-    )
-    parser.add_argument(
-        "-v",
-        "--view",
-        help="Show the plot instead of saving it",
-        action='store_true',
-        default=False
-    )
-    parser.add_argument("--cs-path", help="Path to case_study", default=None)
-    parser.add_argument(
-        "--paper-config",
-        help="Generate plots for all case studies in the current paper config",
-        action='store_true',
-        default=False
-    )
-    parser.add_argument(
-        "--sep-stages",
-        help="Separate different stages of case study in the plot.",
-        action='store_true',
-        default=False
-    )
-    parser.add_argument(
-        "--report-type",
-        help="The report type to generate the plot for."
-        "Plots may ignore this option.",
-        default="EmptyReport"
-    )
-    parser.add_argument(
-        "extra_args",
-        metavar="KEY=VALUE",
-        nargs=argparse.REMAINDER,
-        help=(
-            "Provide additional arguments that will be passed to the plot "
-            "class. (do not put spaces before or after the '=' sign). "
-            "If a value contains spaces, you should define it "
-            'with double quotes: foo="bar baz".'
-        )
-    )
-
-    args = {k: v for k, v in vars(parser.parse_args()).items() if v is not None}
-
-    __plot(args)
-
-
-def __plot(args: tp.Dict[str, tp.Any]) -> None:
-    if 'extra_args' in args.keys():
-        extra_args = {
-            e[0].replace('-', '_'): e[1]
-            for e in [arg.split("=") for arg in args['extra_args']]
-        }
-    else:
-        extra_args = {}
-
-    build_plots(**args, **extra_args)
-
 
 if __name__ == '__main__':
     main()
