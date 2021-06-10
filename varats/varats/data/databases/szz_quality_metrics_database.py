170ff16539dc245e859855558b963c955069fe18
Sebastian Böhm <boehmseb@cs.uni-saarland.de>
2021-06-10 14:20:07 +0200

    Add SZZ quality metrics database + table

---
+++ varats/varats/data/databases/szz_quality_metrics_database.py
@@ -0,0 +1,226 @@
+"""Module for SZZ quality metrics data."""
+import logging
+import typing as tp
+from pathlib import Path
+
+import pandas as pd
+
+from varats.data.cache_helper import build_cached_report_table
+from varats.data.databases.blame_diff_metrics_database import (
+    id_from_paths,
+    timestamp_from_paths,
+    compare_timestamps,
+)
+from varats.data.databases.evaluationdatabase import EvaluationDatabase
+from varats.data.reports.blame_report import BlameReport
+from varats.data.reports.szz_report import (
+    SZZReport,
+    SZZUnleashedReport,
+    PyDrillerSZZReport,
+)
+from varats.jupyterhelper.file import load_blame_report
+from varats.mapping.commit_map import CommitMap, get_commit_map
+from varats.paper.case_study import CaseStudy
+from varats.project.project_util import (
+    get_project_cls_by_name,
+    get_primary_project_source,
+)
+from varats.projects.discover_projects import initialize_projects
+from varats.report.report import MetaReport
+from varats.revision.revisions import get_processed_revisions_files
+from varats.utils.cli_util import initialize_cli_tool
+from varats.utils.git_util import CommitRepoPair, create_commit_lookup_helper
+
+LOG = logging.getLogger(__name__)
+
+
+def _get_requested_report_paths(
+    project_name: str, szz_report: SZZReport
+) -> tp.Tuple[tp.Set[str], tp.Dict[str, Path]]:
+    bugs = szz_report.get_all_raw_bugs()
+    requested_report_revisions: tp.Set[str] = set()
+    for bug in bugs:
+        requested_report_revisions.add(bug.fixing_commit[:10])
+        requested_report_revisions.update(
+            hash[:10] for hash in bug.introducing_commits
+        )
+
+    report_map: tp.Dict[str, Path] = {}
+    for report_path in get_processed_revisions_files(project_name, BlameReport):
+        report_revision = MetaReport.get_commit_hash_from_result_file(
+            report_path.name
+        )
+        if report_revision in requested_report_revisions:
+            report_map[report_revision] = report_path
+
+    return requested_report_revisions, report_map
+
+
+def _get_interactions_for_commit(
+    report: BlameReport, commit: CommitRepoPair
+) -> tp.Tuple[tp.Set[CommitRepoPair], tp.Set[CommitRepoPair]]:
+    in_commits: tp.Set[CommitRepoPair] = set()
+    out_commits: tp.Set[CommitRepoPair] = set()
+    for func_entry in report.function_entries:
+        for interaction in func_entry.interactions:
+            if commit == interaction.base_commit:
+                out_commits.update(interaction.interacting_commits)
+            if commit in interaction.interacting_commits:
+                in_commits.add(interaction.base_commit)
+
+    return in_commits, out_commits
+
+
+def _calculate_szz_quality_score(
+    fix_in: tp.Set[CommitRepoPair], fix_out: tp.Set[CommitRepoPair],
+    intro_in: tp.Set[CommitRepoPair], intro_out: tp.Set[CommitRepoPair]
+):
+    """
+    Calculates a quality score that estimates how likely it is that a commit
+    introduced a bug that is fixed in another commit.
+
+    The score is calculated by computing how well the commit interactions
+    surrounding the fix match the commit interactions surrounding the
+    introducer. The underlying assumption is that if the data-flows (and hence,
+    the interactions) changed a lot between these commits, then it is likely
+    that the bug was introduced at a later point in time. Hence, the higher the
+    score, the more likely it is that the introducer was identified correctly.
+    The score is calculated as the fraction of changed commit interactions.
+    Incoming and outgoing interactions are viewed separately and combined via
+    weighted average.
+
+    Args:
+        fix_in: incoming interactions of the fixing commit
+        fix_out: outgoing interactions of the fixing commit
+        intro_in: incoming interactions of the introducing commit
+        intro_out: outgoing interactions of the introcucing commit
+
+    Returns:
+        a score estimating how likely it is that the introducer is correct
+    """
+    in_all = len(fix_in.union(intro_in))
+    in_diff = len(fix_in.symmetric_difference(intro_in))
+    in_frac = in_diff / in_all if in_all else 0
+    out_all = len(fix_out.union(intro_out))
+    out_diff = len(fix_out.symmetric_difference(intro_out))
+    out_frac = out_diff / out_all if out_all else 0
+    total = in_all + out_all
+    if len(fix_in) + len(fix_out) == 0 or len(intro_in) + len(intro_out) == 0:
+        score = -2
+    elif total == 0:
+        score = -1
+    else:
+        score = 1 - in_frac * (in_all / total) - out_frac * (out_all / total)
+    return score
+
+
+def _load_dataframe_for_report(
+    project_name: str, cache_id: str, columns: tp.List[str],
+    commit_map: CommitMap, szz_report: SZZReport
+) -> pd.DataFrame:
+    commit_lookup = create_commit_lookup_helper(project_name)
+    project = get_project_cls_by_name(project_name)
+    prj_src = get_primary_project_source(project_name)
+
+    def create_dataframe_layout() -> pd.DataFrame:
+        df_layout = pd.DataFrame(columns=columns)
+        return df_layout
+
+    def create_data_frame_for_report(
+        report_paths: tp.Tuple[Path, Path]
+    ) -> tp.Tuple[pd.DataFrame, str, str]:
+        # Look-up commit and infos about the HEAD commit of the report
+        fix_report = load_blame_report(report_paths[0])
+        intro_report = load_blame_report(report_paths[1])
+        fix_commit = commit_lookup(fix_report.head_commit, prj_src.local)
+        intro_commit = commit_lookup(intro_report.head_commit, prj_src.local)
+
+        fix_in, fix_out = _get_interactions_for_commit(
+            fix_report, CommitRepoPair(str(fix_commit.id), prj_src.local)
+        )
+        intro_in, intro_out = _get_interactions_for_commit(
+            intro_report, CommitRepoPair(str(intro_commit.id), prj_src.local)
+        )
+
+        score = _calculate_szz_quality_score(
+            fix_in, fix_out, intro_in, intro_out
+        )
+
+        return (
+            pd.DataFrame({
+                'revision': fix_report.head_commit,
+                'time_id': commit_map.short_time_id(fix_report.head_commit),
+                'introducer': intro_report.head_commit,
+                'score': score
+            },
+                         index=[0]), id_from_paths(report_paths),
+            timestamp_from_paths(report_paths)
+        )
+
+    requested_revisions, report_map = _get_requested_report_paths(
+        project_name, szz_report
+    )
+    available_revisions = report_map.keys()
+
+    new_entries: tp.List[tp.Tuple[Path, Path]] = []
+    remove_entries: tp.List[tp.Tuple[Path, Path]] = []
+    bugs = szz_report.get_all_raw_bugs()
+    for bug in bugs:
+        fix = bug.fixing_commit[:10]
+        if fix in available_revisions:
+            for intro in bug.introducing_commits:
+                intro = intro[:10]
+                if intro in available_revisions:
+                    new_entries.append((report_map[fix], report_map[intro]))
+
+    # cls.CACHE_ID is set by superclass
+    # pylint: disable=E1101
+    data_frame = build_cached_report_table(
+        cache_id, project_name, new_entries, remove_entries,
+        create_dataframe_layout, create_data_frame_for_report, id_from_paths,
+        timestamp_from_paths, compare_timestamps
+    )
+
+    return data_frame
+
+
+class SZZUnleashedQualityMetricsDatabase(
+    EvaluationDatabase,
+    cache_id="szz_unleashed_quality_metrics",
+    columns=["introducer", "score"]
+):
+    """SZZ quality metrics database for SZZUnleashed data."""
+
+    @classmethod
+    def _load_dataframe(
+        cls, project_name: str, commit_map: CommitMap,
+        case_study: tp.Optional[CaseStudy], **kwargs: tp.Any
+    ) -> pd.DataFrame:
+        report_paths = get_processed_revisions_files(
+            project_name, SZZUnleashedReport
+        )
+        return _load_dataframe_for_report(
+            project_name, cls.CACHE_ID, cls.COLUMNS, commit_map,
+            SZZUnleashedReport(report_paths[0])
+        )
+
+
+class PyDrillerSZZQualityMetricsDatabase(
+    EvaluationDatabase,
+    cache_id="pydriller_szz_quality_metrics",
+    columns=["introducer", "score"]
+):
+    """SZZ quality metrics database for PyDriller based SZZ data."""
+
+    @classmethod
+    def _load_dataframe(
+        cls, project_name: str, commit_map: CommitMap,
+        case_study: tp.Optional[CaseStudy], **kwargs: tp.Any
+    ) -> pd.DataFrame:
+        report_paths = get_processed_revisions_files(
+            project_name, PyDrillerSZZReport
+        )
+        return _load_dataframe_for_report(
+            project_name, cls.CACHE_ID, cls.COLUMNS, commit_map,
+            PyDrillerSZZReport(report_paths[0])
+        )
---
+++ varats/varats/tables/szz_quality_metrics_table.py
@@ -0,0 +1,69 @@
+"""Module for writing SZZ quality metrics tables."""
+import typing as tp
+
+import numpy as np
+import pandas as pd
+from tabulate import tabulate
+
+from varats.data.databases.szz_quality_metrics_database import (
+    PyDrillerSZZQualityMetricsDatabase,
+    SZZUnleashedQualityMetricsDatabase,
+)
+from varats.data.reports.szz_report import SZZTool
+from varats.mapping.commit_map import get_commit_map
+from varats.project.project_util import get_project_cls_by_name
+from varats.provider.bug.bug_provider import BugProvider
+from varats.table.table import Table, TableFormat, wrap_table_in_document
+
+
+class BugOverviewTable(Table):
+    """Visualizes SZZ quality metrics for a project."""
+
+    NAME = "szz_quality_metrics"
+
+    def __init__(self, **kwargs: tp.Any):
+        super().__init__(self.NAME, **kwargs)
+
+    def tabulate(self) -> str:
+        project_name = self.table_kwargs["project"]
+        szz_tool_name: tp.Optional[str] = self.table_kwargs.get(
+            "szz_tool", None
+        )
+        if not szz_tool_name:
+            raise ValueError("No szz tool provided")
+        szz_tool = SZZTool[szz_tool_name.upper()]
+
+        commit_map = get_commit_map(project_name)
+        columns = ["revision", "introducer", "score"]
+        if szz_tool == SZZTool.PYDRILLER_SZZ:
+            data = PyDrillerSZZQualityMetricsDatabase.get_data_for_project(
+                project_name, columns, commit_map
+            )
+        elif szz_tool == SZZTool.SZZ_UNLEASHED:
+            data = SZZUnleashedQualityMetricsDatabase.get_data_for_project(
+                project_name, columns, commit_map
+            )
+        else:
+            raise ValueError(f"Unknown SZZ tool '{szz_tool_name}'")
+
+        data.rename(
+            columns={
+                "revision": "fix",
+                "introducer": "introducer",
+                "score": "score"
+            },
+            inplace=True
+        )
+        data.set_index(["fix", "introducer"], inplace=True)
+        data.sort_values("score", inplace=True)
+        data.sort_index(level="fix", sort_remaining=False, inplace=True)
+
+        if self.format in [
+            TableFormat.latex, TableFormat.latex_raw, TableFormat.latex_booktabs
+        ]:
+            tex_code = data.to_latex(multicolumn_format="c", longtable=True)
+            return str(tex_code) if tex_code else ""
+        return tabulate(data, data.columns, self.format.value)
+
+    def wrap_table(self, table: str) -> str:
+        return wrap_table_in_document(table=table, landscape=True)
