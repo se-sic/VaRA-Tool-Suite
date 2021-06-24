a4c0d8cb3f8d9c64f8048b89d254fcea47be23ab
Simon <nerumlp@gmail.com>
2021-06-24 15:38:29 +0200

    add required package to get newer cmake version for libxml2

--- varats/varats/projects/c_projects/libxml2.py
+++ varats/varats/projects/c_projects/libxml2.py
@@ -37,7 +37,7 @@ class Libxml2(bb.Project, CVEProviderHook):  # type: ignore
     ]
 
     CONTAINER = get_base_image(ImageBase.DEBIAN_10
-                              ).run('apt', 'install', '-y', 'wget')\
+                              ).run('apt', 'install', '-y', 'wget', 'tar')\
         .run('wget', '-qO-', '\"https://cmake.org/files/v3.20/cmake-3.20.0-linux-x86_64.tar.gz\"',
              '|', 'tar', '--strip-components=1', '-xz', '-C', '~/.local')
 
