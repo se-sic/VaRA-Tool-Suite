b89efa9fe211dd55dca6d451b690e9cd8fae167f
Simon <nerumlp@gmail.com>
2021-07-05 09:10:32 +0200

    add container and dependecies to tmux

--- varats/varats/projects/c_projects/tmux.py
+++ varats/varats/projects/c_projects/tmux.py
@@ -6,6 +6,7 @@ from benchbuild.utils.cmd import make
 from benchbuild.utils.settings import get_number_of_jobs
 from plumbum import local
 
+from varats.containers.containers import get_base_image, ImageBase
 from varats.paper_mgmt.paper_config import project_filter_generator
 from varats.project.project_util import (
     wrap_paths_to_binaries,
@@ -34,6 +35,11 @@ class Tmux(bb.Project, CVEProviderHook):  # type: ignore
             version_filter=project_filter_generator("tmux")
         )
     ]
+
+    CONTAINER = get_base_image(ImageBase.DEBIAN_10).run(
+        'apt', 'install', '-y', 'libevent-dev', 'pkg-config', 'bison',
+        'autoconf', 'automake'
+    )
 
     @property
     def binaries(self) -> tp.List[ProjectBinaryWrapper]:
