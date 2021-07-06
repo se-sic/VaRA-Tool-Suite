8db0eaddedb262f1782dc93a1778ae823d2ee92d
Sebastian Böhm <boehmseb@cs.uni-saarland.de>
2021-07-06 19:18:01 +0200

    Always initialize custom benchbuild options with a value

--- varats/varats/tools/bb_config.py
+++ varats/varats/tools/bb_config.py
@@ -8,7 +8,6 @@ from copy import deepcopy
 from os import getcwd
 
 from benchbuild.utils import settings as s
-
 from varats.tools.tool_util import (
     get_supported_research_tool_names,
     get_research_tool_type,
@@ -125,9 +124,7 @@ def create_new_bb_config(varats_cfg: s.Configuration) -> s.Configuration:
     }
 
     # Add VaRA experiment config variables
-    add_vara_experiment_options(new_bb_cfg)
-    new_bb_cfg["varats"]["outfile"] = str(varats_cfg["result_dir"])
-    new_bb_cfg["varats"]["result"] = "BC_files"
+    add_vara_experiment_options(new_bb_cfg, varats_cfg)
 
     def replace_bb_cwd_path(
         cfg_varname: str, cfg_node: s.Configuration = new_bb_cfg
