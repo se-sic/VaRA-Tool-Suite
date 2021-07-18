9b712e57ed79889e7beab3c158a5d7f7aec7df3c
Florian Sattler <vuld3r@gmail.com>
2021-07-18 18:01:13 +0200

    Clean up pylint warning

--- varats-core/varats/report/gnu_time_report.py
+++ varats-core/varats/report/gnu_time_report.py
@@ -5,7 +5,7 @@ import re
 from datetime import timedelta
 from pathlib import Path
 
-from varats.report.report import BaseReport, FileStatusExtension
+from varats.report.report import BaseReport, FileStatusExtension, ReportFilename
 from varats.utils.util import static_vars
 
 
@@ -114,7 +114,7 @@ class TimeReport(BaseReport):
         Returns:
             name for the report file that can later be uniquly identified
         """
-        return BaseReport.get_file_name(
+        return ReportFilename.get_file_name(
             TimeReport.SHORTHAND, project_name, binary_name, project_version,
             project_uuid, extension_type, file_ext
         )
--- varats/varats/data/reports/szz_report.py
+++ varats/varats/data/reports/szz_report.py
@@ -6,7 +6,7 @@ import yaml
 
 from varats.base.version_header import VersionHeader
 from varats.provider.bug.bug import RawBug
-from varats.report.report import BaseReport, FileStatusExtension
+from varats.report.report import BaseReport, FileStatusExtension, ReportFilename
 
 
 class SZZReport(BaseReport):
@@ -119,7 +119,7 @@ class SZZUnleashedReport(SZZReport):
         Returns:
             name for the report file that can later be uniquely identified
         """
-        return BaseReport.get_file_name(
+        return ReportFilename.get_file_name(
             SZZUnleashedReport.SHORTHAND, project_name, binary_name,
             project_version, project_uuid, extension_type, file_ext
         )
@@ -160,7 +160,7 @@ class PyDrillerSZZReport(SZZReport):
         Returns:
             name for the report file that can later be uniquely identified
         """
-        return BaseReport.get_file_name(
+        return ReportFilename.get_file_name(
             PyDrillerSZZReport.SHORTHAND, project_name, binary_name,
             project_version, project_uuid, extension_type, file_ext
         )
