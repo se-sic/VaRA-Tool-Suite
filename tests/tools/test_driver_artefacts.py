9f7d6bc3719adaf0950026811452b21fb55d46e8
Sebastian Böhm <boehmseb@cs.uni-saarland.de>
2021-07-18 11:24:59 +0200

    Fix unittest warnings (#495)
    Override __del__() in DummyPydrillerRepo to avoid cleanup warnings.
    Rename TestInput to UnitTestInput and fix flaky tests
    Remove unused imports

--- tests/tools/test_driver_artefacts.py
+++ tests/tools/test_driver_artefacts.py
@@ -4,7 +4,7 @@ import unittest
 import unittest.mock as mock
 from pathlib import Path
 
-from tests.test_utils import run_in_test_environment, TestInputs
+from tests.test_utils import run_in_test_environment, UnitTestInputs
 from varats.data.discover_reports import initialize_reports
 from varats.paper_mgmt.artefacts import (
     Artefact,
@@ -44,7 +44,7 @@ class TestDriverArtefacts(unittest.TestCase):
         initialize_tables()
         initialize_plots()
 
-    @run_in_test_environment(TestInputs.PAPER_CONFIGS)
+    @run_in_test_environment(UnitTestInputs.PAPER_CONFIGS)
     @mock.patch('varats.table.tables.build_table', side_effect=_mock_table)
     @mock.patch('varats.plot.plots.build_plot', side_effect=_mock_plot)
     # pylint: disable=unused-argument
