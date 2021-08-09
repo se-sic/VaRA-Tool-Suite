e27fadbf128a12ad3010d469dae286b023116b67
jo <28813094+J03D03@users.noreply.github.com>
2021-08-09 15:12:59 +0200

    Add paper-config script

---
+++ add-paper-config.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+mkdir -p ${VARA_ROOT}/paper_configs/otfb
+cd ${VARA_ROOT} && vara-cs gen paper_configs/otfb/ UniformSamplingMethod --num-rev 0 --extra-revs 849ba01b8b72c01d60dc335fc4a8207562e87ad9 -p bison
+# cd ${VARA_ROOT} && vara-cs gen paper_configs/otfb/ UniformSamplingMethod --num-rev 0 --extra-revs 18a0f99732b1cb96cbb06763561c7a2526535a67 -p bitlbee
+# cd ${VARA_ROOT} && vara-cs gen paper_configs/otfb/ UniformSamplingMethod --num-rev 0 --extra-revs 08ca313d7edb99687068b93b5d2435b59f3db23a -p busybox
+cd ${VARA_ROOT} && vara-cs gen paper_configs/otfb/ UniformSamplingMethod --num-rev 0 --extra-revs d6125af095c9553f38cba0696f15158f5abe4ecc -p coreutils
+# cd ${VARA_ROOT} && vara-cs gen paper_configs/otfb/ UniformSamplingMethod --num-rev 0 --extra-revs 9bf79d0a5ab4a02ee1dd2b144489717c6c443250 -p curl
+# cd ${VARA_ROOT} && vara-cs gen paper_configs/otfb/ UniformSamplingMethod --num-rev 0 --extra-revs 1bfe35ca9ccfb7a4cefae6e39e135be2ecb02147 -p gawk
+cd ${VARA_ROOT} && vara-cs gen paper_configs/otfb/ UniformSamplingMethod --num-rev 0 --extra-revs 670b81a890388c60b7032a4f5b879f2ece8c4558 -p git
+# cd ${VARA_ROOT} && vara-cs gen paper_configs/otfb/ UniformSamplingMethod --num-rev 0 --extra-revs 79969f41a7caf9a503a04b78653db971918afa26 -p glibc
+cd ${VARA_ROOT} && vara-cs gen paper_configs/otfb/ UniformSamplingMethod --num-rev 0 --extra-revs 8820d0e08d1b389fc1e4ac2a17ad9f5418b21dfc -p gravity
+cd ${VARA_ROOT} && vara-cs gen paper_configs/otfb/ UniformSamplingMethod --num-rev 0 --extra-revs cb15dfa4b2d7fba0d50e87b49f979c7f996b8ebc -p grep
+cd ${VARA_ROOT} && vara-cs gen paper_configs/otfb/ UniformSamplingMethod --num-rev 0 --extra-revs 23a870d14a49803c6d2579071886c1acf497c9d1 -p gzip
+# cd ${VARA_ROOT} && vara-cs gen paper_configs/otfb/ UniformSamplingMethod --num-rev 0 --extra-revs 28d0e86e1e7d8419f9d56b2a91f46e0dce4baa41 -p htop
+cd ${VARA_ROOT} && vara-cs gen paper_configs/otfb/ UniformSamplingMethod --num-rev 0 --extra-revs e31d42b381dd38f41d132ce1455a8cedb089b78a -p irssi
+cd ${VARA_ROOT} && vara-cs gen paper_configs/otfb/ UniformSamplingMethod --num-rev 0 --extra-revs 97a1575cb877e593cf9940cd869f41b1ddd4a4fd -p libjpeg_turbo
+cd ${VARA_ROOT} && vara-cs gen paper_configs/otfb/ UniformSamplingMethod --num-rev 0 --extra-revs a37d4836519517bdce6cb9d956092321eca3e73b -p libpng
+cd ${VARA_ROOT} && vara-cs gen paper_configs/otfb/ UniformSamplingMethod --num-rev 0 --extra-revs cd15043656280e92714c139d820ef271d00876a4 -p libssh
+cd ${VARA_ROOT} && vara-cs gen paper_configs/otfb/ UniformSamplingMethod --num-rev 0 --extra-revs 465afe830f1432fbccb0e228da1f6e7ed4e79653 -p lrzip
+cd ${VARA_ROOT} && vara-cs gen paper_configs/otfb/ UniformSamplingMethod --num-rev 0 --extra-revs c240126068b780d927c126bb6a16cdeb92c35d77 -p lz4
+cd ${VARA_ROOT} && vara-cs gen paper_configs/otfb/ UniformSamplingMethod --num-rev 0 --extra-revs e278127cbfa2709d864ca9628a8ddb160c5c5331 -p openssl
+cd ${VARA_ROOT} && vara-cs gen paper_configs/otfb/ UniformSamplingMethod --num-rev 0 --extra-revs ccee09d1478aa69783926c208bfa235dcb055124 -p openvpn
+cd ${VARA_ROOT} && vara-cs gen paper_configs/otfb/ UniformSamplingMethod --num-rev 0 --extra-revs 61747bc6ec728de69d54db6ece90ad4617f059b8 -p opus
+# cd ${VARA_ROOT} && vara-cs gen paper_configs/otfb/ UniformSamplingMethod --num-rev 0 --extra-revs eba61056e4cca7cb187f3795ef19679d9f333428 -p qemu
+cd ${VARA_ROOT} && vara-cs gen paper_configs/otfb/ UniformSamplingMethod --num-rev 0 --extra-revs 1eb4baa5b8e76adc337ae9fab49acc2585a0cdd0 -p redis
+# cd ${VARA_ROOT} && vara-cs gen paper_configs/otfb/ UniformSamplingMethod --num-rev 0 --extra-revs c1d5dda62e30d3e778896a8aac0c372cda06fb69 -p tmux
+cd ${VARA_ROOT} && vara-cs gen paper_configs/otfb/ UniformSamplingMethod --num-rev 0 --extra-revs 981217c11f92b37f2baa51492cbe12e85d0ea493 -p vim
+cd ${VARA_ROOT} && vara-cs gen paper_configs/otfb/ UniformSamplingMethod --num-rev 0 --extra-revs ae03d92b52bb7581df2e75d571989cb1ecd19cbd -p x264
+cd ${VARA_ROOT} && vara-cs gen paper_configs/otfb/ UniformSamplingMethod --num-rev 0 --extra-revs e7da44d5151e21f153925781ad29334ae0786101 -p xz
