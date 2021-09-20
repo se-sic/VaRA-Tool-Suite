a2e208ff0e351604a89a8cbda05203fa9c19f411
Sebastian Böhm <boehmseb@cs.uni-saarland.de>
2021-09-20 18:53:16 +0200

    Move cli_util.py from core to varats

--- varats/varats/tools/driver_gen_commitmap.py
+++ varats/varats/tools/driver_gen_commitmap.py
@@ -4,7 +4,7 @@ import argparse
 from pathlib import Path
 
 from varats.mapping.commit_map import get_commit_map, store_commit_map
-from varats.utils.cli_util import initialize_cli_tool
+from varats.ts_utils.cli_util import initialize_cli_tool
 from varats.utils.settings import vara_cfg
 
 
