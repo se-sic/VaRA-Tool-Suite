875b015df4cf0da841ef0d6d48b375aba19573ec
Florian Sattler <vuld3r@gmail.com>
2021-09-22 15:31:02 +0200

    szzu shorthand

--- varats/varats/experiments/szz/szz_unleashed_experiment.py
+++ varats/varats/experiments/szz/szz_unleashed_experiment.py
@@ -175,7 +175,9 @@ class CreateSZZUnleashedReport(actions.Step):  # type: ignore
         return actions.StepResult.OK
 
 
-class SZZUnleashedExperiment(VersionExperiment):  # type: ignore
+class SZZUnleashedExperiment(
+    VersionExperiment, shorthand="SZZUnleashed"
+):  # type: ignore
     """
     Generates a SZZUnleashed report.
 
