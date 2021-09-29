de65803df08b34706b7ae82a63eba89b7360f628
jo <28813094+J03D03@users.noreply.github.com>
2021-09-29 17:10:18 +0200

    Add otfb experiment

--- varats/varats/tools/bb_config.py
+++ varats/varats/tools/bb_config.py
@@ -96,6 +96,7 @@ def create_new_bb_config(varats_cfg: s.Configuration) -> s.Configuration:
         'varats.experiments.vara.blame_verifier_experiment',
         'varats.experiments.vara.phasar_fta',
         'varats.experiments.phasar.ide_linear_constant_experiment',
+        'varats.experiments.phasar.otfb_experiment',
         'varats.experiments.phasar.global_analysis_compare',
         'varats.experiments.szz.szz_unleashed_experiment',
         'varats.experiments.szz.pydriller_szz_experiment',
