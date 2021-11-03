a99446624bf8e6458a90568956c7e1bc5f853dc4
sinerum <nerumlp@gmail.com>
2021-11-03 11:06:18 +0100

    port gen-bbconfig to click and create a unit tests for the command

---
+++ tests/tools/test_driver_gen_benchbuild_config.py
@@ -0,0 +1,23 @@
+"""Test varats gen benchbuild config module."""
+import unittest
+from pathlib import Path
+
+from click.testing import CliRunner
+
+from tests.test_utils import run_in_test_environment
+from varats.tools import driver_gen_benchbuild_config
+from varats.utils.settings import vara_cfg
+
+
+class TestDriverGenBenchbuildConfig(unittest.TestCase):
+    """Tests for the driver_gen_benchbuild_config module."""
+
+    @run_in_test_environment()
+    def test_gen_bbconfig(self):
+        """basic tests for the `gen-bbconfig` command."""
+        runner = CliRunner()
+        runner.invoke(driver_gen_benchbuild_config.main, [])
+        self.assertTrue(
+            Path(vara_cfg()["benchbuild_root"].value +
+                 "/.benchbuild.yml").exists()
+        )
