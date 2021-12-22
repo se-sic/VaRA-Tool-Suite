a6065685f82ca1e841d1269694f2e976d6d23e9b
Florian Sattler <vuld3r@gmail.com>
2021-12-22 21:28:04 +0100

    Implements RevisionBinaryMap
    With the RevisionBinaryMap it's easier for projects to define
    RevisionRanges at which a binary is valid, i.e., produced by the project.

--- varats-core/varats/utils/git_util.py
+++ varats-core/varats/utils/git_util.py
@@ -4,15 +4,19 @@ import os
 import re
 import typing as tp
 from enum import Enum
+from itertools import chain
 from pathlib import Path
 
 import pygit2
 from benchbuild.utils.cmd import git
+from benchbuild.utils.revision_ranges import RevisionRange
 from plumbum import local
 
 from varats.project.project_util import (
     get_local_project_git,
     get_primary_project_source,
+    BinaryType,
+    ProjectBinaryWrapper,
 )
 
 if tp.TYPE_CHECKING:
@@ -132,7 +136,60 @@ def get_current_branch(repo_folder: tp.Optional[Path] = None) -> str:
         return tp.cast(str, git("rev-parse", "--abbrev-ref", "HEAD").strip())
 
 
+def get_initial_commit(repo_folder: tp.Optional[Path] = None) -> FullCommitHash:
+    """
+    Get the initial commit of a repository, i.e., the first commit made.
+
+    Args:
+        repo_folder: where the git repository is located
+
+    Returns: initial commit hash
+    """
+    if repo_folder is None or repo_folder == Path(''):
+        return __get_initial_commit_impl()
+
+    with local.cwd(repo_folder):
+        return __get_initial_commit_impl()
+
+
+def __get_initial_commit_impl() -> FullCommitHash:
+    """
+    Get the initial commit of a repository, i.e., the first commit made.
+
+    Args:
+        repo_folder: where the git repository is located
+
+    Returns: initial commit hash
+    """
+    return FullCommitHash(git("rev-list", "--max-parents=0", "HEAD").strip())
+
+
 def get_all_revisions_between(
+    c_start: str,
+    c_end: str,
+    hash_type: tp.Type[CommitHashTy],
+    repo_folder: tp.Optional[Path] = None
+) -> tp.List[CommitHashTy]:
+    """
+    Returns a list of all revisions between two commits c_start and c_end
+    (inclusive), where c_start comes before c_end.
+
+    It is assumed that the current working directory is the git repository.
+
+    Args:
+        c_start: first commit of the range
+        c_end: last commit of the range
+        short: shorten revision hashes
+        repo_folder: where the git repository is located
+    """
+    if repo_folder is None or repo_folder == Path(''):
+        return __get_all_revisions_between_impl(c_start, c_end, hash_type)
+
+    with local.cwd(repo_folder):
+        return __get_all_revisions_between_impl(c_start, c_end, hash_type)
+
+
+def __get_all_revisions_between_impl(
     c_start: str, c_end: str, hash_type: tp.Type[CommitHashTy]
 ) -> tp.List[CommitHashTy]:
     """
@@ -148,10 +205,12 @@ def get_all_revisions_between(
     """
     result = [c_start]
     result.extend(
-        git(
-            "log", "--pretty=%H", "--ancestry-path",
-            "{}..{}".format(c_start, c_end)
-        ).strip().split()
+        reversed(
+            git(
+                "log", "--pretty=%H", "--ancestry-path",
+                "{}..{}".format(c_start, c_end)
+            ).strip().split()
+        )
     )
     return list(map(hash_type, result))
 
@@ -716,3 +775,73 @@ def calc_repo_loc(repo: pygit2.Repository, rev_range: str) -> int:
                 loc += len([line for line in lines if line])
 
     return loc
+
+
+################################################################################
+# Special git-specific classes
+
+
+class RevisionBinaryMap(tp.Container[str]):
+    """A special map that specifies for which revision ranges a binaries is
+    valid."""
+
+    def __init__(self, repo_location: Path) -> None:
+        self.__repo_location = repo_location
+        self.__revision_specific_mappings: tp.Dict[RevisionRange,
+                                                   ProjectBinaryWrapper] = {}
+        self.__always_valid_mappings: tp.List[ProjectBinaryWrapper] = []
+
+    def specify_binary(
+        self, location: str, binary_type: BinaryType, **kwargs
+    ) -> None:
+        """
+
+        Args:
+            location: where the binary can be found, relative to the \
+                      project-source root
+            binary_type: the type of binary that is produced
+            override_binary_name: overrides the used binary name
+            only_valid_in: additinally specifies a validity range that \
+                           specifies in which revision range this binary is \
+                           produced
+        """
+        binary_location_path = Path(location)
+        binary_name: str = kwargs.get(
+            "override_binary_name", binary_location_path.stem
+        )
+        validity_range = kwargs.get("only_valid_in", None)
+
+        wrapped_binary = ProjectBinaryWrapper(
+            binary_name, binary_location_path, binary_type
+        )
+
+        if validity_range:
+            self.__revision_specific_mappings[validity_range] = wrapped_binary
+        else:
+            self.__always_valid_mappings.append(wrapped_binary)
+
+    def __getitem__(self,
+                    revision: ShortCommitHash) -> tp.List[ProjectBinaryWrapper]:
+        revision_specific_binaries = []
+
+        for validity_range, wrapped_binary \
+                in self.__revision_specific_mappings.items():
+            if revision in get_all_revisions_between(
+                validity_range.id_start, validity_range.id_end, ShortCommitHash,
+                self.__repo_location
+            ):
+                revision_specific_binaries.append(wrapped_binary)
+
+        revision_specific_binaries.extend(self.__always_valid_mappings)
+
+        return revision_specific_binaries
+
+    def __contains__(self, binary_name: str) -> bool:
+        for binary in chain(
+            self.__always_valid_mappings,
+            self.__revision_specific_mappings.values()
+        ):
+            if binary.name == binary_name:
+                return True
+
+        return False
