4314ed1d5539f92408786626ef15d1e997732a08
sinerum <nerumlp@gmail.com>
2022-01-14 12:24:39 +0100

    merge vara-dev

--- varats-core/varats/utils/git_util.py
+++ varats-core/varats/utils/git_util.py
@@ -992,7 +992,10 @@ def branch_has_upstream(
     return tp.cast(bool, exit_code == 0)
 
 
-def calc_surviving_lines(repo: pygit2.Repository, revision) -> dict[str, int]:
+def calc_surviving_lines(
+    repo: pygit2.Repository, revision: [str, FullCommitHash],
+    revisions: tp.List[FullCommitHash]
+) -> dict[FullCommitHash, int]:
     """
 
     :param repo: repository to analyze
@@ -1005,7 +1008,7 @@ def calc_surviving_lines(repo: pygit2.Repository, revision) -> dict[str, int]:
         "|".join(churn_config.get_extensions_repr(r"^.*\.", r"$"))
     )
 
-    lines_per_revision: dict = {}
+    lines_per_revision: dict = {k: numpy.NaN for k in revisions}
     with local.cwd(project_path):
         git("checkout", revision)
         files = git("ls-tree", "-r", "--name-only", revision).splitlines()
@@ -1017,7 +1020,9 @@ def calc_surviving_lines(repo: pygit2.Repository, revision) -> dict[str, int]:
                     if line:
                         last_change = line[:FullCommitHash.hash_length()]
                         last_change = FullCommitHash(last_change)
-                        if lines_per_revision.keys().__contains__(last_change):
+                        if lines_per_revision.keys().__contains__(
+                            last_change
+                        ) and not math.isnan(lines_per_revision[last_change]):
                             lines_per_revision[
                                 last_change
                             ] = lines_per_revision[last_change] + 1
