b1f55f34018b0b9502a70c70481c89d9bb945e7d
Florian Sattler <vuld3r@gmail.com>
2022-01-18 12:24:42 +0100

    Makes incremental analysis more flexible

--- varats/varats/experiments/phasar/incremental_analysis.py
+++ varats/varats/experiments/phasar/incremental_analysis.py
@@ -1,7 +1,9 @@
 """Implements experiments for evaluating different incremental analysis
 approaches."""
 
+import os
 import typing as tp
+from enum import Enum
 from pathlib import Path
 
 import benchbuild as bb
@@ -39,6 +41,10 @@ from varats.experiment.wllvm import (
     get_bc_cache_actions,
     RunWLLVM,
 )
+from varats.experiments.vara.blame_experiment import (
+    setup_basic_blame_experiment,
+    generate_basic_blame_experiment_actions,
+)
 from varats.report.report import FileStatusExtension as FSE
 from varats.report.report import ReportSpecification
 from varats.utils.git_util import (
@@ -49,6 +55,35 @@ from varats.utils.git_util import (
 )
 from varats.utils.settings import bb_cfg
 from varats.utils.util import pairwise
+
+
+class AnalysisType(Enum):
+
+    value: str
+
+    TYPE_STATE = "typestate"
+    TAINT = "taint"
+    LCA = "ide-lca"
+
+    @staticmethod
+    def convert_from(value: str) -> tp.List['AnalysisType']:
+        enabled_analysis_types = []
+        for analysis_type in AnalysisType:
+            if analysis_type.value in value:
+                enabled_analysis_types.append(analysis_type)
+
+        return enabled_analysis_types
+
+
+def _get_enabled_analyses() -> tp.List[AnalysisType]:
+    """Allows overriding of analyses run by an experiment, this should only be
+    used for testing purposes, as the experiment will not generate all the
+    required results."""
+    env_analysis_selection = os.getenv("PHASAR_ANALYSIS")
+    if env_analysis_selection:
+        return AnalysisType.convert_from(env_analysis_selection)
+
+    return [at for at in AnalysisType]
 
 
 class RunAnalysisBase(actions.Step):
@@ -66,15 +101,13 @@ class RunAnalysisBase(actions.Step):
 
     def __init__(
         self, project: Project, experiment_handle: ExperimentHandle,
-        base_revision: ShortCommitHash, next_revision: ShortCommitHash,
-        analysis_flag: str
+        base_revision: ShortCommitHash, analysis_type: AnalysisType
     ) -> None:
         super().__init__(obj=project, action_fn=self.run_analysis)
 
         self.__experiment_handle = experiment_handle
-        self.__analysis_flag = analysis_flag
         self.__base_revision = base_revision
-        self.__next_revision = next_revision
+        self.__analysis_type = analysis_type
 
     def run_analysis(self) -> actions.StepResult:
         """Defines and runs the analysis comparision."""
@@ -89,11 +122,10 @@ class RunAnalysisBase(actions.Step):
             "--module",
             get_cached_bc_file_path(
                 project, binary, self.BC_FILE_EXTENSIONS, self.__base_revision
-            ), "--increment",
-            get_cached_bc_file_path(
-                project, binary, self.BC_FILE_EXTENSIONS, self.__next_revision
-            ), "--compare-mode", self.__analysis_flag
+            )
         ]
+        params += self._get_extra_parameters()
+        params += ["-D", str(self.__analysis_type)]
 
         run_cmd = phasar_llvm_inc[params]
 
@@ -108,65 +140,51 @@ class RunAnalysisBase(actions.Step):
 
         return actions.StepResult.OK
 
-
-class RunTypeStateAnalysis(RunAnalysisBase):
-    """
-    Runs a type-state analysis.
-
-    Analysis flag: --analysis typestate
-    """
-
-    NAME = "RunTypeStateAnalysis"
-    DESCRIPTION = "Compare the precision of the type-state analysis."
-
-    def __init__(
-        self, project: Project, experiment_handle: ExperimentHandle,
-        base_revision: ShortCommitHash, next_revision: ShortCommitHash
-    ) -> None:
-        super().__init__(
-            project, experiment_handle, base_revision, next_revision,
-            "--analysis typestate"
-        )
+    def _get_extra_parameters(self) -> tp.List[str]:
+        return []
 
 
-class RunTaintAnalysis(RunAnalysisBase):
-    """
-    Runs a taint analysis.
+class WholeProgramAnalysis(RunAnalysisBase):
 
-    Analysis flag: --analysis taint
-    """
+    NAME = "RunWholeAnalysis"
+    DESCRIPTION = "Running the configured analysis on the whole program."
 
-    NAME = "RunTaintAnalysis"
-    DESCRIPTION = "Compare the precision of the taint analysis."
+
+class IncrementalProgramAnalysis(RunAnalysisBase):
+
+    NAME = "RunIncrementalAnalysis"
+    DESCRIPTION = "Running the configured analysis only on the increment " \
+        + "between two revisison."
 
     def __init__(
         self, project: Project, experiment_handle: ExperimentHandle,
-        base_revision: ShortCommitHash, next_revision: ShortCommitHash
+        base_revision: ShortCommitHash, next_revision: ShortCommitHash,
+        analysis_type: AnalysisType
     ) -> None:
         super().__init__(
-            project, experiment_handle, base_revision, next_revision,
-            "--analysis taint"
+            project, experiment_handle, base_revision, analysis_type
         )
+        self.__next_revision = next_revision
+
+    def _get_extra_parameters(self) -> tp.List[str]:
+        return [
+            "--inc-module",
+            get_cached_bc_file_path(
+                project, binary, self.BC_FILE_EXTENSIONS, self.__next_revision
+            )
+        ]
 
 
-class RunLineraConstantPropagationAnalysis(RunAnalysisBase):
-    """
-    Runs a linear constant propagation.
+class AnalysisComparision(IncrementalProgramAnalysis):
 
-    Analysis flag: --analysis lca
-    """
+    NAME = "RunIncWPACompAnalysis"
+    DESCRIPTION = "Running the configured analysis in both, whole program and" \
+        + " incremental style."
 
-    NAME = "RunLineraConstantPropagationAnalysis"
-    DESCRIPTION = "Compare the precision of the liner constant analysis."
-
-    def __init__(
-        self, project: Project, experiment_handle: ExperimentHandle,
-        base_revision: ShortCommitHash, next_revision: ShortCommitHash
-    ) -> None:
-        super().__init__(
-            project, experiment_handle, base_revision, next_revision,
-            "--analysis lca"
-        )
+    def _get_extra_parameters(self) -> tp.List[str]:
+        return super()._get_extra_parameters() + [
+            "--wpa-inc-in-memory-comparison"
+        ]
 
 
 class PrecisionComparisionBase(VersionExperiment, shorthand=""):
@@ -176,44 +194,33 @@ class PrecisionComparisionBase(VersionExperiment, shorthand=""):
 
     REPORT_SPEC = ReportSpecification(EmptyReport)
 
-    MAX_REVISIONS_TO_EXPLORE = 3
-
     def __init__(
-        self, revision_step_with: int, *args: tp.Any, **kwargs: tp.Any
+        self, revision_step_with: int, max_revisions_to_explore: int,
+        analysis: IncrementalProgramAnalysis, *args: tp.Any, **kwargs: tp.Any
     ) -> None:
         super().__init__(*args, **kwargs)
         self.__revision_step_with = revision_step_with
+        self.__max_revisions_to_explore = max_revisions_to_explore
+        self.__analysis = analysis
 
     def actions_for_project(
         self, project: Project
     ) -> tp.MutableSequence[actions.Step]:
 
-        # Computes list of revisions that should be analyzed
-        revision_list = self.compute_revisions_to_explore(project)
-
-        project.runtime_extension = run.RuntimeExtension(project, self) \
-            << time.RunWithTime()
-
-        # Add the required compiler extensions to the project(s).
-        project.compiler_extension = compiler.RunCompiler(project, self) \
-            << RunWLLVM() \
-            << run.WithTimeout()
-
-        # Add own error handler to compile step.
-        project.compile = get_default_compile_error_wrapped(
-            self.get_handle(), project, EmptyReport
-        )
+        setup_basic_blame_experiment(self, project, REPORT_SPEC.main_report)
 
         analysis_actions = []
-
         analysis_actions.extend(
-            get_bc_cache_actions(
+            generate_basic_blame_experiment_actions(
                 project, RunAnalysisBase.BC_FILE_EXTENSIONS,
                 create_default_compiler_error_handler(
                     self.get_handle(), project, self.REPORT_SPEC.main_report
                 )
             )
         )
+
+        # Computes list of revisions that should be analyzed
+        revision_list = self.compute_revisions_to_explore(project)
 
         # Generate all required bc files for analysis
         for next_revision in revision_list[1:]:
@@ -237,22 +244,15 @@ class PrecisionComparisionBase(VersionExperiment, shorthand=""):
         # TODO (python3.10): replace with itertools.pairwise
         for base_revision, next_revision in pairwise(reversed(revision_list)):
             print(f"Compare From: {base_revision} -> {next_revision}")
-            # Run all analysis steps
-            analysis_actions.append(
-                RunTypeStateAnalysis(
-                    project, self.get_handle(), base_revision, next_revision
+
+            for enabled_analysis_type in _get_enabled_analyses():
+                # Run all analysis steps
+                analysis_actions.append(
+                    self.__analysis(
+                        project, self.get_handle(), base_revision,
+                        next_revision, enabled_analysis_type
+                    )
                 )
-            )
-            analysis_actions.append(
-                RunTaintAnalysis(
-                    project, self.get_handle(), base_revision, next_revision
-                )
-            )
-            analysis_actions.append(
-                RunLineraConstantPropagationAnalysis(
-                    project, self.get_handle(), base_revision, next_revision
-                )
-            )
 
         # Clean up the generated files afterwards
         analysis_actions.append(actions.Clean(project))
@@ -268,7 +268,65 @@ class PrecisionComparisionBase(VersionExperiment, shorthand=""):
         return get_all_revisions_between(
             get_initial_commit(project_repo_git).hash,
             project.version_of_primary, ShortCommitHash, project_repo_git
-        )[::-self.__revision_step_with][:self.MAX_REVISIONS_TO_EXPLORE]
+        )[::-self.__revision_step_with][:self.__max_revisions_to_explore]
+
+
+# Actuall, full scale experiments
+
+
+class RunPhasarIncWPA(PrecisionComparisionBase, shorthand="PIWPA"):
+    """Run the analyses WPA style."""
+
+    NAME = "PIWPA"
+
+    def actions_for_project(
+        self, project: Project
+    ) -> tp.MutableSequence[actions.Step]:
+
+        setup_basic_blame_experiment(self, project, REPORT_SPEC.main_report)
+
+        analysis_actions = []
+        analysis_actions.extend(
+            generate_basic_blame_experiment_actions(
+                project, RunAnalysisBase.BC_FILE_EXTENSIONS,
+                create_default_compiler_error_handler(
+                    self.get_handle(), project, self.REPORT_SPEC.main_report
+                )
+            )
+        )
+
+        for enabled_analysis_type in _get_enabled_analyses():
+            analysis_actions.append(
+                WholeProgramAnalysis(
+                    project, self.get_handle(),
+                    get_initial_commit(project_repo_git).hash,
+                    enabled_analysis_type
+                )
+            )
+
+        # Clean up the generated files afterwards
+        analysis_actions.append(actions.Clean(project))
+
+        return analysis_actions
+
+
+class RunPhasarIncIncremental(PrecisionComparisionBase, shorthand="PIInc"):
+    """Run the analyses incremental style."""
+
+    NAME = "PIInc"
+
+    def __init__(self, *args: tp.Any, **kwargs: tp.Any) -> None:
+        super().__init__(1, 2, IncrementalProgramAnalysis, *args, **kwargs)
+
+
+class RunPhasarIncCompare(PrecisionComparisionBase, shorthand="PIComp"):
+    """Run the analyses incremental, as well as, WPA style and compare their
+    results."""
+
+    NAME = "PIComp"
+
+    def __init__(self, *args: tp.Any, **kwargs: tp.Any) -> None:
+        super().__init__(1, 2, AnalysisComparision, *args, **kwargs)
 
 
 class IncrementalAnalysisPrecisionComparisionS1(
@@ -279,7 +337,7 @@ class IncrementalAnalysisPrecisionComparisionS1(
     NAME = "IncAPCs1"
 
     def __init__(self, *args: tp.Any, **kwargs: tp.Any) -> None:
-        super().__init__(1, *args, **kwargs)
+        super().__init__(1, 3, AnalysisComparision, *args, **kwargs)
 
 
 class IncrementalAnalysisPrecisionComparisionS5(
@@ -290,4 +348,4 @@ class IncrementalAnalysisPrecisionComparisionS5(
     NAME = "IncAPCs5"
 
     def __init__(self, *args: tp.Any, **kwargs: tp.Any) -> None:
-        super().__init__(5, *args, **kwargs)
+        super().__init__(5, 3, AnalysisComparision, *args, **kwargs)
