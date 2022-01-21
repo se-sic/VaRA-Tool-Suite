56060ebf50cee63200d62a962fcc4fae1e779d7a
Daniel Gusenburger <danielgusenburger@online.de>
2022-01-21 10:20:49 +0100

    first naive implementation of RunTraced and RunUntraced experiments

--- varats/varats/experiments/vara/compare_traced_untraced.py
+++ varats/varats/experiments/vara/compare_traced_untraced.py
@@ -3,12 +3,18 @@ execution performance of each binary that is produced by a project."""
 import os
 import typing as tp
 
+from pathlib import Path
+
 from benchbuild import Project
 from benchbuild.extensions import compiler, run, time
 from benchbuild.utils import actions
+from benchbuild.utils.cmd import time_cmd
 from plumbum import local
 
 from varats.experiment.experiment_util import (
+    exec_func_with_pe_error_handler,
+    get_default_compile_error_wrapped,
+    create_default_analysis_failure_handler,
     ExperimentHandle,
     get_varats_result_folder,
     VersionExperiment,
@@ -22,6 +28,63 @@ from varats.provider.feature.feature_model_provider import (
 from varats.report.report import ReportSpecification
 from varats.report.report import FileStatusExtension as FSE
 from varats.report.tef_report import TEFReport
+from varats.report.gnu_time_report import TimeReport
+
+# TODO: Refactor to use a bass class for experiments to avoid code duplication
+# where possible
+
+
+class TimeBinary(actions.Step):
+    """Executes the specified binaries and record the time it took to run"""
+
+    NAME = "TimeBinary"
+    DESCRIPTION = "Executes each binary and times it"
+
+    def __init__(self, project: Project, experiment_handle: ExperimentHandle):
+        super().__init__(obj=project, action_fn=self.time_run)  # type: ignore
+        self.__experiment_handle = experiment_handle
+
+    def time_run(self) -> actions.StepResult:
+        """Execute the specified binaries of the project, in specific
+        configurations, against one or multiple workloads."""
+        project: Project = self.obj
+
+        print(f"PWD {os.getcwd()}")
+
+        vara_result_folder = get_varats_result_folder(project)
+
+        for binary in project.binaries:  # type: ignore[attr-defined]
+            if binary.type != BinaryType.EXECUTABLE:
+                continue
+
+            result_file = self.__experiment_handle.get_file_name(
+                TimeReport.shorthand(),
+                project_name=str(project.name),
+                binary_name=binary.name,
+                project_revision=project.version_of_primary,  # type: ignore
+                project_uuid=str(project.run_uuid),
+                extension_type=FSE.SUCCESS,
+            )
+
+            with local.cwd(local.path(project.source_of_primary)):
+                print(f"Currently at {local.path(project.source_of_primary)}")
+                print(f"Bin path {binary.path}")
+                time_cmd = local["/usr/bin/time"]
+                run_cmd = time_cmd[
+                    "-o", f"{vara_result_folder}/{result_file}", "-v", binary.path
+                ]
+                # REVIEW: Is this correct ? Copied it from JustCompile
+                exec_func_with_pe_error_handler(
+                    run_cmd,
+                    create_default_analysis_failure_handler(
+                        self.__experiment_handle,
+                        project,
+                        TimeReport,
+                        Path(vara_result_folder),
+                    ),
+                )
+
+        return actions.StepResult.OK
 
 
 class ExecAndTraceBinary(actions.Step):  # type: ignore
@@ -29,7 +92,7 @@ class ExecAndTraceBinary(actions.Step):  # type: ignore
     configurations, against one or multiple workloads."""
 
     NAME = "ExecBinary"
-    DESCRIPTION = "Executes each binary and captures white-box performance traces."
+    DESCRIPTION = "Executes each binary and white-box performance traces."
 
     def __init__(self, project: Project, experiment_handle: ExperimentHandle):
         super().__init__(obj=project, action_fn=self.run_perf_tracing)  # type: ignore
@@ -66,7 +129,7 @@ class ExecAndTraceBinary(actions.Step):  # type: ignore
         return actions.StepResult.OK
 
 
-class RunTraced(VersionExperiment, shorthand="RT"):
+class RunTracedTEF(VersionExperiment, shorthand="RTTEF"):
     """Build and run the traced version of the binary"""
 
     NAME = "RunTraced"
@@ -119,12 +182,12 @@ class RunTraced(VersionExperiment, shorthand="RT"):
         return analysis_actions
 
 
-class RunUntraced(VersionExperiment, shorthand="RU"):
-    """Build and run the untraced version of the binary"""
+class RunTracedTime(VersionExperiment, shorthand="RTTIME"):
+    """Build and run the traced version of the binary"""
 
-    NAME = "RunUntraced"
+    NAME = "RunTracedTime"
 
-    REPORT_SPEC = ReportSpecification(TEFReport)
+    REPORT_SPEC = ReportSpecification(TimeReport)
 
     def actions_for_project(self, project: Project) -> tp.MutableSequence[actions.Step]:
         """
@@ -143,6 +206,11 @@ class RunUntraced(VersionExperiment, shorthand="RU"):
         if fm_path is None or not fm_path.exists():
             raise FeatureModelNotFound(project, fm_path)
 
+        # Sets FM model flags
+        project.cflags += ["-fvara-feature", f"-fvara-fm-path={fm_path.absolute()}"]
+        # Sets vara tracing flags
+        project.cflags += ["-fsanitize=vara", "-fvara-instr=trace_event"]
+
         # Add the required runtime extensions to the project(s).
         project.runtime_extension = (
             run.RuntimeExtension(project, self) << time.RunWithTime()
@@ -155,13 +223,52 @@ class RunUntraced(VersionExperiment, shorthand="RU"):
 
         # Add own error handler to compile step.
         project.compile = get_default_compile_error_wrapped(
-            self.get_handle(), project, TEFReport
+            self.get_handle(), project, TimeReport
         )
 
         analysis_actions = []
 
         analysis_actions.append(actions.Compile(project))
-        analysis_actions.append(ExecAndTraceBinary(project, self.get_handle()))
+        analysis_actions.append(TimeBinary(project, self.get_handle()))
+        analysis_actions.append(actions.Clean(project))
+
+        return analysis_actions
+
+
+class RunUntraced(VersionExperiment, shorthand="RU"):
+    """Build and run the untraced version of the binary"""
+
+    NAME = "RunUntraced"
+
+    REPORT_SPEC = ReportSpecification(TimeReport)
+
+    def actions_for_project(self, project: Project) -> tp.MutableSequence[actions.Step]:
+        """
+        Returns the specified steps to run the project(s) specified in the call
+        in a fixed order.
+
+        Args:
+            project: to analyze
+        """
+        # Add the required runtime extensions to the project(s).
+        project.runtime_extension = (
+            run.RuntimeExtension(project, self) << time.RunWithTime()
+        )
+
+        # Add the required compiler extensions to the project(s).
+        project.compiler_extension = (
+            compiler.RunCompiler(project, self) << run.WithTimeout()
+        )
+
+        # Add own error handler to compile step.
+        project.compile = get_default_compile_error_wrapped(
+            self.get_handle(), project, TimeReport
+        )
+
+        analysis_actions = []
+
+        analysis_actions.append(actions.Compile(project))
+        analysis_actions.append(TimeBinary(project, self.get_handle()))
         analysis_actions.append(actions.Clean(project))
 
         return analysis_actions
