0a9573f6ace289daf26b5ae0c208af85e002900d
Florian Sattler <vuld3r@gmail.com>
2022-01-29 16:42:29 +0100

    Remove Literal types

--- varats/varats/plots/bug_fixing_relations.py
+++ varats/varats/plots/bug_fixing_relations.py
@@ -267,7 +267,8 @@ def _create_line(
     dist = _get_distance(start, end)
     interval = _get_interval(dist)
 
-    control_points: np.ndarray[tp.Literal[2], np.dtype[np.float64]] = np.array([
+    # TODO: With min python 3.8 replace tp.Any -> tp.Literal[2]
+    control_points: np.ndarray[tp.Any, np.dtype[np.float64]] = np.array([
         start,
         np.true_divide(start, (__CP_PARAMETERS[interval])),
         np.true_divide(end, (__CP_PARAMETERS[interval])), end
@@ -376,14 +377,15 @@ def _get_commit_interval(distance: float, commit_count: int) -> int:
 
 
 def _get_bezier_curve(
-    ctrl_points: np.ndarray[tp.Literal[2], np.dtype[np.float64]],
+    # TODO: With min python 3.8 replace tp.Any -> tp.Literal[2]
+    ctrl_points: np.ndarray[tp.Any, np.dtype[np.float64]],
     num_points: int = 5
-) -> np.ndarray[tp.Literal[2], np.dtype[np.float64]]:
+) -> np.ndarray[tp.Any, np.dtype[np.float64]]:
     """Implements bezier edges to display between commit nodes."""
     n = ctrl_points.shape[0]
 
     def get_coordinate_on_curve(factor: float) -> npt.NDArray[np.float64]:
-        points_cp: np.ndarray[tp.Literal[2],
+        points_cp: np.ndarray[tp.Any,
                               np.dtype[np.float64]] = np.copy(ctrl_points)
         for i in range(1, n):
             points_cp[:n - i, :] = (
