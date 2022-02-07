d9f47e90e0c0332415a869a81c674e4d1409dbfd
Sebastian Böhm <boehmseb@cs.uni-saarland.de>
2022-02-07 18:20:43 +0100

    Fix broken tests

--- tests/test_containers.py
+++ tests/test_containers.py
@@ -65,23 +65,23 @@ class TestContainerSupport(unittest.TestCase):
         layers = image_context.layers
         self.check_layer_type(layers[0], FromLayer)
 
-        varats_install_layer = self.check_layer_type(layers[4], RunLayer)
+        varats_install_layer = self.check_layer_type(layers[10], RunLayer)
         self.assertEqual("pip3", varats_install_layer.command)
         self.assertTupleEqual(("install", "varats-core", "varats"),
                               varats_install_layer.args)
 
-        varats_copy_config_layer = self.check_layer_type(layers[5], CopyLayer)
+        varats_copy_config_layer = self.check_layer_type(layers[11], CopyLayer)
         self.assertEqual(
             "/varats_root/.varats.yaml", varats_copy_config_layer.destination
         )
 
-        varats_config_layer = self.check_layer_type(layers[6], UpdateEnv)
+        varats_config_layer = self.check_layer_type(layers[12], UpdateEnv)
         self.assertTupleEqual(
             ("VARATS_CONFIG_FILE", "/varats_root/.varats.yaml"),
             varats_config_layer.env[0]
         )
 
-        bb_config_layer = self.check_layer_type(layers[7], UpdateEnv)
+        bb_config_layer = self.check_layer_type(layers[13], UpdateEnv)
         self.assertTupleEqual(("BB_VARATS_OUTFILE", "/varats_root/results"),
                               bb_config_layer.env[0])
         self.assertTupleEqual(("BB_VARATS_RESULT", "/varats_root/BC_files"),
@@ -89,7 +89,7 @@ class TestContainerSupport(unittest.TestCase):
         self.assertTupleEqual(("BB_JOBS", "42"), bb_config_layer.env[2])
         self.assertTupleEqual(("BB_ENV", "{}"), bb_config_layer.env[3])
 
-        cwd_layer = self.check_layer_type(layers[8], WorkingDirectory)
+        cwd_layer = self.check_layer_type(layers[14], WorkingDirectory)
         self.assertEqual("/app", cwd_layer.directory)
 
     @run_in_test_environment()
@@ -109,12 +109,12 @@ class TestContainerSupport(unittest.TestCase):
 
         layers = image_context.layers
 
-        varats_install_layer = self.check_layer_type(layers[6], RunLayer)
+        varats_install_layer = self.check_layer_type(layers[12], RunLayer)
         self.assertEqual("pip3", varats_install_layer.command)
-        self.assertTupleEqual((
-            "install", "--ignore-installed", "/varats/varats-core",
-            "/varats/varats"
-        ), varats_install_layer.args)
+        self.assertTupleEqual(
+            ("install", "/varats/varats-core", "/varats/varats"),
+            varats_install_layer.args
+        )
         self.assertIn(("mount", "type=bind,src=varats_src,target=/varats"),
                       varats_install_layer.kwargs)
 
@@ -136,11 +136,11 @@ class TestContainerSupport(unittest.TestCase):
             _create_base_image_layers(image_context)
 
         layers = image_context.layers
-        ld_copy_layer = self.check_layer_type(layers[7], CopyLayer)
+        ld_copy_layer = self.check_layer_type(layers[13], CopyLayer)
         self.assertEqual("/varats_root/libs", ld_copy_layer.destination)
         self.assertTupleEqual(("foo/libs",), ld_copy_layer.sources)
 
-        bb_config_layer = self.check_layer_type(layers[8], UpdateEnv)
+        bb_config_layer = self.check_layer_type(layers[14], UpdateEnv)
         self.assertTupleEqual(
             ("BB_ENV", "{LD_LIBRARY_PATH: [/varats_root/libs]}"),
             bb_config_layer.env[3]
@@ -164,11 +164,11 @@ class TestContainerSupport(unittest.TestCase):
             _create_base_image_layers(image_context)
 
         layers = image_context.layers
-        vara_copy_layer = self.check_layer_type(layers[6], CopyLayer)
+        vara_copy_layer = self.check_layer_type(layers[12], CopyLayer)
         self.assertEqual("/varats_root/tools/VaRA", vara_copy_layer.destination)
         self.assertTupleEqual(("tools/VaRA",), vara_copy_layer.sources)
 
-        bb_config_layer = self.check_layer_type(layers[9], UpdateEnv)
+        bb_config_layer = self.check_layer_type(layers[15], UpdateEnv)
         self.assertTupleEqual(
             ("BB_ENV", "{PATH: [/varats_root/tools/VaRA/bin]}"),
             bb_config_layer.env[3]
