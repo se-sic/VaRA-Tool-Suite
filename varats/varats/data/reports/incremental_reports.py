179eb17da493c3a55c2566c585f0d503e1640d61
Florian Sattler <vuld3r@gmail.com>
2022-02-12 01:49:49 +0100

    Adds draft impl for phasar-inc time report loading

--- varats/varats/data/reports/incremental_reports.py
+++ varats/varats/data/reports/incremental_reports.py
@@ -5,6 +5,9 @@ import tempfile
 import typing as tp
 from enum import Enum
 from pathlib import Path
+from statistics import mean
+
+import yaml
 
 from varats.report.report import BaseReport, FileStatusExtension, ReportFilename
 
@@ -30,18 +33,277 @@ class AnalysisType(Enum):
         return f"{self.value}"
 
 
+class IncrementalTimings():
+    """
+    ---
+    INC_INITIAL_PT_CONSTRUCTION_TIME: 1000
+    INC_INITIAL_ICFG_CONSTRUCTION_TIME: 1000
+    INC_INITIAL_IRDB_CONSTRUCTION_TIME: 1000
+    INC_INITIAL_TH_CONSTRUCTION_TIME: 1000
+    INC_INCREMENTAL_IRDB_CONSTRUCTION_TIME: 1000
+
+    WPA_PT_CONSTRUCTION_TIME: 1000
+    WPA_ICFG_CONSTRUCTION_TIME: 1000
+    WPA_TH_CONSTRUCTION_TIME: 1000
+    WPA_IRDB_CONSTRUCTION_TIME: 1000
+    ...
+
+    * WPA_IRDB_CONSTRUCTION_TIME
+    * WPA_TH_CONSTRUCTION_TIME
+    * WPA_PT_CONSTRUCTION_TIME
+    * WPA_ICFG_CONSTRUCTION_TIME
+    * WPA_DFA_TEST_SOLVING_TIME
+
+
+    * INC_INITIAL_IRDB_CONSTRUCTION_TIME
+    * INC_INITIAL_TH_CONSTRUCTION_TIME
+    * INC_INITIAL_PT_CONSTRUCTION_TIME
+    * INC_INITIAL_ICFG_CONSTRUCTION_TIME
+    * INC_INITIAL_DFA_SOLVING_TIME
+
+
+    * INC_INCREMENTAL_IRDB_CONSTRUCTION_TIME
+    * INC_INCREMENTAL_DELTA_CONSTRUCTION_TIME
+    * INC_INCREMENTAL_IR_REMAP_TIME
+
+    * INC_INCREMENTAL_TH_CONSTRUCTION_TIME
+    * INC_INCREMENTAL_PT_CONSTRUCTION_TIME
+    * INC_INCREMENTAL_ICFG_CONSTRUCTION_TIME
+    * INC_INCREMENTAL_DFA_SOLVING_TIME
+
+    """
+
+    def __init__(self, path: Path) -> None:
+        self.__wpa_irdb_construction_time = []
+        self.__wpa_th_construction_time = []
+        self.__wpa_pt_construction_time = []
+        self.__wpa_icfg_construction_time = []
+        self.__wpa_dfa_test_solving_time = []
+        self.__inc_initial_irdb_construction_time = []
+        self.__inc_initial_th_construction_time = []
+        self.__inc_initial_pt_construction_time = []
+        self.__inc_initial_icfg_construction_time = []
+        self.__inc_initial_dfa_solving_time = []
+        self.__inc_incremental_irdb_construction_time = []
+        self.__inc_incremental_delta_construction_time = []
+        self.__inc_incremental_ir_remap_time = []
+        self.__inc_incremental_th_construction_time = []
+        self.__inc_incremental_pt_construction_time = []
+        self.__inc_incremental_icfg_construction_time = []
+        self.__inc_incremental_dfa_solving_time = []
+
+        with open(path, 'r') as stream:
+            documents = yaml.load_all(stream, Loader=yaml.CLoader)
+            for doc in documents:
+                for line in doc:
+                    print(f"{line} = {doc[line]}")
+
+                    if line == 'WPA_IRDB_CONSTRUCTION_TIME':
+                        self.__wpa_irdb_construction_time.append(
+                            float(doc[line])
+                        )
+                    if line == 'WPA_TH_CONSTRUCTION_TIME':
+                        self.__wpa_th_construction_time.append(float(doc[line]))
+
+                    if line == 'WPA_PT_CONSTRUCTION_TIME':
+                        self.__wpa_pt_construction_time.append(float(doc[line]))
+                    if line == 'WPA_ICFG_CONSTRUCTION_TIME':
+                        self.__wpa_icfg_construction_time.append(
+                            float(doc[line])
+                        )
+                    if line == 'WPA_DFA_TEST_SOLVING_TIME':
+                        self.__wpa_dfa_test_solving_time.append(
+                            float(doc[line])
+                        )
+                    if line == 'INC_INITIAL_IRDB_CONSTRUCTION_TIME':
+                        self.__inc_initial_irdb_construction_time.append(
+                            float(doc[line])
+                        )
+                    if line == 'INC_INITIAL_TH_CONSTRUCTION_TIME':
+                        self.__inc_initial_th_construction_time.append(
+                            float(doc[line])
+                        )
+                    if line == 'INC_INITIAL_PT_CONSTRUCTION_TIME':
+                        self.__inc_initial_pt_construction_time.append(
+                            float(doc[line])
+                        )
+                    if line == 'INC_INITIAL_ICFG_CONSTRUCTION_TIME':
+                        self.__inc_initial_icfg_construction_time.append(
+                            float(doc[line])
+                        )
+                    if line == 'INC_INITIAL_DFA_SOLVING_TIME':
+                        self.__inc_initial_dfa_solving_time.append(
+                            float(doc[line])
+                        )
+                    if line == 'INC_INCREMENTAL_IRDB_CONSTRUCTION_TIME':
+                        self.__inc_incremental_irdb_construction_time.append(
+                            float(doc[line])
+                        )
+                    if line == 'INC_INCREMENTAL_DELTA_CONSTRUCTION_TIME':
+                        self.__inc_incremental_delta_construction_time.append(
+                            float(doc[line])
+                        )
+                    if line == 'INC_INCREMENTAL_IR_REMAP_TIME':
+                        self.__inc_incremental_ir_remap_time.append(
+                            float(doc[line])
+                        )
+                    if line == 'INC_INCREMENTAL_TH_CONSTRUCTION_TIME':
+                        self.__inc_incremental_th_construction_time.append(
+                            float(doc[line])
+                        )
+                    if line == 'INC_INCREMENTAL_PT_CONSTRUCTION_TIME':
+                        self.__inc_incremental_pt_construction_time.append(
+                            float(doc[line])
+                        )
+                    if line == 'INC_INCREMENTAL_ICFG_CONSTRUCTION_TIME':
+                        self.__inc_incremental_icfg_construction_time.append(
+                            float(doc[line])
+                        )
+                    if line == 'INC_INCREMENTAL_DFA_SOLVING_TIME':
+                        self.__inc_incremental_dfa_solving_time.append(
+                            float(doc[line])
+                        )
+
+    @property
+    def wpa_irdb_construction_time(self) -> float:
+        return self.__mean(self.__wpa_irdb_construction_time)
+
+    @property
+    def wpa_th_construction_time(self) -> float:
+        return self.__mean(self.__wpa_th_construction_time)
+
+    @property
+    def wpa_pt_construction_time(self) -> float:
+        return self.__mean(self.__wpa_pt_construction_time)
+
+    @property
+    def wpa_icfg_construction_time(self) -> float:
+        return self.__mean(self.__wpa_icfg_construction_time)
+
+    @property
+    def wpa_dfa_test_solving_time(self) -> float:
+        return self.__mean(self.__wpa_dfa_test_solving_time)
+
+    def total_wpa(self) -> float:
+        return self.wpa_irdb_construction_time + \
+            self.wpa_th_construction_time + \
+            self.wpa_pt_construction_time + \
+            self.wpa_icfg_construction_time + \
+            self.wpa_dfa_test_solving_time
+
+    @property
+    def inc_initial_irdb_construction_time(self) -> float:
+        return self.__mean(self.__inc_initial_irdb_construction_time)
+
+    @property
+    def inc_initial_th_construction_time(self) -> float:
+        return self.__mean(self.__inc_initial_th_construction_time)
+
+    @property
+    def inc_initial_pt_construction_time(self) -> float:
+        return self.__mean(self.__inc_initial_pt_construction_time)
+
+    @property
+    def inc_initial_icfg_construction_time(self) -> float:
+        return self.__mean(self.__inc_initial_icfg_construction_time)
+
+    @property
+    def inc_initial_dfa_solving_time(self) -> float:
+        return self.__mean(self.__inc_initial_dfa_solving_time)
+
+    def total_initial(self) -> float:
+        return self.inc_initial_irdb_construction_time + \
+            self.inc_initial_th_construction_time + \
+            self.inc_initial_pt_construction_time + \
+            self.inc_initial_icfg_construction_time + \
+            self.inc_initial_dfa_solving_time
+
+    @property
+    def inc_incremental_irdb_construction_time(self) -> float:
+        return self.__mean(self.__inc_incremental_irdb_construction_time)
+
+    @property
+    def inc_incremental_delta_construction_time(self) -> float:
+        return self.__mean(self.__inc_incremental_delta_construction_time)
+
+    @property
+    def inc_incremental_ir_remap_time(self) -> float:
+        return self.__mean(self.__inc_incremental_ir_remap_time)
+
+    @property
+    def inc_incremental_th_construction_time(self) -> float:
+        return self.__mean(self.__inc_incremental_th_construction_time)
+
+    @property
+    def inc_incremental_pt_construction_time(self) -> float:
+        return self.__mean(self.__inc_incremental_pt_construction_time)
+
+    @property
+    def inc_incremental_icfg_construction_time(self) -> float:
+        return self.__mean(self.__inc_incremental_icfg_construction_time)
+
+    @property
+    def inc_incremental_dfa_solving_time(self) -> float:
+        return self.__mean(self.__inc_incremental_dfa_solving_time)
+
+    def total_incremental(self) -> float:
+        return self.inc_incremental_irdb_construction_time + \
+            self.inc_incremental_th_construction_time + \
+            self.inc_incremental_pt_construction_time + \
+            self.inc_incremental_icfg_construction_time + \
+            self.inc_incremental_dfa_solving_time
+
+    @staticmethod
+    def __mean(values: tp.List[float]) -> float:
+        if len(values) == 0:
+            return float('nan')
+
+        return mean(values)
+
+    def __str__(self) -> str:
+        string = f"""WPA_IRDB_CONSTRUCTION_TIME              = {self.wpa_irdb_construction_time}
+WPA_TH_CONSTRUCTION_TIME                = {self.wpa_th_construction_time}
+WPA_PT_CONSTRUCTION_TIME                = {self.wpa_pt_construction_time}
+WPA_ICFG_CONSTRUCTION_TIME              = {self.wpa_icfg_construction_time}
+WPA_DFA_TEST_SOLVING_TIME               = {self.wpa_dfa_test_solving_time}
+INC_INITIAL_IRDB_CONSTRUCTION_TIME      = {self.inc_initial_irdb_construction_time}
+INC_INITIAL_TH_CONSTRUCTION_TIME        = {self.inc_initial_th_construction_time}
+INC_INITIAL_PT_CONSTRUCTION_TIME        = {self.inc_initial_pt_construction_time}
+INC_INITIAL_ICFG_CONSTRUCTION_TIME      = {self.inc_initial_icfg_construction_time}
+INC_INITIAL_DFA_SOLVING_TIME            = {self.inc_initial_dfa_solving_time}
+INC_INCREMENTAL_IRDB_CONSTRUCTION_TIME  = {self.inc_incremental_irdb_construction_time}
+INC_INCREMENTAL_DELTA_CONSTRUCTION_TIME = {self.inc_incremental_delta_construction_time}
+INC_INCREMENTAL_IR_REMAP_TIME           = {self.inc_incremental_ir_remap_time}
+INC_INCREMENTAL_TH_CONSTRUCTION_TIME    = {self.inc_incremental_th_construction_time}
+INC_INCREMENTAL_PT_CONSTRUCTION_TIME    = {self.inc_incremental_pt_construction_time}
+INC_INCREMENTAL_ICFG_CONSTRUCTION_TIME  = {self.inc_incremental_icfg_construction_time}
+INC_INCREMENTAL_DFA_SOLVING_TIME        = {self.inc_incremental_dfa_solving_time}"""
+        return string
+
+
 class IncrementalReport(BaseReport, shorthand="Inc", file_type="zip"):
     """Report for phasar incremental analysis results."""
 
     def __init__(self, path: Path) -> None:
         super().__init__(path)
+
         with tempfile.TemporaryDirectory() as tmp_result_dir:
             shutil.unpack_archive(path, extract_dir=Path(tmp_result_dir))
+
+            for res_file in Path(tmp_result_dir).iterdir():
+                print(f"{res_file=}")
+                if str(res_file
+                      ).endswith('IDELinearConstantAnalysis-timings.yml'):
+                    self.__ide_timings = IncrementalTimings(res_file)
 
             # TODO: impl actual file handling
             collected_files = []
             for (dirpath, dirnames, filenames) in os.walk(Path(tmp_result_dir)):
                 collected_files.extend(filenames)
+
                 break
 
             print(f"Found files: {collected_files}")
+
+    def ide_timings(self) -> IncrementalTimings:
+        return self.__ide_timings
