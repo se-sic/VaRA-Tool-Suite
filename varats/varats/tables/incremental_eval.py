4df7d9f64f26f4cc4b6a6aaf3eb4eb4b15e0a589
Florian Sattler <vuld3r@gmail.com>
2022-02-14 22:26:53 +0100

    Add own metrics table for inc paper

--- varats/varats/tables/incremental_eval.py
+++ varats/varats/tables/incremental_eval.py
@@ -1,6 +1,7 @@
 import typing as tp
 
 import pandas as pd
+from benchbuild.utils.cmd import git
 from pylatex import Document, Package
 from tabulate import tabulate
 
@@ -12,9 +13,14 @@ from varats.jupyterhelper.file import load_incremental_report
 from varats.paper.case_study import CaseStudy
 from varats.paper_mgmt.case_study import get_case_study_file_name_filter
 from varats.paper_mgmt.paper_config import get_loaded_paper_config
+from varats.project.project_util import (
+    get_local_project_git,
+    get_project_cls_by_name,
+)
 from varats.revision.revisions import get_processed_revisions_files
 from varats.table.table import Table, wrap_table_in_document
 from varats.table.tables import TableFormat
+from varats.utils.git_util import calc_repo_loc
 
 
 def __round_and_format_delta(base_line: float, increment: float) -> float:
@@ -140,3 +146,68 @@ class PhasarGlobalsDataComparision(Table):
         return wrap_table_in_document(
             table=table, landscape=True, document_decorator=add_doc_defs
         )
+
+
+class PhasarIncMetricsTable(Table):
+    """Table showing some general information about the case studies in a paper
+    config."""
+
+    NAME = "phasar_inc_cs_metrics"
+
+    def __init__(self, **kwargs: tp.Any):
+        super().__init__(self.NAME, **kwargs)
+
+    def tabulate(self) -> str:
+        case_studies = get_loaded_paper_config().get_all_case_studies()
+
+        cs_data: tp.List[pd.DataFrame] = []
+        for case_study in case_studies:
+            project_name = case_study.project_name
+            project_cls = get_project_cls_by_name(project_name)
+            project_repo = get_local_project_git(project_name)
+            project_path = project_repo.path[:-5]
+            project_git = git["-C", project_path]
+
+            revision = self.table_kwargs.get("revisions", {}).get(
+                case_study.project_name, None
+            )
+            revisions = case_study.revisions
+            if not revision and len(revisions) == 1:
+                revision = revisions[0]
+            rev_range = revision.hash if revision else "HEAD"
+
+            cs_dict = {
+                project_name: {
+                    "Domain":
+                        str(project_cls.DOMAIN)[0].upper() +
+                        str(project_cls.DOMAIN)[1:],
+                    "LOC":
+                        calc_repo_loc(project_repo, rev_range),
+                    "Commits":
+                        int(project_git("rev-list", "--count", rev_range)),
+                    # "Authors":
+                    #     len(
+                    #         project_git("shortlog", "-s",
+                    #                     rev_range).splitlines()
+                    #     )
+                    "Samples":
+                        len(case_study.revisions)
+                }
+            }
+            # if revision:
+            #     cs_dict[project_name]["Revision"] = revision.short_hash
+
+            cs_data.append(pd.DataFrame.from_dict(cs_dict, orient="index"))
+
+        df = pd.concat(cs_data).sort_index()
+        if self.format in [
+            TableFormat.LATEX, TableFormat.LATEX_BOOKTABS, TableFormat.LATEX_RAW
+        ]:
+            table = df.to_latex(
+                bold_rows=True, multicolumn_format="c", multirow=True
+            )
+            return str(table) if table else ""
+        return tabulate(df, df.columns, self.format.value)
+
+    def wrap_table(self, table: str) -> str:
+        return wrap_table_in_document(table=table, landscape=True)
