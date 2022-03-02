83a5e4b43475041f7ed5dbea658c9603d5945adf
Florian Sattler <vuld3r@gmail.com>
2022-03-02 22:01:56 +0100

    Require new BB version

--- varats-core/setup.py
+++ varats-core/setup.py
@@ -10,7 +10,7 @@ setup(
     setup_requires=["pytest-runner", "setuptools_scm"],
     tests_require=["pytest", "pytest-cov"],
     install_requires=[
-        "benchbuild>=6.3.0",
+        "benchbuild>=6.3.1",
         "plumbum>=1.6.6",
         "PyGithub>=1.47",
         "PyDriller>=2.0",
