abff13412f0ac4ee6b1e4c824cbe6ec163c6136a
Florian Sattler <vuld3r@gmail.com>
2022-03-03 19:32:27 +0100

    Apply suggestions from code review
    Co-authored-by: Jonas Kaufmann <jokau03@gmail.com>

--- tests/utils/test_project_util.py
+++ tests/utils/test_project_util.py
@@ -234,7 +234,7 @@ class TestProjectBinaryWrapper(unittest.TestCase):
     """Test if we can correctly setup and use the RevisionBinaryMap."""
 
     def test_execution_of_executable(self) -> None:
-        """Check if we can execute a executable bianries."""
+        """Check if we can execute an executable binary."""
         binary = ProjectBinaryWrapper("ls", "/bin/ls", BinaryType.EXECUTABLE)
 
         ret = binary()
@@ -242,7 +242,7 @@ class TestProjectBinaryWrapper(unittest.TestCase):
         self.assertIsInstance(ret, str)
 
     def test_execution_of_libraries(self) -> None:
-        """Check if we don't fail when executing a shared/static library."""
+        """Check whether we fail when executing a shared/static library."""
         static_lib_binary = ProjectBinaryWrapper(
             "ls", "/bin/ls", BinaryType.STATIC_LIBRARY
         )
