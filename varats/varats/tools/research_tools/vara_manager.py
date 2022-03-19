97c164e9ac671b0d4342a302d543c1b8ccefc44c
Sinerum <s8sifrie@stud.uni-saarland.de>
2022-03-19 16:00:19 +0100

    move simple git command functions from vara-manager to new git_command module and refactor all to use plumbum

--- varats/varats/tools/research_tools/vara_manager.py
+++ varats/varats/tools/research_tools/vara_manager.py
@@ -20,6 +20,7 @@ from PyQt5.QtCore import QProcess
 
 from varats.utils.exceptions import ProcessTerminatedError
 from varats.utils.git_commands import fetch_remote
+from varats.utils.git_commands import fetch_remote
 from varats.utils.settings import vara_cfg
 
 LOG = logging.getLogger(__name__)
