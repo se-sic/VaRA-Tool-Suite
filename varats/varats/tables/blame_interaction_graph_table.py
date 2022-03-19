3b22b73ab1946d9e79710dc1d3c8d3380aa456d0
Simon Lichtnecker <cormensratio@posteo.de>
2022-03-19 15:14:22 +0100

    Adapts caig-metrics-table to click cli

--- varats/varats/tables/blame_interaction_graph_table.py
+++ varats/varats/tables/blame_interaction_graph_table.py
@@ -198,15 +198,6 @@ class CommitAuthorInteractionGraphMetricsTable(Table):
         super().__init__(self.NAME, table_config, **kwargs)
 
     def tabulate(self) -> str:
-        if "project" not in self.table_kwargs:
-            case_studies = get_loaded_paper_config().get_all_case_studies()
-        else:
-            if "table_case_study" in self.table_kwargs:
-                case_studies = [self.table_kwargs["table_case_study"]]
-            else:
-                case_studies = get_loaded_paper_config().get_case_studies(
-                    self.table_kwargs["project"]
-                )
 
         def create_graph(
             project_name: str, revision: FullCommitHash
@@ -215,10 +206,30 @@ class CommitAuthorInteractionGraphMetricsTable(Table):
                 project_name, revision
             ).commit_author_interaction_graph()
 
-        return _generate_graph_table(case_studies, create_graph, self.format)
+        return _generate_graph_table(
+            self.table_kwargs["case_study"], create_graph,
+            self.table_kwargs["format"]
+        )
 
     def wrap_table(self, table: str) -> str:
         return wrap_table_in_document(table=table, landscape=True)
+
+
+class CommitAuthorInteractionGraphMetricsTableGenerator(
+    TableGenerator,
+    generator_name="caig-metrics-table",
+    options=[
+        REQUIRE_MULTI_CASE_STUDY, OPTIONAL_REPORT_TYPE, OPTIONAL_TABLE_FORMAT
+    ]
+):
+    """Generates a caig-metrics table for the selected case study(ies)."""
+
+    def generate(self) -> tp.List[Table]:
+        return [
+            CommitAuthorInteractionGraphMetricsTable(
+                self.table_config, **self.table_kwargs
+            )
+        ]
 
 
 class AuthorBlameVsFileDegreesTable(Table):
