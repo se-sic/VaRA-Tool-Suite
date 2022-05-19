d4cf9776616875bcd4bb2cf0e75c69b2162fba55
Sinerum <s8sifrie@stud.uni-saarland.de>
2022-05-19 14:56:07 +0200

    make brotli compile function revision based and block early revisions that are not easily buildable

--- varats/varats/projects/c_projects/brotli.py
+++ varats/varats/projects/c_projects/brotli.py
@@ -2,7 +2,8 @@
 import typing as tp
 
 import benchbuild as bb
-from benchbuild.utils.cmd import mkdir, make
+from benchbuild.utils.cmd import mkdir, make, echo
+from benchbuild.utils.revision_ranges import RevisionRange, block_revisions
 from benchbuild.utils.settings import get_number_of_jobs
 from plumbum import local
 
@@ -16,7 +17,11 @@ from varats.project.project_util import (
     verify_binaries,
 )
 from varats.project.varats_project import VProject
-from varats.utils.git_util import ShortCommitHash, RevisionBinaryMap
+from varats.utils.git_util import (
+    ShortCommitHash,
+    RevisionBinaryMap,
+    get_all_revisions_between,
+)
 from varats.utils.settings import bb_cfg
 
 
@@ -28,13 +33,21 @@ class Brotli(VProject):
     DOMAIN = ProjectDomains.COMPRESSION
 
     SOURCE = [
-        PaperConfigSpecificGit(
-            project_name="brotli",
-            remote="https://github.com/google/brotli.git",
-            local="brotli_git",
-            refspec="origin/HEAD",
-            limit=None,
-            shallow=False
+        block_revisions([
+            RevisionRange(
+                '8f30907d0f2ef354c2b31bdee340c2b11dda0fb0',
+                '378485b097fd7b80a5e404a3cb912f7b18f78cdb',
+                "building not unified"
+            )
+        ])(
+            PaperConfigSpecificGit(
+                project_name="brotli",
+                remote="https://github.com/google/brotli.git",
+                local="brotli_git",
+                refspec="origin/HEAD",
+                limit=None,
+                shallow=False
+            )
         )
     ]
 
@@ -47,8 +60,21 @@ class Brotli(VProject):
     ) -> tp.List[ProjectBinaryWrapper]:
         binary_map = RevisionBinaryMap(get_local_project_git_path(Brotli.NAME))
 
-        binary_map.specify_binary("out/brotli", BinaryType.EXECUTABLE)
-
+        binary_map.specify_binary(
+            "out/brotli",
+            BinaryType.EXECUTABLE,
+            only_valid_in=RevisionRange(
+                "5814438791fb2d4394b46e5682a96b68cd092803", "HEAD"
+            )
+        )
+        binary_map.specify_binary(
+            "bin/bro",
+            BinaryType.EXECUTABLE,
+            only_valid_in=RevisionRange(
+                "f9ab24a7aaee93d5932ba212e5e3d32e4306f748",
+                "5814438791fb2d4394b46e5682a96b68cd092803"
+            )
+        )
         return binary_map[revision]
 
     def run_tests(self) -> None:
@@ -57,13 +83,25 @@ class Brotli(VProject):
     def compile(self) -> None:
         """Compile the project."""
         brotli_version_source = local.path(self.source_of_primary)
-
+        brotli_git_path = get_local_project_git_path(self.NAME)
+        brotli_version = ShortCommitHash(self.version_of_primary)
+        with local.cwd(brotli_git_path):
+            configure_revisions = get_all_revisions_between(
+                "f9ab24a7aaee93d5932ba212e5e3d32e4306f748",
+                "5814438791fb2d4394b46e5682a96b68cd092803", ShortCommitHash
+            )
         c_compiler = bb.compiler.cc(self)
-        mkdir(brotli_version_source / "out")
-        with local.cwd(brotli_version_source / "out"):
-            with local.env(CC=str(c_compiler)):
-                bb.watch(local["../configure-cmake"])()
-            bb.watch(make)("-j", get_number_of_jobs(bb_cfg()))
+        if brotli_version in configure_revisions:
+            with local.cwd(brotli_version_source):
+                with local.env(CC=str(c_compiler)):
+                    bb.watch(local["./configure"])()
+                bb.watch(make)("-j", get_number_of_jobs(bb_cfg()))
+        else:
+            mkdir(brotli_version_source / "out")
+            with local.cwd(brotli_version_source / "out"):
+                with local.env(CC=str(c_compiler)):
+                    bb.watch(local["../configure-cmake"])()
+                bb.watch(make)("-j", get_number_of_jobs(bb_cfg()))
 
         with local.cwd(brotli_version_source):
             verify_binaries(self)
