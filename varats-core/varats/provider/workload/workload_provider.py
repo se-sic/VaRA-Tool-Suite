6c65dda510ad4920889ab0ce6733c68fc5f993a7
Jonas Kaufmann <jokau03@gmail.com>
2022-07-10 23:04:47 +0200

    automatically fetch repository containing workload files

--- varats-core/varats/provider/workload/workload_provider.py
+++ varats-core/varats/provider/workload/workload_provider.py
@@ -7,7 +7,9 @@ TODO (se-sic/VaRA#841): replace with bb workloads if possible
 import typing as tp
 from pathlib import Path
 
+import benchbuild as bb
 from benchbuild.project import Project
+from benchbuild.source.base import target_prefix
 
 from varats.projects.c_projects.brotli import Brotli
 from varats.projects.c_projects.bzip2 import Bzip2
@@ -24,35 +26,70 @@ class WorkloadProvider(Provider):
     """Provider for a list of arguments to execute binaries in a project
     with."""
 
-    WORKLOADS_BASE_DIR = Path(
-        str(vara_cfg()["experiment"]["workloads_base_location"])
-    )
-    WORKLOADS = {
-        f"{FeaturePerfCSCollection.NAME},SimpleSleepLoop": [
-            "--iterations", 3 * 10**3, "--sleepms", 10
-        ],
-        f"{FeaturePerfCSCollection.NAME},SimpleBusyLoop": [
-            "--iterations", 3 * 10**5, "--count_to", 10**5
-        ],
-        f"{Xz.NAME},xz": [
-            "-k", "-f", "-9e", "--compress", "--threads=8", "--format=xz",
-            str(WORKLOADS_BASE_DIR / "compression/countries-land-1km.geo.json")
-        ],
-        f"{Brotli.NAME},brotli": [
-            "-f", "-o", "/tmp/brotli_compression_test.br",
-            str(WORKLOADS_BASE_DIR / "compression/countries-land-1km.geo.json")
-        ],
-        f"{Bzip2.NAME},bzip2": [
-            "--compress", "--best", "--verbose", "--keep", "--force",
-            str(WORKLOADS_BASE_DIR / "compression/countries-land-1m.geo.json")
-        ],
-        f"{Gzip.NAME},gzip": [
-            "--force", "--keep", "--name", "--recursive", "--verbose", "--best",
-            str(
-                WORKLOADS_BASE_DIR / "compression/countries-land-10km.geo.json"
-            )
-        ],
-    }
+    def __init__(self, project: tp.Type[Project]) -> None:
+        super().__init__(project)
+
+        workloads_source = bb.source.Git(
+            remote=(
+                "https://github.com/Kaufi-Jonas/"
+                "WorkloadsForConfigurableSystems.git"
+            ),
+            local="WorkloadsForConfigurableSystems",
+            refspec="origin/HEAD",
+            limit=1,
+        )
+        workloads_source.fetch()
+        self._workloads_base_dir = Path(
+            Path(target_prefix()) / workloads_source.local
+        )
+
+        self._workloads = {
+            f"{FeaturePerfCSCollection.NAME},SimpleSleepLoop": [
+                "--iterations", 3 * 10**3, "--sleepms", 10
+            ],
+            f"{FeaturePerfCSCollection.NAME},SimpleBusyLoop": [
+                "--iterations", 3 * 10**5, "--count_to", 10**5
+            ],
+            f"{Xz.NAME},xz": [
+                "-k", "-f", "-9e", "--compress", "--threads=0", "--format=xz",
+                "-vv",
+                str(
+                    self._workloads_base_dir /
+                    "compression/countries-land-250m.geo.json"
+                )
+            ],
+            f"{Brotli.NAME},brotli": [
+                "-f", "-o", "/tmp/brotli_compression_test.br",
+                str(
+                    self._workloads_base_dir /
+                    "compression/countries-land-1km.geo.json"
+                )
+            ],
+            f"{Bzip2.NAME},bzip2": [
+                "--compress", "--best", "--verbose", "--verbose", "--keep",
+                "--force",
+                str(
+                    self._workloads_base_dir /
+                    "compression/countries-land-1m.geo.json"
+                ),
+                str(
+                    self._workloads_base_dir /
+                    "compression/countries-land-10m.geo.json"
+                ),
+                str(
+                    self._workloads_base_dir /
+                    "compression/countries-land-100m.geo.json"
+                )
+            ],
+            f"{Gzip.NAME},gzip": [
+                "--force", "--keep", "--name", "--recursive", "--verbose",
+                "--best",
+                str(
+                    self._workloads_base_dir /
+                    "compression/countries-land-500m.geo.json"
+                )
+            ],
+        }
 
     @classmethod
     def create_provider_for_project(
@@ -85,4 +122,4 @@ class WorkloadProvider(Provider):
                                 binary_name: str) -> tp.Optional[tp.List[str]]:
         """Get a list of arguments to execute the given binary with."""
         key = f"{self.project.NAME},{binary_name}"
-        return self.WORKLOADS.get(key, None)
+        return self._workloads.get(key, None)
