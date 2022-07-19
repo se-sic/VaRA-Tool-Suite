266bd9f0188582cfe8a779ad97d36519b3d6fcb6
Nico Buchholz <s8nibuch@stud.uni-saarland.de>
2022-07-19 17:05:51 +0200

    Fix file formatting

--- varats/varats/projects/cpp_projects/fast_downward.py
+++ varats/varats/projects/cpp_projects/fast_downward.py
@@ -10,12 +10,22 @@ from plumbum import local
 from varats.paper_mgmt.paper_config import PaperConfigSpecificGit
 from varats.project.project_domain import ProjectDomains
 from varats.project.project_util import (
+    BinaryType,
     ProjectBinaryWrapper,
-    verify_binaries, get_local_project_git_path, BinaryType, get_tagged_commits,
+    get_local_project_git_path,
+    get_tagged_commits,
+    verify_binaries,
 )
 from varats.project.varats_project import VProject
-from varats.provider.release.release_provider import ReleaseProviderHook, ReleaseType
-from varats.utils.git_util import ShortCommitHash, RevisionBinaryMap, FullCommitHash
+from varats.provider.release.release_provider import (
+    ReleaseProviderHook,
+    ReleaseType,
+)
+from varats.utils.git_util import (
+    FullCommitHash,
+    RevisionBinaryMap,
+    ShortCommitHash,
+)
 from varats.utils.settings import bb_cfg
 
 
@@ -41,7 +51,9 @@ class FastDownward(VProject, ReleaseProviderHook):
     def binaries_for_revision(
         revision: ShortCommitHash
     ) -> tp.List[ProjectBinaryWrapper]:
-        binary_map = RevisionBinaryMap(get_local_project_git_path(FastDownward.NAME))
+        binary_map = RevisionBinaryMap(
+            get_local_project_git_path(FastDownward.NAME)
+        )
         binary_map.specify_binary('build/bin/downward', BinaryType.EXECUTABLE)
 
         return binary_map[revision]
@@ -62,10 +74,7 @@ class FastDownward(VProject, ReleaseProviderHook):
             with local.env(CC=str(c_compiler), CXX=str(cxx_compiler)):
                 bb.watch(cmake)("../src")
 
-            bb.watch(cmake)(
-                "--build", ".", "-j",
-                get_number_of_jobs(bb_cfg())
-            )
+            bb.watch(cmake)("--build", ".", "-j", get_number_of_jobs(bb_cfg()))
 
         with local.cwd(version_source):
             verify_binaries(self)
@@ -87,12 +96,30 @@ class FastDownward(VProject, ReleaseProviderHook):
             # Before 2019_07, there were no real releases, but the following
             # commits were identified as suitable.
             release_commits = {
-                (FullCommitHash('e4eb64c613ae34b97ab9409deac5331ec2ce5e43'), 'release-16.07.0'),
-                (FullCommitHash('91f44fa59ea57014a7769062a92aa752f503128e'), 'release-17.01.0'),
-                (FullCommitHash('363e2fc9a8b7adb48b4c30e929097798928c7370'), 'release-17.07.0'),
-                (FullCommitHash('0e8acd2f2613e032040dc6b6d4bf1926848aa4cb'), 'release-18.01.0'),
-                (FullCommitHash('dd7ddfeea72a699d381dce35657d683259be77c0'), 'release-18.07.0'),
-                (FullCommitHash('2cc2a66e8073a73571e0a37cd806380f13751a7c'), 'release-19.01.0')
+                (
+                    FullCommitHash('e4eb64c613ae34b97ab9409deac5331ec2ce5e43'),
+                    'release-16.07.0'
+                ),
+                (
+                    FullCommitHash('91f44fa59ea57014a7769062a92aa752f503128e'),
+                    'release-17.01.0'
+                ),
+                (
+                    FullCommitHash('363e2fc9a8b7adb48b4c30e929097798928c7370'),
+                    'release-17.07.0'
+                ),
+                (
+                    FullCommitHash('0e8acd2f2613e032040dc6b6d4bf1926848aa4cb'),
+                    'release-18.01.0'
+                ),
+                (
+                    FullCommitHash('dd7ddfeea72a699d381dce35657d683259be77c0'),
+                    'release-18.07.0'
+                ),
+                (
+                    FullCommitHash('2cc2a66e8073a73571e0a37cd806380f13751a7c'),
+                    'release-19.01.0'
+                )
             }
 
             release_commits = release_commits.union({
@@ -102,4 +129,3 @@ class FastDownward(VProject, ReleaseProviderHook):
             })
 
             return list(release_commits)
-
