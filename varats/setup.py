8a9376dad686b02532e65e160e100320c2259aeb
Florian Sattler <vuld3r@gmail.com>
2022-08-23 12:24:52 +0200

    Bump version to 11.1.5

--- varats/setup.py
+++ varats/setup.py
@@ -10,7 +10,7 @@ with open(base_dir + '/README.md') as f:
 
 setup(
     name='varats',
-    version='11.1.4',
+    version='11.1.5',
     url='https://github.com/se-sic/vara-tool-suite',
     packages=find_namespace_packages(include=['varats.*']),
     namespace_packages=["varats"],
@@ -47,7 +47,7 @@ setup(
         "seaborn>=0.8.0",
         "statsmodels~=0.13.1",
         "tabulate>=0.8.6",
-        "varats-core>=11.1.4",
+        "varats-core>=11.1.5",
         "wllvm>=1.1.4",
     ],
     author="Florian Sattler",
