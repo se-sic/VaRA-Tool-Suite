3ee4bc742fa8d23a0f869ceaa5ac29caff5668c1
Florian Sattler <vuld3r@gmail.com>
2022-09-15 21:14:47 +0200

    Removes old test experiment step

--- varats/varats/experiments/base/time_workloads.py
+++ varats/varats/experiments/base/time_workloads.py
@@ -34,73 +34,11 @@ from varats.report.gnu_time_report import TimeReport, TimeReportAggregate
 from varats.report.report import ReportSpecification
 
 
-class TimeProjectWorkloads(actions.Step):  # type: ignore
-    """Times the execution time of all project workloads."""
-
-    NAME = "TimeAnalysis"
-    DESCRIPTION = "Time the execution of all produced binaries."
-
-    def __init__(self, project: Project, experiment_handle: ExperimentHandle):
-        super().__init__(obj=project, action_fn=self.analyze)
-        self.__experiment_handle = experiment_handle
-
-    def analyze(self) -> actions.StepResult:
-        """Only create a report file."""
-        project = self.obj
-        repetitions = 10
-
-        vara_result_folder = get_varats_result_folder(project)
-
-        for binary in project.binaries:
-            result_file = create_new_success_result_filepath(
-                self.__experiment_handle,
-                self.__experiment_handle.report_spec().main_report, project,
-                binary
-            )
-
-            report_file = f"{vara_result_folder}/{result_file}"
-
-            with local.cwd(local.path(project.source_of_primary)):
-                workload = "countries-land-1km.geo.json"
-
-                with ZippedReportFolder(Path(report_file)) as time_reports_dir:
-                    for i in range(repetitions):
-                        wget(
-                            "https://github.com/simonepri/geo-maps/releases/"
-                            "download/v0.6.0/countries-land-1km.geo.json", "-O",
-                            workload
-                        )
-                        rm('-f', workload + ".xz")
-                        rm('-f', workload + ".lrz")
-                        rm('-f', workload + ".gz")
-
-                        # Start actual experiment code/measurement code
-                        run_report_name = Path(
-                            time_reports_dir
-                        ) / f"time_report_{i}.txt"
-
-                        run_cmd = time['-v', '-o', f'{run_report_name}',
-                                       binary[workload]]
-
-                        print(f"Run-{i}: ({run_cmd})")
-
-                        exec_func_with_pe_error_handler(
-                            run_cmd,
-                            create_default_analysis_failure_handler(
-                                self.__experiment_handle, project,
-                                self.__experiment_handle.report_spec().
-                                main_report, Path(vara_result_folder)
-                            )
-                        )
-
-        return actions.StepResult.OK
-
-
 class TimeProjectWorkloadsNew(actions.ProjectStep):  # type: ignore
-    """Times the execution time of all project workloads."""
+    """Times the execution of all project workloads."""
 
-    NAME = "TimeAnalysis"
-    DESCRIPTION = "Time the execution of all produced binaries."
+    NAME = "TimeWorkloads"
+    DESCRIPTION = "Time the execution of all project workloads."
 
     def __init__(
         self, project: Project, experiment_handle: ExperimentHandle, num: int
