c977a436be545cdb474438037188fd8da4823ff7
Aufrichtig98 <manuelmesserig@gmail.com>
2022-09-26 20:27:03 +0200

    Changed Time Report aggregate

--- varats-core/varats/tefReportTestLocal/main.py
+++ varats-core/varats/tefReportTestLocal/main.py
@@ -1,12 +1,11 @@
 from varats.report.tef_report import TEFReport
 from varats.report.tef_report import TEFReportAggregate
+from varats.report.gnu_time_report import TimeReport, TimeReportAggregate
 import json
 import typing as tp
 from enum import Enum
 from pathlib import Path
 
 # Press the green button in the gutter to run the script.
-tef_report_aggregate = TEFReportAggregate(Path("/home/aufrichtig/bachelor/XZCompressionLevel0.zip"))
-tef_report_aggregate.wall_clock_times
-print("test")
-
+blackbox_test = TimeReportAggregate(Path("/home/aufrichtig/bachelor") / Path("XZCompressionLevel0.zip"))
+print(blackbox_test.summary)
