6998eb8809710187e1bb5101ce911c2fe57a4b1e
Florian Sattler <vuld3r@gmail.com>
2022-09-29 15:42:35 +0200

    Adds workload support to varats (#667)
    Adds initial workload implementation and example code that demonstrates how to handle workloads in experiments/plots/tables.
    
    resolves se-sic/VaRA#841

--- varats/varats/experiments/base/just_compile.py
+++ varats/varats/experiments/base/just_compile.py
@@ -1,7 +1,6 @@
 """Implements an empty experiment that just compiles the project."""
 
 import typing as tp
-from pathlib import Path
 
 from benchbuild import Project
 from benchbuild.extensions import compiler, run, time
@@ -15,33 +14,35 @@ from varats.experiment.experiment_util import (
     exec_func_with_pe_error_handler,
     get_default_compile_error_wrapped,
     create_default_analysis_failure_handler,
-    get_varats_result_folder,
     create_new_success_result_filepath,
 )
 from varats.experiment.wllvm import RunWLLVM
+from varats.project.varats_project import VProject
 from varats.report.report import ReportSpecification
 
 
 # Please take care when changing this file, see docs experiments/just_compile
-class EmptyAnalysis(actions.Step):  # type: ignore
+class EmptyAnalysis(actions.ProjectStep):  # type: ignore
     """Empty analysis step for testing."""
 
     NAME = "EmptyAnalysis"
     DESCRIPTION = "Analyses nothing."
 
+    project: VProject
+
     def __init__(self, project: Project, experiment_handle: ExperimentHandle):
-        super().__init__(obj=project, action_fn=self.analyze)
+        super().__init__(project=project)
         self.__experiment_handle = experiment_handle
+
+    def __call__(self) -> actions.StepResult:
+        return self.analyze()
 
     def analyze(self) -> actions.StepResult:
         """Only create a report file."""
-        if not self.obj:
-            return actions.StepResult.ERROR
-        project = self.obj
 
-        for binary in project.binaries:
+        for binary in self.project.binaries:
             result_file = create_new_success_result_filepath(
-                self.__experiment_handle, EmptyReport, project, binary
+                self.__experiment_handle, EmptyReport, self.project, binary
             )
 
             run_cmd = touch[f"{result_file}"]
@@ -49,7 +50,7 @@ class EmptyAnalysis(actions.Step):  # type: ignore
             exec_func_with_pe_error_handler(
                 run_cmd,
                 create_default_analysis_failure_handler(
-                    self.__experiment_handle, project, EmptyReport
+                    self.__experiment_handle, self.project, EmptyReport
                 )
             )
 
--- varats/varats/experiments/vara/instrumentation_point_printer.py
+++ varats/varats/experiments/vara/instrumentation_point_printer.py
@@ -1,7 +1,6 @@
 """Implements experiment for VaRA's InstrumentationPointPrinter utility pass."""
 
 import typing as tp
-from pathlib import Path
 
 from benchbuild import Project
 from benchbuild.extensions import compiler, run, time
@@ -24,6 +23,7 @@ from varats.experiment.wllvm import (
     RunWLLVM,
     get_cached_bc_file_path,
 )
+from varats.project.varats_project import VProject
 from varats.provider.feature.feature_model_provider import (
     FeatureModelNotFound,
     FeatureModelProvider,
@@ -31,7 +31,7 @@ from varats.provider.feature.feature_model_provider import (
 from varats.report.report import ReportSpecification
 
 
-class CollectInstrumentationPoints(actions.Step):  # type: ignore
+class CollectInstrumentationPoints(actions.ProjectStep):  # type: ignore
     """Runs utility pass on LLVM-IR to extract instrumentation point
     information."""
 
@@ -39,20 +39,23 @@ class CollectInstrumentationPoints(actions.Step):  # type: ignore
     DESCRIPTION = "Runs utility pass on LLVM-IR to extract instrumentation " \
         "point information."
 
+    project: VProject
+
     def __init__(self, project: Project, experiment_handle: ExperimentHandle):
-        super().__init__(obj=project, action_fn=self.analyze)
+        super().__init__(project=project)
         self.__experiment_handle = experiment_handle
+
+    def __call__(self) -> actions.StepResult:
+        return self.analyze()
 
     def analyze(self) -> actions.StepResult:
         """Run VaRA-IPP utility pass and extract instrumentation point
         information."""
-        project = self.obj
+        vara_result_folder = get_varats_result_folder(self.project)
 
-        vara_result_folder = get_varats_result_folder(project)
-
-        for binary in project.binaries:
+        for binary in self.project.binaries:
             result_file = create_new_success_result_filepath(
-                self.__experiment_handle, VaraIPPReport, project, binary
+                self.__experiment_handle, VaraIPPReport, self.project, binary
             )
 
             # Need the following passes:
@@ -61,7 +64,7 @@ class CollectInstrumentationPoints(actions.Step):  # type: ignore
             opt_params = [
                 "--vara-PTFDD", "-vara-IPP", "-o", "/dev/null",
                 get_cached_bc_file_path(
-                    project, binary,
+                    self.project, binary,
                     [BCFileExtensions.DEBUG, BCFileExtensions.FEATURE]
                 )
             ]
@@ -74,7 +77,7 @@ class CollectInstrumentationPoints(actions.Step):  # type: ignore
             exec_func_with_pe_error_handler(
                 run_cmd,
                 create_default_analysis_failure_handler(
-                    self.__experiment_handle, project, VaraIPPReport
+                    self.__experiment_handle, self.project, VaraIPPReport
                 )
             )
 
