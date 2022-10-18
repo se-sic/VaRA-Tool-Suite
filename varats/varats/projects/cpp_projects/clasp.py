48e3ee517186a93119425a945940ed955a1654dd
Nico Buchholz <s8nibuch@stud.uni-saarland.de>
2022-10-18 18:39:28 +0200

    Added clasp and z3

--- varats/varats/projects/cpp_projects/clasp.py
+++ varats/varats/projects/cpp_projects/clasp.py
@@ -1,8 +1,8 @@
-"""Project file for Hypre."""
+"""Project file for clasp."""
 import typing as tp
 
 import benchbuild as bb
-from benchbuild.utils.cmd import cmake
+from benchbuild.utils.cmd import cmake, mkdir
 from benchbuild.utils.settings import get_number_of_jobs
 from plumbum import local
 
@@ -29,22 +29,21 @@ from varats.utils.settings import bb_cfg
 
 class Hypre(VProject, ReleaseProviderHook):
     """
-    HYPRE is a library of high performance preconditioners and solvers featuring
-    multigrid methods for the solution of large, sparse linear systems of
-    equations on massively parallel computers.
+    clasp is an answer set solver for (extended) normal and disjunctive logic
+    programs.
 
     (fetched by Git)
     """
 
-    NAME = 'hypre'
+    NAME = 'clasp'
     GROUP = 'cpp_projects'
     DOMAIN = ProjectDomains.SOLVER
 
     SOURCE = [
         PaperConfigSpecificGit(
-            project_name="hypre",
-            remote="https://github.com/hypre-space/hypre.git",
-            local="hypre",
+            project_name="clasp",
+            remote="https://github.com/potassco/clasp.git",
+            local="clasp",
             refspec="origin/HEAD",
             limit=None,
             shallow=False
@@ -56,7 +55,7 @@ class Hypre(VProject, ReleaseProviderHook):
         revision: ShortCommitHash
     ) -> tp.List[ProjectBinaryWrapper]:
         binary_map = RevisionBinaryMap(get_local_project_git_path(Hypre.NAME))
-        binary_map.specify_binary('src/cmbuild/test/ij', BinaryType.EXECUTABLE)
+        binary_map.specify_binary('build/bin/clasp', BinaryType.EXECUTABLE)
 
         return binary_map[revision]
 
@@ -65,17 +64,17 @@ class Hypre(VProject, ReleaseProviderHook):
 
     def compile(self) -> None:
         """Compile the project."""
-        hypre_source = local.path(self.source_of(self.primary_source))
+        clasp_source = local.path(self.source_of(self.primary_source))
 
         c_compiler = bb.compiler.cc(self)
         cxx_compiler = bb.compiler.cxx(self)
 
-        with local.cwd(hypre_source / "src" / "cmbuild"):
+        mkdir("-p", clasp_source / "build")
+
+        with local.cwd(clasp_source / "build"):
             with local.env(CC=str(c_compiler), CXX=str(cxx_compiler)):
-                bb.watch(cmake)(
-                    "-DHYPRE_WITH_MPI=OFF", "-DHYPRE_BUILD_TESTS=ON", ".."
-                )
+                bb.watch(cmake)("../")
 
             bb.watch(cmake)("--build", ".", "-j", get_number_of_jobs(bb_cfg()))
-        with local.cwd(hypre_source):
+        with local.cwd(clasp_source):
             verify_binaries(self)
--- varats/varats/projects/cpp_projects/z3.py
+++ varats/varats/projects/cpp_projects/z3.py
@@ -1,8 +1,8 @@
-"""Project file for Hypre."""
+"""Project file for Z3."""
 import typing as tp
 
 import benchbuild as bb
-from benchbuild.utils.cmd import cmake
+from benchbuild.utils.cmd import cmake, mkdir
 from benchbuild.utils.settings import get_number_of_jobs
 from plumbum import local
 
@@ -15,36 +15,27 @@ from varats.project.project_util import (
     verify_binaries,
 )
 from varats.project.varats_project import VProject
-from varats.provider.release.release_provider import (
-    ReleaseProviderHook,
-    ReleaseType,
-)
-from varats.utils.git_util import (
-    RevisionBinaryMap,
-    ShortCommitHash,
-    FullCommitHash,
-)
+from varats.provider.release.release_provider import ReleaseProviderHook
+from varats.utils.git_util import RevisionBinaryMap, ShortCommitHash
 from varats.utils.settings import bb_cfg
 
 
-class Hypre(VProject, ReleaseProviderHook):
+class Z3(VProject, ReleaseProviderHook):
     """
-    HYPRE is a library of high performance preconditioners and solvers featuring
-    multigrid methods for the solution of large, sparse linear systems of
-    equations on massively parallel computers.
+    Z3 is a theorem prover from Microsoft Research.
 
     (fetched by Git)
     """
 
-    NAME = 'hypre'
+    NAME = 'z3'
     GROUP = 'cpp_projects'
     DOMAIN = ProjectDomains.SOLVER
 
     SOURCE = [
         PaperConfigSpecificGit(
-            project_name="hypre",
-            remote="https://github.com/hypre-space/hypre.git",
-            local="hypre",
+            project_name="z3",
+            remote="https://github.com/Z3Prover/z3.git",
+            local="z3",
             refspec="origin/HEAD",
             limit=None,
             shallow=False
@@ -55,8 +46,8 @@ class Hypre(VProject, ReleaseProviderHook):
     def binaries_for_revision(
         revision: ShortCommitHash
     ) -> tp.List[ProjectBinaryWrapper]:
-        binary_map = RevisionBinaryMap(get_local_project_git_path(Hypre.NAME))
-        binary_map.specify_binary('src/cmbuild/test/ij', BinaryType.EXECUTABLE)
+        binary_map = RevisionBinaryMap(get_local_project_git_path(Z3.NAME))
+        binary_map.specify_binary('build/z3', BinaryType.EXECUTABLE)
 
         return binary_map[revision]
 
@@ -65,17 +56,17 @@ class Hypre(VProject, ReleaseProviderHook):
 
     def compile(self) -> None:
         """Compile the project."""
-        hypre_source = local.path(self.source_of(self.primary_source))
+        z3_source = local.path(self.source_of(self.primary_source))
 
         c_compiler = bb.compiler.cc(self)
         cxx_compiler = bb.compiler.cxx(self)
 
-        with local.cwd(hypre_source / "src" / "cmbuild"):
+        mkdir("-p", z3_source / "build")
+
+        with local.cwd(z3_source / "build"):
             with local.env(CC=str(c_compiler), CXX=str(cxx_compiler)):
-                bb.watch(cmake)(
-                    "-DHYPRE_WITH_MPI=OFF", "-DHYPRE_BUILD_TESTS=ON", ".."
-                )
+                bb.watch(cmake)("-G", "Unix Makefiles", "../")
 
             bb.watch(cmake)("--build", ".", "-j", get_number_of_jobs(bb_cfg()))
-        with local.cwd(hypre_source):
+        with local.cwd(z3_source):
             verify_binaries(self)
