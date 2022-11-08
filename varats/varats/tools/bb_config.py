a33fb2f5087a94baa3a1438811e63693890a3486
Keller-1998 <s8jkkell@stud.uni-saarland.de>
2022-11-08 11:23:49 +0100

    project added to bb_config.py

--- varats/varats/tools/bb_config.py
+++ varats/varats/tools/bb_config.py
@@ -84,6 +84,7 @@ def create_new_bb_config(
         'varats.projects.cpp_projects.mongodb',
         'varats.projects.cpp_projects.poppler',
         'varats.projects.cpp_projects.z3',
+        'varats.projects.cpp_projects.ect'
     ]
     projects_conf.value[:] += [
         'varats.projects.cpp_projects.doxygen', 'varats.projects.cpp_projects'
