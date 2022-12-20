f082871f9012cb2493971dc9c975cd6d4c965786
Florian Sattler <vuld3r@gmail.com>
2022-12-20 14:36:12 +0100

    Ensure that the verifier checks all regions, even very small ones

--- varats/varats/experiments/vara/instrumentation_verifier.py
+++ varats/varats/experiments/vara/instrumentation_verifier.py
@@ -40,6 +40,9 @@ class RunInstrVerifier(FeatureExperiment, shorthand="RIV"):
 
         project.cflags += self.get_vara_tracing_cflags(instr_type, True)
 
+        # Ensure that we detect all regions, when verifying
+        project.cflags += ["-fvara-instruction-threshold=0"]
+
         project.ldflags += self.get_vara_tracing_ldflags()
 
         # Add the required runtime extensions to the project(s).
