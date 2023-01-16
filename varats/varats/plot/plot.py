88aa7c469529f7ae5ba270c87fe43ef0c9811cfc
Jan Schmitz <44864658+danjujan@users.noreply.github.com>
2023-01-16 16:37:04 +0100

    fix plot_file_name when using REQUIRE_MULTI_CASE_STUDY

--- varats/varats/plot/plot.py
+++ varats/varats/plot/plot.py
@@ -149,8 +149,17 @@ class Plot:
         """
         plot_ident = ''
         if 'case_study' in self.plot_kwargs:
-            case_study: 'CaseStudy' = self.plot_kwargs['case_study']
-            plot_ident = f"{case_study.project_name}_{case_study.version}_"
+            if isinstance(self.plot_kwargs['case_study'], list):
+                case_studies: tp.List[CaseStudy] = self.plot_kwargs['case_study'
+                                                                   ]
+                plot_ident += "-".join([
+                    f"{case_study.project_name}_{case_study.version}"
+                    for case_study in case_studies
+                ])
+                plot_ident += "_"
+            else:
+                case_study: CaseStudy = self.plot_kwargs['case_study']
+                plot_ident = f"{case_study.project_name}_{case_study.version}_"
         elif 'project' in self.plot_kwargs:
             plot_ident = f"{self.plot_kwargs['project']}_"
 
