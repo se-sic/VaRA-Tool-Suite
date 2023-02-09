64642d464988c60e984532c40bec59b8dcc80c5f
Florian Sattler <vuld3r@gmail.com>
2023-02-09 14:29:24 +0100

    Apply suggestions from code review

--- varats/varats/tools/bb_config.py
+++ varats/varats/tools/bb_config.py
@@ -127,10 +127,6 @@ def create_new_bb_config(
         'varats.experiments.vara.marker_tester',
         'varats.experiments.vara.phasar_fta',
         'varats.experiments.vara.feature_region_verifier_experiment',
-        'varats.experiments.phasar.ide_linear_constant_experiment',
-        'varats.experiments.phasar.global_analysis_compare',
-        'varats.experiments.szz.szz_unleashed_experiment',
-        'varats.experiments.szz.pydriller_szz_experiment',
     ]
     # yapf: enable
 
