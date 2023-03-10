d9e2696f5ad4196d35d1e7ec813fa85ef2475db9
Sebastian Böhm <boehmseb@cs.uni-saarland.de>
2023-03-10 17:22:51 +0100

    Debug broken test

--- tests/tools/test_driver_run.py
+++ tests/tools/test_driver_run.py
@@ -82,6 +82,8 @@ class TestDriverRun(unittest.TestCase):
         result = runner.invoke(
             driver_run.main, ["--slurm", "--container", "-E", "JustCompile"]
         )
+        print(result.stdout)
+        print(result.stderr)
         self.assertEqual(0, result.exit_code, result.exception)
         self.assertTrue(Path(str(bb_cfg()["slurm"]["template"])).exists())
         self.assertTrue(
