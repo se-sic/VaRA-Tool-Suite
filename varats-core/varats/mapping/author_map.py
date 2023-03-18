296c88429e898dad821e1f3ba397417e63f2de13
Simon Friedel <s8sifrie@uni-saarland.de>
2023-03-18 13:18:23 +0100

    implement a crude version of an author map

--- varats-core/varats/mapping/author_map.py
+++ varats-core/varats/mapping/author_map.py
@@ -30,33 +30,10 @@ class AmbiguousAuthor(Exception):
 name_regex = re.compile("\s*\d+\t(.*) <(.*)>")
 
 
-def generate_Author_List(path):
-    with local.cwd(path):
-        author_list: tp.List[Author] = []
-        test = git["shortlog", "-sne", "--all"]().strip().split("\n")
-        for line in test:
-            match = name_regex.match(line)
-            if not match:
-                LOG.info("Invalid author format.")
-                continue
-            name = match.group(1)
-            email = match.group(2)
-            id = name + email
-            author = Author(id, name, email)
-            author_list.append(author)
-
-        return author_list
-
-
-class AuthorMap():
-    """Provides a mapping of an author to all combinations of author name to
-    mail address."""
-
-
 class Author():
     """Representation of one author."""
 
-    def __init__(self, id, name, email):
+    def __init__(self, id: int, name: str, email: str):
         self.id = id
         self.name = name
         self.mail = email
@@ -66,12 +43,82 @@ class Author():
             return self.id == other.id
         return False
 
+    def __str__(self):
+        return f"{self.id} {self.name} <{self.mail}>"
+
+    def __repr__(self):
+        return f"{self.id} {self.name} <{self.mail}>"
+
     def merge_if_equal(self, other):
         if isinstance(other, Author):
             if self.__eq__(self, other):
                 print("hi")
 
 
+class AuthorMap():
+    """Provides a mapping of an author to all combinations of author name to
+    mail address."""
+
+    def __init__(self):
+        self.current_id = 0
+        self.mail_dict: tp.Dict[str, Author] = {}
+        self.name_dict: tp.Dict[str, Author] = {}
+
+    def get_author_by_name(self, name: str):
+        return self.name_dict[name]
+
+    def get_auhtor_by_email(self, email: str):
+        return self.mail_dict[email]
+
+    def get_author(self, name: str, mail: str):
+        if self.mail_dict[mail] == self.name_dict[name]:
+            return self.name_dict[name]
+        else:
+            raise AmbiguousAuthor
+
+    def add_entry(self, name: str, mail: str):
+        author_by_name = self.name_dict.get(name, None)
+        author_by_mail = self.mail_dict.get(mail, None)
+        if author_by_name:
+            if author_by_mail:
+                if author_by_name != author_by_mail:
+                    self.name_dict[name] = author_by_mail
+                    self.mail_dict[author_by_name.mail] = author_by_mail
+                else:
+                    return
+            else:
+                self.mail_dict[mail] = author_by_name
+        elif author_by_mail:
+            self.name_dict[name] = author_by_mail
+        else:
+            new_author = Author(self.current_id, name, mail)
+            self.current_id += 1
+            self.name_dict[name] = new_author
+            self.mail_dict[mail] = new_author
+
+    def __repr__(self):
+        return f"{self.name_dict} \n {self.mail_dict}"
+
+
+def generate_author_map(path: Path) -> AuthorMap:
+    """Generate an AuthorMap for the repository at the given path."""
+    with local.cwd(path):
+        author_map = AuthorMap()
+        test = git["shortlog", "-sne", "--all"]().strip().split("\n")
+        for line in test:
+            match = name_regex.match(line)
+            if not match:
+                LOG.info("Invalid author format.")
+                continue
+            name = match.group(1)
+            email = match.group(2)
+            author_map.add_entry(name, email)
+
+        return author_map
+
+
 if __name__ == '__main__':
-    a = generate_Author_List("/local/storage/hechtl/Vara/VaRA-Tool-Suite")
-    print("hi")
+    a = generate_author_map(
+        "/home/simon/Documents/Vara/vara-ts/VaRA-Tool-Suite"
+    )
+    print(a)
