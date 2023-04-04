921617c5c70463bb222c8535afbaf66cbda5231f
Jonas Kaufmann <jokau03@gmail.com>
2023-04-04 23:25:15 +0200

    update benchbuild to 6.6.7

--- varats-core/setup.py
+++ varats-core/setup.py
@@ -10,7 +10,7 @@ setup(
     setup_requires=["pytest-runner", "setuptools_scm"],
     tests_require=["pytest", "pytest-cov"],
     install_requires=[
-        "benchbuild>=6.6.4",
+        "benchbuild>=6.6.7",
         "ijson>=3.1.4",
         "plumbum>=1.6",
         "PyGithub>=1.58",
--- varats/setup.py
+++ varats/setup.py
@@ -17,7 +17,7 @@ setup(
     setup_requires=["pytest-runner", "setuptools_scm"],
     tests_require=["pytest", "pytest-cov"],
     install_requires=[
-        "benchbuild>=6.6.4",
+        "benchbuild>=6.6.7",
         "click>=8.1.3",
         "distro>=1.5.0",
         "graphviz>=0.14.2",
