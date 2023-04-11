9722976911c2f4cb8d911d217683f3468c63b7fe
Simon Rüdiger Steuer <s8sisteu@stud.uni-saarland.de>
2023-04-11 18:27:45 +0200

    implement FeatureBlameReportExperiment

---
+++ varats/varats/data/reports/feature_blame_report.py
@@ -0,0 +1,228 @@
+"""Module for FeatureBlameReport."""
+
+import re
+import typing as tp
+from pathlib import Path
+
+import yaml
+
+from varats.base.version_header import VersionHeader
+from varats.report.report import BaseReport
+
+
+class FeatureCommitRegion():
+    """Data containing feature and commit regions."""
+    def __init__(
+        self, feature: str, commit_hash: str, repo: str
+    ) -> None:
+        self.__feature = feature
+        self.__commit_hash = commit_hash
+        self.__repo = repo
+
+    @staticmethod
+    def create_feature_commit_region(
+        raw_region_data: tp.Dict[str, tp.Any]
+    ) -> 'FeatureCommitRegion':
+        feature = raw_region_data["feature"]
+        commit_hash = raw_region_data["commit-hash"]
+        repo = raw_region_data["repo"]
+        return FeatureCommitRegion(feature, commit_hash, repo)
+
+class FeatureCommitRegionInstruction():
+    """An instruction that has one or more feature-commit interaction and its location
+    in the project."""
+
+    def __init__(
+        self, instruction: str, location: str, feature_commit_regions: tp.List[str]
+    ) -> None:
+        self.__instruction = instruction
+        self.__location = location
+        self.__feature_commit_regions = sorted(feature_commit_regions)
+         
+
+    @staticmethod
+    def create_feature_tainted_instruction(
+        raw_inst_entry: tp.Dict[str, tp.Any]
+    ) -> 'FeatureCommitRegionInstruction':
+        """Creates a `FeatureCommitRegionInstruction` entry from the corresponding
+        yaml document section."""
+        instruction = str(raw_inst_entry['inst'])
+        location = str(raw_inst_entry['location'])
+        feature_commit_regions: tp.List[str] = []
+        for fcr in raw_inst_entry['regions']:
+            feature_commit_regions.append(str(fcr))
+        return FeatureCommitRegionInstruction(instruction, location, feature_commit_regions)
+
+
+    @property
+    def instruction(self) -> str:
+        """instruction containg commit and feature regions."""
+        return self.__instruction
+
+    @property
+    def location(self) -> str:
+        """Location of instruction in the project."""
+        return self.__location
+
+    @property
+    def feature_taints(self) -> tp.List[str]:
+        """List of commit and feature regions of this instruction."""
+        return self.__feature_commit_regions
+
+    def is_terminator(self) -> bool:
+        br_regex = re.compile(r'(br( i1 | label ))|(switch i\d{1,} )')
+        return br_regex.search(self.__instruction) is not None
+
+
+class FeatureBlameResultFunctionEntry():
+    """Collection of all feature commit region instructions for a specific
+    function."""
+
+    def __init__(
+        self, name: str, demangled_name: str,
+        feature_commit_region_insts: tp.List[FeatureCommitRegionInstruction]
+    ) -> None:
+        self.__name = name
+        self.__demangled_name = demangled_name
+        self.__feature_commit_region_insts = feature_commit_region_insts
+
+
+    @staticmethod
+    def create_feature_analysis_result_function_entry(
+        name: str, raw_function_entry: tp.Dict[str, tp.Any]
+    ) -> 'FeatureBlameResultFunctionEntry':
+        """Creates a `FeatureBlameResultFunctionEntry` from the corresponding
+        yaml document section."""
+        demangled_name = str(raw_function_entry['demangled-name'])
+        inst_list: tp.List[FeatureCommitRegionInstruction] = []
+        for raw_inst_entry in raw_function_entry['commit-feature-interaction-related-insts']:
+            inst_list.append(
+                FeatureCommitRegionInstruction.
+                create_feature_tainted_instruction(raw_inst_entry)
+            )
+        return FeatureBlameResultFunctionEntry(
+            name, demangled_name, inst_list
+        )
+    
+
+    @property
+    def name(self) -> str:
+        """
+        Name of the function.
+
+        The name is mangled for C++ code, either with the itanium or windows
+        mangling schema.
+        """
+        return self.__name
+
+    @property
+    def demangled_name(self) -> str:
+        """Demangled name of the function."""
+        return self.__demangled_name
+
+    @property
+    def feature_commit_region_insts(self) -> tp.List[FeatureCommitRegionInstruction]:
+        """List of found feature commit region instructions."""
+        return self.__feature_commit_region_insts
+
+
+class FeatureBlameReportMetaData():
+    """Provides extra meta data about llvm::Module, which was analyzed to
+    generate this ``FeatureBlameReport``."""
+
+    def __init__(
+        self, num_functions: int, num_instructions: int,
+        num_br_switch_insts: int
+    ) -> None:
+        self.__number_of_functions_in_module = num_functions
+        self.__number_of_instructions_in_module = num_instructions
+        self.__number_of_branch_and_switch_ints_in_module = num_br_switch_insts
+
+    @property
+    def num_functions(self) -> int:
+        """Number of functions in the analyzed llvm::Module."""
+        return self.__number_of_functions_in_module
+
+    @property
+    def num_instructions(self) -> int:
+        """Number of instructions processed in the analyzed llvm::Module."""
+        return self.__number_of_instructions_in_module
+
+    @property
+    def num_br_switch_insts(self) -> int:
+        """Number of branch and switch instructions processed in the analyzed
+        llvm::Module."""
+        return self.__number_of_branch_and_switch_ints_in_module
+
+    @staticmethod
+    def create_feature_analysis_report_meta_data(
+        raw_document: tp.Dict[str, tp.Any]
+    ) -> 'FeatureBlameReportMetaData':
+        """Creates `FeatureBlameReportMetaData` from the corresponding yaml
+        document."""
+        num_functions = int(raw_document['funcs-in-module'])
+        num_instructions = int(raw_document['insts-in-module'])
+        num_br_switch_insts = int(raw_document['br-switch-insts-in-module'])
+
+        return FeatureBlameReportMetaData(
+            num_functions, num_instructions, num_br_switch_insts
+        )
+
+class FeatureBlameReport(BaseReport, shorthand="FBR", file_type="yaml"):
+    """Data class that gives access to a loaded feature blame report."""
+
+    def __init__(self, path: Path) -> None:
+        super().__init__(path)
+
+        with open(path, 'r') as stream:
+            documents = yaml.load_all(stream, Loader=yaml.CLoader)
+            version_header = VersionHeader(next(documents))
+            version_header.raise_if_not_type("FeatureBlameReport")
+            version_header.raise_if_version_is_less_than(1)
+
+            self.__meta_data = FeatureBlameReportMetaData \
+                .create_feature_analysis_report_meta_data(next(documents))
+            
+            self.__function_entries: tp.Dict[
+                str, FeatureBlameResultFunctionEntry] = {}
+            raw_feature_analysis_report = next(documents)
+            for raw_func_entry in raw_feature_analysis_report['result-map']:
+                new_function_entry = (
+                    FeatureBlameResultFunctionEntry.
+                    create_feature_analysis_result_function_entry(
+                        raw_func_entry,
+                        raw_feature_analysis_report['result-map']
+                        [raw_func_entry]
+                    )
+                )
+                self.__function_entries[new_function_entry.name
+                                       ] = new_function_entry
+                
+    @property
+    def meta_data(self) -> FeatureBlameReportMetaData:
+        """Access the meta data that was gathered with the
+        ``FeatureBlameReport``."""
+        return self.__meta_data
+    
+    @property
+    def function_entries(
+        self
+    ) -> tp.ValuesView[FeatureBlameResultFunctionEntry]:
+        """Iterate over all function entries."""
+        return self.__function_entries.values()
+    
+    def get_feature_analysis_result_function_entry(
+        self, mangled_function_name: str
+    ) -> FeatureBlameResultFunctionEntry:
+        """
+        Get the result entry for a specific function.
+
+        Args:
+            mangled_function_name: mangled name of the function to look up
+        """
+        return self.__function_entries[mangled_function_name]
+
+    def get_feature_locations_dict(self) -> tp.Dict[str, tp.Set[str]]:
+        """Returns a dictionary that maps a feature name to a list of all
+        locations of tainted br and switch instructions."""
+        
---
+++ varats/varats/experiments/vara/feature_blame_report_experiment.py
@@ -0,0 +1,138 @@
+"""
+Implements the basic feature blame report experiment.
+
+The experiment analyses a project with VaRA's blame and feature analysis and generates a
+FeatureBlameReport.
+"""
+
+import typing as tp
+
+from benchbuild import Project
+from benchbuild.utils import actions
+from benchbuild.utils.cmd import opt
+from benchbuild.utils.requirements import Requirement, SlurmMem
+
+import varats.experiments.vara.feature_blame_experiment as FBE
+from varats.data.reports.feature_blame_report import FeatureBlameReport as FBR
+from varats.experiment.experiment_util import (
+    exec_func_with_pe_error_handler,
+    VersionExperiment,
+    ExperimentHandle,
+    wrap_unlimit_stack_size,
+    create_default_compiler_error_handler,
+    create_default_analysis_failure_handler,
+    create_new_success_result_filepath,
+)
+from varats.experiment.wllvm import get_cached_bc_file_path, BCFileExtensions
+from varats.project.project_util import get_local_project_git_paths
+from varats.project.varats_project import VProject
+from varats.report.report import ReportSpecification
+
+
+class FeatureBlameReportGeneration(actions.ProjectStep):  # type: ignore
+    """Analyse a project with VaRA and generate a FeatureBlameReport."""
+
+    NAME = "FeatureBlameReportGeneration"
+    DESCRIPTION = "Analyses the bitcode with -vara-FBR of VaRA."
+
+    project: VProject
+
+    def __init__(
+        self, project: Project, experiment_handle: ExperimentHandle,
+    ):
+        super().__init__(project=project)
+        self.__experiment_handle = experiment_handle
+
+    def __call__(self) -> actions.StepResult:
+        return self.analyze()
+    
+    def analyze(self) -> actions.StepResult:
+        """
+        This step performs the actual analysis with the correct command line
+        flags.
+
+        Flags used:
+            * -vara-FBR: to run a commit feature interaction report
+            * -yaml-report-outfile=<path>: specify the path to store the results
+        """
+
+        for binary in self.project.binaries:
+            # Add to the user-defined path for saving the results of the
+            # analysis also the name and the unique id of the project of every
+            # run.
+            result_file = create_new_success_result_filepath(
+                self.__experiment_handle, FBR, self.project, binary
+            )
+
+            opt_params = [
+                "--enable-new-pm=0", "-vara-PTFDD", "-vara-BD", "-vara-FBR",
+                "-vara-init-commits",  "-vara-use-phasar",
+                f"-vara-report-outfile={result_file}",
+                get_cached_bc_file_path(
+                    self.project, binary, [
+                        BCFileExtensions.NO_OPT, BCFileExtensions.TBAA,
+                        BCFileExtensions.BLAME
+                    ]
+                )
+            ]
+
+            run_cmd = opt[opt_params]
+
+            run_cmd = wrap_unlimit_stack_size(run_cmd)
+
+            exec_func_with_pe_error_handler(
+                run_cmd,
+                create_default_analysis_failure_handler(
+                    self.__experiment_handle, self.project, FBR
+                )
+            )
+
+        return actions.StepResult.OK
+    
+
+class FeatureBlameReportExperiment(VersionExperiment, shorthand="FBRE"):
+    """Generates a feature blame report of the project(s) specified in the call."""
+
+    NAME = "GenerateFeatureBlameReport"
+
+    REPORT_SPEC = ReportSpecification(FBR)
+    REQUIREMENTS: tp.List[Requirement] = [SlurmMem("250G")]
+
+    def actions_for_project(
+        self, project: VProject
+    ) -> tp.MutableSequence[actions.Step]:
+        """
+        Returns the specified steps to run the project(s) specified in the call
+        in a fixed order.
+
+        Args:
+            project: to analyze
+        """
+        # Try, to build the project without optimizations to get more precise
+        # blame annotations. Note: this does not guarantee that a project is
+        # build without optimizations because the used build tool/script can
+        # still add optimizations flags after the experiment specified cflags.
+        project.cflags += ["-O1", "-Xclang", "-disable-llvm-optzns", "-g0"]
+        bc_file_extensions = [
+            BCFileExtensions.NO_OPT,
+            BCFileExtensions.TBAA,
+            BCFileExtensions.BLAME,
+        ]
+
+        FBE.setup_basic_feature_blame_experiment(self, project, FBR)
+
+        analysis_actions = FBE.generate_basic_feature_blame_experiment_actions(
+            project,
+            bc_file_extensions,
+            extraction_error_handler=create_default_compiler_error_handler(
+                self.get_handle(), project, self.REPORT_SPEC.main_report
+            )
+        )
+        analysis_actions.append(
+            FeatureBlameReportGeneration(
+                project, self.get_handle()
+            )
+        )
+        analysis_actions.append(actions.Clean(project))
+
+        return analysis_actions 
