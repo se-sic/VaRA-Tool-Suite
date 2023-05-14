1cc2d3465cd7feb89cfe4fcfd0e41f3043f9bf1b
Lukas Abelt <git@login.luabelt.de>
2023-05-14 19:10:02 +0200

    Implemented binary_for_revision for dune project

--- varats/varats/projects/cpp_projects/dune.py
+++ varats/varats/projects/cpp_projects/dune.py
@@ -4,12 +4,14 @@ import typing as tp
 from plumbum import local
 
 from benchbuild.utils import cmd
+from benchbuild.utils.revision_ranges import RevisionRange
 import benchbuild as bb
 
 from varats.paper.paper_config import PaperConfigSpecificGit
 from varats.project.project_domain import ProjectDomains
+from varats.project.project_util import get_local_project_git_path, BinaryType
 from varats.project.varats_project import VProject
-from varats.utils.git_util import ShortCommitHash
+from varats.utils.git_util import ShortCommitHash, RevisionBinaryMap
 
 
 class Dune(VProject):
@@ -34,7 +36,45 @@ class Dune(VProject):
 
     @staticmethod
     def binaries_for_revision(revision: ShortCommitHash) -> tp.List['ProjectBinaryWrapper']:
-        pass
+        binary_map = RevisionBinaryMap(
+            get_local_project_git_path(Dune.NAME)
+        )
+
+        binary_map.specify_binary('dune-performance-regressions/build-cmake/src/dune-performance-regressions'
+                                  , BinaryType.EXECUTABLE)
+
+        binary_map.specify_binary('dune-performance-regressions/build-cmake/src/poisson-test'
+                                  , BinaryType.EXECUTABLE
+                                  , only_valid_in=RevisionRange("0d02b7b9acddfc57c3a0c905d6374fabbcaa0f58", "main"))
+
+        separated_poisson_range = RevisionRange("97fecde34910ba1f81c988ac2e1331aecddada06", "main")
+        binary_map.specify_binary('dune-performance-regressions/build-cmake/src/poisson-alberta'
+                                  , BinaryType.EXECUTABLE
+                                  , only_valid_in=separated_poisson_range)
+
+        binary_map.specify_binary('dune-performance-regressions/build-cmake/src/poisson-ug-pk-2d'
+                                  , BinaryType.EXECUTABLE
+                                  , only_valid_in=separated_poisson_range)
+
+        binary_map.specify_binary('dune-performance-regressions/build-cmake/src/poisson-yasp-q1-2d'
+                                  , BinaryType.EXECUTABLE
+                                  , only_valid_in=separated_poisson_range)
+
+        binary_map.specify_binary('dune-performance-regressions/build-cmake/src/poisson-yasp-q1-3d'
+                                  , BinaryType.EXECUTABLE
+                                  , only_valid_in=separated_poisson_range)
+
+        binary_map.specify_binary('dune-performance-regressions/build-cmake/src/poisson-yasp-q2-2d'
+                                  , BinaryType.EXECUTABLE
+                                  , only_valid_in=separated_poisson_range)
+
+        binary_map.specify_binary('dune-performance-regressions/build-cmake/src/poisson-yasp-q2-3d'
+                                  , BinaryType.EXECUTABLE
+                                  , only_valid_in=separated_poisson_range)
+
+        return binary_map[revision]
+
+
 
     def compile(self) -> None:
         """Compile the project using the in-built tooling from dune"""
