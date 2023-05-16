99829279382e93439907f81fa57b27c72f535487
Daniel Gusenburger <danielgusenburger@online.de>
2023-05-16 10:46:01 +0200

    added multiple budget experiment to instrumentation verifier

--- varats/varats/experiments/vara/instrumentation_verifier.py
+++ varats/varats/experiments/vara/instrumentation_verifier.py
@@ -20,12 +20,68 @@ from varats.experiments.vara.feature_experiment import (
 )
 from varats.project.varats_project import VProject
 from varats.report.report import ReportSpecification
+from varats.experiments.vara.multi_compile_experiment import VaryingStartingBudgetExperiment
 
 
 class RunInstrVerifier(FeatureExperiment, shorthand="RIV"):
     """Test runner for feature performance."""
 
     NAME = "RunInstrVerifier"
+
+    REPORT_SPEC = ReportSpecification(InstrVerifierReport)
+
+    def actions_for_project(
+        self, project: VProject
+    ) -> tp.MutableSequence[actions.Step]:
+        """
+        Returns the specified steps to run the project(s) specified in the call
+        in a fixed order.
+
+        Args:
+            project: to analyze
+        """
+        project.cflags += self.get_vara_feature_cflags(project)
+
+        project.cflags += self.get_vara_tracing_cflags(
+            FeatureInstrType.VERIFY, True
+        )
+
+        # Ensure that we detect all regions, when verifying
+        project.cflags += ["-fvara-instruction-threshold=0"]
+
+        # Add debug information, so traces can be better interpreted
+        project.cflags += ["-g"]
+
+        project.ldflags += self.get_vara_tracing_ldflags()
+
+        # Add the required runtime extensions to the project(s).
+        project.runtime_extension = run.RuntimeExtension(project, self)
+
+        # Add the required compiler extensions to the project(s).
+        project.compiler_extension = compiler.RunCompiler(project, self) \
+            << WithUnlimitedStackSize()
+
+        # Add own error handler to compile step.
+        project.compile = get_default_compile_error_wrapped(
+            self.get_handle(), project, self.REPORT_SPEC.main_report
+        )
+
+        analysis_actions = []
+
+        analysis_actions.append(actions.Compile(project))
+        analysis_actions.append(
+            RunVaRATracedWorkloads(
+                project, self.get_handle(), report_file_ending="ivr", workload_categories=[WorkloadCategory.EXAMPLE, WorkloadCategory.SMALL]
+            )
+        )
+        analysis_actions.append(actions.Clean(project))
+
+        return analysis_actions
+
+class RunInstrVerifierBudget(VaryingStartingBudgetExperiment, shorthand="RIVB"):
+    """Test runner for feature performance."""
+
+    NAME = "RunInstrVerifierBudget"
 
     REPORT_SPEC = ReportSpecification(InstrVerifierReport)
 
