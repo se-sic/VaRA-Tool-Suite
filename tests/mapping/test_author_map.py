70442da9f818208e7f1288137da6c133c363b69c
Simon Friedel <s8sifrie@uni-saarland.de>
2023-06-13 10:55:16 +0200

    add tests for generic get_author method

--- tests/mapping/test_author_map.py
+++ tests/mapping/test_author_map.py
@@ -33,3 +33,42 @@ class TestAuthorMap(unittest.TestCase):
             test_author.mail_addresses,
             {"jiat0218@gmail.com", "jiat75@gmail.com"}
         )
+
+    def test_get_author(self) -> None:
+        initialize_projects()
+        git_path = get_local_project_git_path("xz")
+        amap = generate_author_map(git_path)
+        test_author = amap.get_author("Jia Cheong Tan", "jiat75@gmail.com")
+        self.assertEqual(
+            test_author.names, {"Jia Cheong Tan", "Jia Tan", "jiat75"}
+        )
+        self.assertEqual(
+            test_author.mail_addresses,
+            {"jiat0218@gmail.com", "jiat75@gmail.com"}
+        )
+
+    def test_get_author_ambiguous(self) -> None:
+        initialize_projects()
+        git_path = get_local_project_git_path("xz")
+        amap = generate_author_map(git_path)
+        self.assertIsNone(amap.get_author("Jia Cheong Tan", "jim@meyering.net"))
+
+    def test_get_author_missing_mail(self) -> None:
+        initialize_projects()
+        git_path = get_local_project_git_path("xz")
+        amap = generate_author_map(git_path)
+        self.assertIsNone(
+            amap.get_author("Jia Cheong Tan", "notpresent@None.com")
+        )
+
+    def test_get_author_missing_name(self) -> None:
+        initialize_projects()
+        git_path = get_local_project_git_path("xz")
+        amap = generate_author_map(git_path)
+        self.assertIsNone(amap.get_author("Not Present", "jim@meyering.net"))
+
+    def test_get_author_missing(self) -> None:
+        initialize_projects()
+        git_path = get_local_project_git_path("xz")
+        amap = generate_author_map(git_path)
+        self.assertIsNone(amap.get_author("Not Present", "notpresent@None.com"))
