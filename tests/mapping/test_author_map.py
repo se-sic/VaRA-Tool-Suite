d085847e736a40a21ffccda719f3e7b5c3e47674
Nerum <nerumlp@gmail.com>
2023-06-26 10:16:21 +0200

    add tests for author merging

--- tests/mapping/test_author_map.py
+++ tests/mapping/test_author_map.py
@@ -1,7 +1,26 @@
 import unittest
 
-from varats.mapping.author_map import generate_author_map
+from varats.mapping.author_map import generate_author_map, Author
 from varats.projects.discover_projects import initialize_projects
+
+
+class TestAuthor(unittest.TestCase):
+
+    def test_merge(self) -> None:
+        author_one = Author(1, "Jon Doe", "jon_doe@jon_doe.com")
+        author_two = Author(4, "J. Doe", "jon_doe@jon_doe.com")
+        author_three = Author(2, "Jon Doe", "j_doe@gmail.com")
+        merge_one = author_one.merge(author_two)
+        merge_two = author_three.merge(author_one)
+        self.assertEqual(merge_one.author_id, 1)
+        self.assertEqual(merge_one.names, {"Jon Doe", "J. Doe"})
+        self.assertEqual(merge_one.mail_addresses, {"jon_doe@jon_doe.com"})
+        self.assertEqual(merge_two.author_id, 1)
+        self.assertEqual(merge_two.names, {"Jon Doe", "J. Doe"})
+        self.assertEqual(
+            merge_two.mail_addresses,
+            {"jon_doe@jon_doe.com", "j_doe@gmail.com"}
+        )
 
 
 class TestAuthorMap(unittest.TestCase):
@@ -61,3 +80,20 @@ class TestAuthorMap(unittest.TestCase):
         initialize_projects()
         amap = generate_author_map("xz")
         self.assertIsNone(amap.get_author("Not Present", "notpresent@None.com"))
+
+    def test_author_merging(self) -> None:
+        initialize_projects()
+        amap = generate_author_map("brotli")
+        test_author = amap.get_author("eustas", "eustas@google.com")
+        self.assertEqual(
+            test_author.names, {
+                "eustas", "Eugene Kliuchnikov", "Eugene Klyuchnikov",
+                "Evgenii Kliuchnikov"
+            }
+        )
+        self.assertEqual(
+            test_author.mail_addresses, {
+                "eustas@google.com", "eustas.ru@gmail.com",
+                "eustas@eustas-wfh.fra.corp.google.com"
+            }
+        )
