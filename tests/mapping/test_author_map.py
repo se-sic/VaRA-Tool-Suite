fa4e4f0e6a0a2e7634d212b8fc32efe38acd726e
Nerum <nerumlp@gmail.com>
2023-06-26 09:40:58 +0200

    adapt author map tests to new generation method

--- tests/mapping/test_author_map.py
+++ tests/mapping/test_author_map.py
@@ -1,10 +1,6 @@
 import unittest
-import unittest.mock as mock
-from pathlib import Path
-from tempfile import NamedTemporaryFile
 
-from varats.mapping.author_map import generate_author_map, Author
-from varats.project.project_util import get_local_project_git_path
+from varats.mapping.author_map import generate_author_map
 from varats.projects.discover_projects import initialize_projects
 
 
@@ -12,8 +8,7 @@ class TestAuthorMap(unittest.TestCase):
 
     def test_get_author_by_email(self) -> None:
         initialize_projects()
-        git_path = get_local_project_git_path("xz")
-        amap = generate_author_map(git_path)
+        amap = generate_author_map("xz")
         test_author = amap.get_author_by_email("jim@meyering.net")
         self.assertEqual(
             test_author.mail_addresses,
@@ -23,8 +18,7 @@ class TestAuthorMap(unittest.TestCase):
 
     def test_get_author_by_name(self) -> None:
         initialize_projects()
-        git_path = get_local_project_git_path("xz")
-        amap = generate_author_map(git_path)
+        amap = generate_author_map("xz")
         test_author = amap.get_author_by_name("Jia Cheong Tan")
         self.assertEqual(
             test_author.names, {"Jia Cheong Tan", "Jia Tan", "jiat75"}
@@ -36,8 +30,7 @@ class TestAuthorMap(unittest.TestCase):
 
     def test_get_author(self) -> None:
         initialize_projects()
-        git_path = get_local_project_git_path("xz")
-        amap = generate_author_map(git_path)
+        amap = generate_author_map("xz")
         test_author = amap.get_author("Jia Cheong Tan", "jiat75@gmail.com")
         self.assertEqual(
             test_author.names, {"Jia Cheong Tan", "Jia Tan", "jiat75"}
@@ -49,26 +42,22 @@ class TestAuthorMap(unittest.TestCase):
 
     def test_get_author_ambiguous(self) -> None:
         initialize_projects()
-        git_path = get_local_project_git_path("xz")
-        amap = generate_author_map(git_path)
+        amap = generate_author_map("xz")
         self.assertIsNone(amap.get_author("Jia Cheong Tan", "jim@meyering.net"))
 
     def test_get_author_missing_mail(self) -> None:
         initialize_projects()
-        git_path = get_local_project_git_path("xz")
-        amap = generate_author_map(git_path)
+        amap = generate_author_map("xz")
         self.assertIsNone(
             amap.get_author("Jia Cheong Tan", "notpresent@None.com")
         )
 
     def test_get_author_missing_name(self) -> None:
         initialize_projects()
-        git_path = get_local_project_git_path("xz")
-        amap = generate_author_map(git_path)
+        amap = generate_author_map("xz")
         self.assertIsNone(amap.get_author("Not Present", "jim@meyering.net"))
 
     def test_get_author_missing(self) -> None:
         initialize_projects()
-        git_path = get_local_project_git_path("xz")
-        amap = generate_author_map(git_path)
+        amap = generate_author_map("xz")
         self.assertIsNone(amap.get_author("Not Present", "notpresent@None.com"))
