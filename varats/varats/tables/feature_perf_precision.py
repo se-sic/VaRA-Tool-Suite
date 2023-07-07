4947187f5b7c396ccafe5a48d656a74a8f2846ee
Florian Sattler <vuld3r@gmail.com>
2023-07-07 14:52:02 +0200

    Implements profiler split

--- varats/varats/tables/feature_perf_precision.py
+++ varats/varats/tables/feature_perf_precision.py
@@ -5,10 +5,12 @@ import typing as tp
 import pandas as pd
 
 from varats.experiments.vara.feature_experiment import FeatureExperiment
+from varats.experiments.vara.feature_perf_runner import FeaturePerfRunner
 from varats.paper.case_study import CaseStudy
 from varats.paper.paper_config import get_loaded_paper_config
 from varats.paper_mgmt.case_study import get_case_study_file_name_filter
 from varats.report.report import BaseReport
+from varats.report.tef_report import TEFReport
 from varats.revision.revisions import get_processed_revisions_files
 from varats.table.table import Table
 from varats.table.table_utils import dataframe_to_table
@@ -16,19 +18,19 @@ from varats.table.tables import TableFormat, TableGenerator
 
 
 def get_regressing_config_ids_GT(case_study: CaseStudy) -> tp.Dict[int, bool]:
-    if case_study.project_name == "SynthSAContextSensitivity":
-        return {
-            0: True,
-            1: True,
-            2: True,
-            3: True,
-            4: False,
-            5: False,
-            6: False,
-            7: False
-        }
+    #if case_study.project_name == "SynthSAContextSensitivity":
+    return {
+        0: True,
+        1: True,
+        2: True,
+        3: True,
+        4: False,
+        5: False,
+        6: False,
+        7: False
+    }
 
-    raise NotImplementedError()
+    # raise NotImplementedError()
     return {}
 
 
@@ -112,20 +114,35 @@ class ClassificationResults:
 
 class Profiler():
 
-    @property
-    @abc.abstractmethod
-    def name(self) -> tp.Type[FeatureExperiment]:
-        """Hame of the profiler used."""
+    def __init__(
+        self, name: str, experiment: tp.Type[FeatureExperiment],
+        report_type: tp.Type[BaseReport]
+    ) -> None:
+        self.__name = name
+        self.__experiment = experiment
+        self.__report_type = report_type
 
     @property
-    @abc.abstractmethod
+    def name(self) -> str:
+        """Hame of the profiler used."""
+        return self.__name
+
+    @property
     def experiment(self) -> tp.Type[FeatureExperiment]:
         """Experiment used to produce this profilers information."""
+        return self.__experiment
 
     @property
-    @abc.abstractmethod
     def report_type(self) -> tp.Type[BaseReport]:
         """Report type used to load this profilers information."""
+        return self.__report_type
+
+
+class VXray(Profiler):
+
+    def __init__(self) -> None:
+        # TODO: fix with actual
+        super().__init__("WXray", FeaturePerfRunner, TEFReport)
 
 
 def compute_profiler_predictions(
@@ -145,7 +162,7 @@ class FeaturePerfPrecisionTable(Table, table_name="fperf_precision"):
 
     def tabulate(self, table_format: TableFormat, wrap_table: bool) -> str:
         case_studies = get_loaded_paper_config().get_all_case_studies()
-        profilers: tp.List[Profiler] = []
+        profilers: tp.List[Profiler] = [VXray()]
 
         # Data aggregation
         df = pd.DataFrame()
@@ -155,17 +172,31 @@ class FeaturePerfPrecisionTable(Table, table_name="fperf_precision"):
             rev = case_study.revisions[0]
             project_name = case_study.project_name
 
-            new_row = {
-                'CaseStudy': project_name,
-                'Configs': len(case_study.get_config_ids_for_revision(rev))
-            }
-
             ground_truth = get_regressing_config_ids_GT(case_study)
+
+            new_row = {
+                'CaseStudy':
+                    project_name,
+                'Configs':
+                    len(case_study.get_config_ids_for_revision(rev)),
+                'RegressedConfigs':
+                    len(map_to_positive_config_ids(ground_truth))
+            }
 
             for profiler in profilers:
                 predicted = compute_profiler_predictions(
                     profiler, project_name, case_study
                 )
+                predicted = {
+                    0: True,
+                    1: True,
+                    2: False,
+                    3: True,
+                    4: False,
+                    5: True,
+                    6: True,
+                    7: False
+                }
 
                 results = ClassificationResults(
                     map_to_positive_config_ids(ground_truth),
@@ -173,6 +204,10 @@ class FeaturePerfPrecisionTable(Table, table_name="fperf_precision"):
                     map_to_positive_config_ids(predicted),
                     map_to_negative_config_ids(predicted)
                 )
+                new_row[f"{profiler.name}_precision"] = results.precision()
+                new_row[f"{profiler.name}_recall"] = results.recall()
+                new_row[f"{profiler.name}_baccuracy"
+                       ] = results.balanced_accuracy()
 
             table_rows.append(new_row)
             # df.append(new_row, ignore_index=True)
@@ -183,6 +218,16 @@ class FeaturePerfPrecisionTable(Table, table_name="fperf_precision"):
             ["CaseStudy"],
             inplace=True,
         )
+
+        print(f"{df=}")
+        colum_setup = [('', 'Configs'), ('', 'RegressedConfigs')]
+        for profiler in profilers:
+            colum_setup.append((profiler.name, 'Precision'))
+            colum_setup.append((profiler.name, 'Recall'))
+            colum_setup.append((profiler.name, 'BAcc'))
+
+        print(f"{colum_setup=}")
+        df.columns = pd.MultiIndex.from_tuples(colum_setup)
 
         # Table config
 
