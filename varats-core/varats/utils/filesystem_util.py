92406afd94d3842e1de49e8b46d3ceb65c070c88
Lukas Abelt <git@login.luabelt.de>
2023-09-13 13:55:50 +0200

    * Fix mypy

--- varats-core/varats/utils/filesystem_util.py
+++ varats-core/varats/utils/filesystem_util.py
@@ -18,7 +18,7 @@ class FolderAlreadyPresentError(Exception):
 
 
 @contextmanager
-def lock_file(lock_path: Path, lock_mode=fcntl.LOCK_EX):
+def lock_file(lock_path: Path, lock_mode: int = fcntl.LOCK_EX) -> tp.Generator:
     # Ensure that the lock exists
     lock_path.touch(exist_ok=True)
 
