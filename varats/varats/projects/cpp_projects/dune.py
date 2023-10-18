9ba9b70d633c0410b0f3fb4b6310c21604427163
Lukas Abelt <github@login.luabelt.de>
2023-10-18 15:50:57 +0200

    Cleans up old binaries and workloads for Dune (#847)

--- varats/varats/projects/cpp_projects/dune.py
+++ varats/varats/projects/cpp_projects/dune.py
@@ -57,58 +57,6 @@ class DunePerfRegression(VProject):
             Command(
                 SourceRoot(
                     "dune-VaRA/dune-performance-regressions/build-cmake/src"
-                ) / RSBinary('dune-performance-regressions'),
-                label='dune-helloworld'
-            ),
-            Command(
-                SourceRoot(
-                    "dune-VaRA/dune-performance-regressions/build-cmake/src"
-                ) / RSBinary('poisson-test'),
-                label='poisson-non-separated',
-                creates=[
-                    'poisson_UG_Pk_2d.vtu', 'poisson-yasp-Q1-2d.vtu',
-                    'poisson-yasp-Q1-3d.vtu', 'poisson-yasp-Q2-2d.vtu',
-                    'poisson-yasp-Q2-3d.vtu'
-                ]
-            ),
-            Command(
-                SourceRoot(
-                    "dune-VaRA/dune-performance-regressions/build-cmake/src"
-                ) / RSBinary('poisson-ug-pk-2d'),
-                label='poisson-ug-pk-2d',
-                creates=['poisson-UG-Pk-2d.vtu']
-            ),
-            Command(
-                SourceRoot(
-                    "dune-VaRA/dune-performance-regressions/build-cmake/src"
-                ) / RSBinary('poisson-yasp-q1-2d'),
-                label='poisson-yasp-q1-2d',
-                creates=['poisson-yasp-q1-2d.vtu']
-            ),
-            Command(
-                SourceRoot(
-                    "dune-VaRA/dune-performance-regressions/build-cmake/src"
-                ) / RSBinary('poisson-yasp-q1-3d'),
-                label='poisson-yasp-q1-3d',
-                creates=['poisson-yasp-q1-3d.vtu']
-            ),
-            Command(
-                SourceRoot(
-                    "dune-VaRA/dune-performance-regressions/build-cmake/src"
-                ) / RSBinary('poisson-yasp-q2-2d'),
-                label='poisson-yasp-q2-2d',
-                creates=['poisson-yasp-q2-2d.vtu']
-            ),
-            Command(
-                SourceRoot(
-                    "dune-VaRA/dune-performance-regressions/build-cmake/src"
-                ) / RSBinary('poisson-yasp-q2-3d'),
-                label='poisson-yasp-q2-3d',
-                creates=['poisson-yasp-q2-3d.vtu']
-            ),
-            Command(
-                SourceRoot(
-                    "dune-VaRA/dune-performance-regressions/build-cmake/src"
                 ) / RSBinary('dune_performance_regressions'),
                 label='dune_helloworld'
             ),
@@ -157,6 +105,13 @@ class DunePerfRegression(VProject):
                 ) / RSBinary('poisson_yasp_q2_3d'),
                 label='poisson_yasp_q2_3d',
                 creates=['poisson-yasp-q2-3d.vtu']
+            ),
+            Command(
+                SourceRoot(
+                    "dune-VaRA/dune-performance-regressions/build-cmake/src"
+                ) / RSBinary('poisson_alugrid'),
+                label='poisson_alugrid',
+                creates=['poisson_ALU_Pk_2d.vtu']
             )
         ]
     }
@@ -169,108 +124,54 @@ class DunePerfRegression(VProject):
             get_local_project_git_path(DunePerfRegression.NAME)
         )
 
-        binary_map.specify_binary(
-            'dune-performance-regressions', BinaryType.EXECUTABLE
-        )
-
-        binary_map.specify_binary(
-            'poisson-test',
-            BinaryType.EXECUTABLE,
-            only_valid_in=RevisionRange(
-                '0d02b7b9acddfc57c3a0c905d6374fabbcaa0f58', 'main'
-            )
-        )
-
-        separated_poisson_range = RevisionRange(
-            '97fecde34910ba1f81c988ac2e1331aecddada06', 'main'
-        )
-
-        binary_map.specify_binary(
-            'poisson-alberta',
-            BinaryType.EXECUTABLE,
-            only_valid_in=separated_poisson_range
-        )
-
-        binary_map.specify_binary(
-            'poisson-ug-pk-2d',
-            BinaryType.EXECUTABLE,
-            only_valid_in=separated_poisson_range
-        )
-
-        binary_map.specify_binary(
-            'poisson-yasp-q1-2d',
-            BinaryType.EXECUTABLE,
-            only_valid_in=separated_poisson_range
-        )
-
-        binary_map.specify_binary(
-            'poisson-yasp-q2-3d',
-            BinaryType.EXECUTABLE,
-            only_valid_in=separated_poisson_range
-        )
-
-        binary_map.specify_binary(
-            'poisson-yasp-q2-2d',
-            BinaryType.EXECUTABLE,
-            only_valid_in=separated_poisson_range
-        )
-
-        binary_map.specify_binary(
-            'poisson-yasp-q1-3d',
-            BinaryType.EXECUTABLE,
-            only_valid_in=separated_poisson_range
-        )
-
-        new_binary_naming_range = RevisionRange(
+        rev_range = RevisionRange(
             '332a9af0b7e3336dd72c4f4b74e09df525b6db0d', 'main'
         )
 
         binary_map.specify_binary(
             'dune_performance_regressions',
             BinaryType.EXECUTABLE,
-            only_valid_in=new_binary_naming_range
+            only_valid_in=rev_range
         )
 
         binary_map.specify_binary(
-            'poisson_test',
-            BinaryType.EXECUTABLE,
-            only_valid_in=new_binary_naming_range
+            'poisson_test', BinaryType.EXECUTABLE, only_valid_in=rev_range
         )
 
         binary_map.specify_binary(
-            'poisson_alberta',
-            BinaryType.EXECUTABLE,
-            only_valid_in=new_binary_naming_range
+            'poisson_alberta', BinaryType.EXECUTABLE, only_valid_in=rev_range
         )
 
         binary_map.specify_binary(
-            'poisson_ug_pk_2d',
-            BinaryType.EXECUTABLE,
-            only_valid_in=new_binary_naming_range
+            'poisson_ug_pk_2d', BinaryType.EXECUTABLE, only_valid_in=rev_range
         )
 
         binary_map.specify_binary(
             'poisson_yasp_q1_2d',
             BinaryType.EXECUTABLE,
-            only_valid_in=new_binary_naming_range
+            only_valid_in=rev_range
         )
 
         binary_map.specify_binary(
             'poisson_yasp_q2_3d',
             BinaryType.EXECUTABLE,
-            only_valid_in=new_binary_naming_range
+            only_valid_in=rev_range
         )
 
         binary_map.specify_binary(
             'poisson_yasp_q2_2d',
             BinaryType.EXECUTABLE,
-            only_valid_in=new_binary_naming_range
+            only_valid_in=rev_range
         )
 
         binary_map.specify_binary(
             'poisson_yasp_q1_3d',
             BinaryType.EXECUTABLE,
-            only_valid_in=new_binary_naming_range
+            only_valid_in=rev_range
+        )
+
+        binary_map.specify_binary(
+            'poisson_alugrid', BinaryType.EXECUTABLE, only_valid_in=rev_range
         )
 
         return binary_map[revision]
