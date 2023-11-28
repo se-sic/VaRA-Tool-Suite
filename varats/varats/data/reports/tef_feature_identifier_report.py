778d31699338b6acb9f2e84675d9a2124bba02ed
Lukas Abelt <git@login.luabelt.de>
2023-11-28 09:35:57 +0100

    - Added implementation for Feature Identifier Report

--- varats/varats/data/reports/tef_feature_identifier_report.py
+++ varats/varats/data/reports/tef_feature_identifier_report.py
@@ -1,5 +1,74 @@
+import json
+import typing as tp
+from collections import defaultdict
+from pathlib import Path
+
 from varats.report.report import BaseReport
 
+RegionTupleTy = tp.Tuple[frozenset, int]
+PatchesTupleTy = tp.Tuple[str, int]
 
-class TEFFeatureIdentifierReport(BaseReport, shorthand="TEFID", file_type=".json"):
-    pass
+
+class TEFFeatureIdentifierReport(
+    BaseReport, shorthand="TEFID", file_type=".json"
+):
+
+    def __init__(self, path: Path):
+        super().__init__(path)
+
+        self.__patch_names: tp.Set[str] = set()
+        self.__patch_to_regions: tp.Dict[str, tp.Set[RegionTupleTy]] = dict()
+        self.__regions_to_patches = defaultdict(list)
+        self.__baseline_regions: tp.List = list()
+
+        with open(self.path, "r") as report:
+            results = json.load(report)
+
+            for entry in results:
+                if entry == "Baseline":
+                    self.__baseline_regions = set([
+                        (r.split('*'), results[entry][r])
+                        for r in results[entry]
+                    ])
+                    continue
+
+                patch_name = entry[len("PATCHED_"):]
+
+                self.__patch_names.add(patch_name)
+                self.__patch_to_regions[patch_name] = set()
+
+                for region in results[entry]:
+                    regions = frozenset(region.split('*'))
+                    self.__patch_to_regions[patch_name].add(
+                        (regions, results[entry][region])
+                    )
+                    self.__regions_to_patches[regions].append(
+                        (patch_name, results[entry][region])
+                    )
+
+    @property
+    def patch_names(self) -> tp.Set[str]:
+        return self.__patch_names
+
+    @property
+    def baseline_regions(self) -> tp.Set[RegionTupleTy]:
+        return self.__baseline_regions
+
+    def regions_for_patch(self, patch_name: str) -> tp.Set[RegionTupleTy]:
+        return self.__patch_to_regions[patch_name]
+
+    def patches_containing_region(
+        self, regions: tp.Iterable[str]
+    ) -> tp.List[PatchesTupleTy]:
+        result = []
+        for region in self.__regions_to_patches:
+            if regions in region:
+                result += self.__regions_to_patches[region]
+        return result
+
+    def patches_for_regions(
+        self, regions: tp.Iterable[str]
+    ) -> tp.List[PatchesTupleTy]:
+        if regions in self.__regions_to_patches:
+            return self.__regions_to_patches[regions]
+        return []
