4b2bc7417849d6971f2a40b8827cae99a6e1f80a
Lukas Abelt <git@login.luabelt.de>
2023-12-15 13:10:46 +0100

    * Added accuracy plot for individual feature times

--- varats/varats/plots/feature_perf_accuracy.py
+++ varats/varats/plots/feature_perf_accuracy.py
@@ -65,3 +65,45 @@ class PerfAccuracyDistPlotGenerator(
     def generate(self) -> tp.List[Plot]:
 
         return [PerfAccuracyDistPlot(self.plot_config, **self.plot_kwargs)]
+
+
+class PerfWBAccuracyDistPlot(Plot, plot_name='fperf_wb_accuracy_dist'):
+    """Accuracy plot that plots the measuring accuracy for feature regressions
+    for individual feature regions for different profilers."""
+
+    def plot(self, view_mode: bool) -> None:
+        case_studies = get_loaded_paper_config().get_all_case_studies()
+        profilers: tp.List[Profiler] = [
+            VXray(experiment=TEFProfileRunnerPrecision),
+            PIMTracer(experiment=PIMProfileRunnerPrecision),
+            EbpfTraceTEF(experiment=EbpfTraceTEFProfileRunnerPrecision)
+        ]
+
+        # Data aggregation
+        df = load_accuracy_data(case_studies, profilers)
+        df.sort_values(["CaseStudy"], inplace=True)
+
+        g = sns.displot(
+            data=df[df["Features"] != "__ALL__"],
+            x="epsilon",
+            hue="Profiler",
+            kind="kde",
+            col="CaseStudy"
+        )
+        g.set_axis_labels("\u03b5", "Density")
+        g.set_titles("{col_name}")
+
+    def calc_missing_revisions(
+        self, boundary_gradient: float
+    ) -> tp.Set[FullCommitHash]:
+        raise UnsupportedOperation
+
+
+class PerfWBAccuracyDistPlotGenerator(
+    PlotGenerator, generator_name="fperf-wb-accuracy-dist", options=[]
+):
+    """Generates accuracy distribution plot."""
+
+    def generate(self) -> tp.List[Plot]:
+
+        return [PerfAccuracyDistPlot(self.plot_config, **self.plot_kwargs)]
