79d04462d8f7e1fd4aa11fe3b592c8d7b3c6b366
Johannes Weissmann <s8jsweis@stud.uni-saarland.de>
2024-03-18 13:38:25 +0100

    Adds a BC file extension for Architecture, a bare-bones MDReport and ArchitectureAnalysis experiment

---
+++ varats/varats/data/reports/md_report.py
@@ -0,0 +1,11 @@
+"""Metadata report implementation."""
+
+from varats.report.report import BaseReport
+
+
+class MDReport(BaseReport, shorthand="MD", file_type="txt"):
+    """
+    An Metadata report which serves as a base class for the Architecture report.
+    """
+
+    # TODO Implement the MDReport class.
