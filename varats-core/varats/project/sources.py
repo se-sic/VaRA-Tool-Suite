bf0086082f92eca1f7c3f973b4922a40014753f1
Sebastian Böhm <boehmseb@cs.uni-saarland.de>
2024-04-07 17:09:21 +0200

    Add another picosat workload

--- varats-core/varats/project/sources.py
+++ varats-core/varats/project/sources.py
@@ -4,8 +4,9 @@ import typing as tp
 
 import benchbuild as bb
 import plumbum as pb
-from benchbuild.source import Variant
+from benchbuild.source import Variant, HTTP
 from benchbuild.source.base import Revision
+from benchbuild.utils.cmd import mkdir, ln, unzip
 
 from varats.paper.paper_config import get_paper_config
 from varats.utils.git_util import ShortCommitHash
@@ -49,3 +50,46 @@ class FeatureSource(bb.source.FetchableSource):  # type: ignore
         )
 
         return [Variant(self, str(config_id)) for config_id in config_ids]
+
+
+class HTTPUnzip(HTTP):  # type: ignore
+    """Fetch and download source via http and auto-unpack using unzip."""
+
+    def version(self, target_dir: str, version: str) -> pb.LocalPath:
+        """
+        Set up the given version of this HTTPUnzip source.
+
+        This will fetch the given version from the remote source and unpack the
+        archive into the build directory using unzip.
+
+        The location matches the behavior of other sources. However, you need
+        to consider that benchbuild will return a directory instead of a file path.
+
+        When using workloads, you can refer to a directory with the SourceRootRenderer using
+        ``benchbuild.command.source_root``.
+
+        Example:
+            You specify a remote version 1.0 of an archive compression.zip and
+            a local name of "compression.zip".
+            The build directory will look as follows:
+
+            <builddir>/1.0-compression.dir/
+            <builddir>/1.0-compression.zip
+            <builddir>/compression.zip -> ./1.0-compression.tar.dir
+
+            The content of the archive is found in the directory compression.zip.
+            Your workloads need to make sure to reference this directory (e.g. using tokens),
+            e.g., ``source_root("compression.zip")``
+        """
+        archive_path = super().version(target_dir, version)
+
+        target_name = str(pb.local.path(archive_path).with_suffix(".dir"))
+        target_path = pb.local.path(target_dir) / target_name
+        active_loc = pb.local.path(target_dir) / self.local
+
+        mkdir(target_path)
+        unzip(archive_path, "-d", target_path)
+
+        ln('-sf', target_path, active_loc)
+
+        return target_path
--- varats/varats/projects/c_projects/picosat.py
+++ varats/varats/projects/c_projects/picosat.py
@@ -10,6 +10,7 @@ from benchbuild.utils.cmd import make
 from benchbuild.utils.settings import get_number_of_jobs
 from plumbum import local
 
+from varats.containers.containers import get_base_image, ImageBase
 from varats.experiment.workload_util import (
     RSBinary,
     WorkloadCategory,
@@ -24,7 +25,7 @@ from varats.project.project_util import (
     get_local_project_git_path,
     verify_binaries,
 )
-from varats.project.sources import FeatureSource
+from varats.project.sources import FeatureSource, HTTPUnzip
 from varats.project.varats_command import VCommand
 from varats.project.varats_project import VProject
 from varats.provider.release.release_provider import (
@@ -50,9 +51,9 @@ class PicoSAT(VProject, ReleaseProviderHook):
     SOURCE = [
         PaperConfigSpecificGit(
             project_name="picosat",
-            remote="https://github.com/se-sic/picoSAT-mirror",
+            remote="https://github.com/se-sic/picoSAT-vara",
             local="picosat",
-            refspec="origin/HEAD",
+            refspec="origin/version-history",
             limit=None,
             shallow=False
         ),
@@ -63,6 +64,20 @@ class PicoSAT(VProject, ReleaseProviderHook):
                 "1.0":
                     "https://github.com/se-sic/picoSAT-mirror/releases/"
                     "download/picoSAT-965/example.cnf"
+            }
+        ),
+        HTTP(
+            local="aim-100-1_6-no-1.cnf",
+            remote={
+                "1.0":
+                    "https://people.sc.fsu.edu/~jburkardt/data/cnf/aim-100-1_6-no-1.cnf"
+            }
+        ),
+        HTTPUnzip(
+            local="sr15bench",
+            remote={
+                "1.0":
+                    "https://baldur.iti.kit.edu/sat-race-2015/downloads/sr15bench.zip"
             }
         ),
         HTTPUntar(
@@ -101,6 +116,8 @@ class PicoSAT(VProject, ReleaseProviderHook):
         ),
     ]
 
+    CONTAINER = get_base_image(ImageBase.DEBIAN_12)
+
     WORKLOADS = {
         WorkloadSet(WorkloadCategory.EXAMPLE): [
             VCommand(
@@ -117,6 +134,13 @@ class PicoSAT(VProject, ReleaseProviderHook):
             )
         ],
         WorkloadSet(WorkloadCategory.MEDIUM): [
+            VCommand(
+                SourceRoot("picosat") / RSBinary("picosat"),
+                "sr15bench/mrpp_8x8#16_12.cnf",
+                label="mrpp_8x8#16_12.cnf",
+            )
+        ],
+        WorkloadSet(WorkloadCategory.LARGE): [
             VCommand(
                 SourceRoot("picosat") / RSBinary("picosat"),
                 "traffic_kkb_unknown.cnf/traffic_kkb_unknown.cnf",
@@ -167,142 +191,7 @@ class PicoSAT(VProject, ReleaseProviderHook):
 
         with local.cwd(picosat_source):
             revisions_with_new_config_name = get_all_revisions_between(
-                "33c685e82213228726364980814f0183e435de78", "", ShortCommitHash
-            )
-        picosat_version = ShortCommitHash(self.version_of_primary)
-        if picosat_version in revisions_with_new_config_name:
-            config_script_name = "./configure.sh"
-        else:
-            config_script_name = "./configure"
-
-        with local.cwd(picosat_source):
-            with local.env(CC=str(c_compiler), CXX=str(cxx_compiler)):
-                bb.watch(local[config_script_name])(["--trace", "--stats"])
-                bb.watch(make)("-j", get_number_of_jobs(bb_cfg()))
-
-        with local.cwd(picosat_source):
-            verify_binaries(self)
-
-    @classmethod
-    def get_release_revisions(
-        cls, release_type: ReleaseType
-    ) -> tp.List[tp.Tuple[FullCommitHash, str]]:
-        release_regex = "^picoSAT-[0-9]+$"
-
-        tagged_commits = get_tagged_commits(cls.NAME)
-
-        return [(FullCommitHash(h), tag)
-                for h, tag in tagged_commits
-                if re.match(release_regex, tag)]
-
-
-class PicoSATLoadTime(VProject, ReleaseProviderHook):
-    """Adapted version of picoSAT that has been refactored, such that it does
-    not require a field-sensitive analysis."""
-
-    NAME = 'PicoSATLoadTime'
-    GROUP = 'c_projects'
-    DOMAIN = ProjectDomains.SOLVER
-
-    SOURCE = [
-        PaperConfigSpecificGit(
-            project_name="PicoSATLoadTime",
-            remote="https://github.com/se-sic/picoSAT-vara",
-            local="PicoSATLoadTime",
-            refspec="origin/HEAD",
-            limit=None,
-            shallow=False
-        ),
-        FeatureSource(),
-        HTTP(
-            local="example.cnf",
-            remote={
-                "1.0":
-                    "https://github.com/se-sic/picoSAT-mirror/releases/"
-                    "download/picoSAT-965/example.cnf"
-            }
-        ),
-        HTTP(
-            local="ibm-2004-03-k70.cnf",
-            remote={
-                "1.0":
-                    "https://github.com/se-sic/picoSAT-vara/releases/"
-                    "download/workloads-sat-race-2006/ibm-2004-03-k70.cnf"
-            }
-        ),
-        HTTPUntar(
-            local="abw-N-bcsstk07.mtx-w44.cnf",
-            remote={
-                "1.0":
-                    "https://github.com/se-sic/picoSAT-mirror/releases/"
-                    "download/picoSAT-965/abw-N-bcsstk07.mtx-w44.cnf.tar.gz"
-            }
-        ),
-        HTTPUntar(
-            local="traffic_kkb_unknown.cnf",
-            remote={
-                "1.0":
-                    "https://github.com/se-sic/picoSAT-mirror/releases/"
-                    "download/picoSAT-965/traffic_kkb_unknown.cnf.tar.gz"
-            }
-        ),
-        HTTPUntar(
-            local="UNSAT_H_instances_childsnack_p11.hddl_1.cnf",
-            remote={
-                "1.0":
-                    "https://github.com/se-sic/picoSAT-mirror/releases/"
-                    "download/picoSAT-965/"
-                    "UNSAT_H_instances_childsnack_p11.hddl_1.cnf.tar.gz"
-            }
-        ),
-        HTTPUntar(
-            local="UNSAT_H_instances_childsnack_p12.hddl_1.cnf",
-            remote={
-                "1.0":
-                    "https://github.com/se-sic/picoSAT-mirror/releases/"
-                    "download/picoSAT-965/"
-                    "UNSAT_H_instances_childsnack_p12.hddl_1.cnf.tar.gz"
-            }
-        ),
-    ]
-
-    WORKLOADS = {
-        WorkloadSet(WorkloadCategory.SMALL): [
-            VCommand(
-                SourceRoot("PicoSATLoadTime") / RSBinary("picosat"),
-                "ibm-2004-03-k70.cnf",
-                ConfigParams(),
-                label="ibm-2004-03-k70.cnf",
-            ),
-        ],
-    }
-
-    @staticmethod
-    def binaries_for_revision(
-        revision: ShortCommitHash
-    ) -> tp.List[ProjectBinaryWrapper]:
-        binary_map = RevisionBinaryMap(
-            get_local_project_git_path(PicoSATLoadTime.NAME)
-        )
-        binary_map.specify_binary(
-            'picosat', BinaryType.EXECUTABLE, valid_exit_codes=[0, 10, 20]
-        )
-
-        return binary_map[revision]
-
-    def run_tests(self) -> None:
-        pass
-
-    def compile(self) -> None:
-        """Compile the project."""
-        picosat_source = local.path(self.source_of(self.primary_source))
-
-        c_compiler = bb.compiler.cc(self)
-        cxx_compiler = bb.compiler.cxx(self)
-
-        with local.cwd(picosat_source):
-            revisions_with_new_config_name = get_all_revisions_between(
-                "33c685e82213228726364980814f0183e435de78", "", ShortCommitHash
+                "63a74c25dfadc447b7eea07773ef40f589ca1ed5", "", ShortCommitHash
             )
         picosat_version = ShortCommitHash(self.version_of_primary)
         if picosat_version in revisions_with_new_config_name:
