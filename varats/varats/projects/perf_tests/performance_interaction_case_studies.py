13cc74eee5274fa22823d81237943fbce82cd020
Sebastian Böhm <boehmseb@cs.uni-saarland.de>
2024-05-29 11:32:47 +0200

    Update perf inter case studies

--- varats/varats/projects/perf_tests/performance_interaction_case_studies.py
+++ varats/varats/projects/perf_tests/performance_interaction_case_studies.py
@@ -64,7 +64,7 @@ def _perf_inter_cs_binary(
     ).specify_binary(
         f"build/bin/{binary_name}",
         BinaryType.EXECUTABLE,
-        only_valid_in=RevisionRange("e3c3ab43f2", "HEAD")
+        only_valid_in=RevisionRange("636737b6e1", "HEAD")
     )
 
 
