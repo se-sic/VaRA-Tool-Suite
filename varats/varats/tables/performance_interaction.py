7e11b1bb88d2b62dda4f66ad1d99f3467da8bd7c
Sebastian Böhm <boehmseb@cs.uni-saarland.de>
2024-05-29 13:23:44 +0200

    Add perf-reg-synth table

--- varats/varats/tables/performance_interaction.py
+++ varats/varats/tables/performance_interaction.py
@@ -13,10 +13,19 @@ from varats.data.databases.performance_evolution_database import (
     PerformanceEvolutionDatabase,
 )
 from varats.data.metrics import ConfusionMatrix
+from varats.data.reports.performance_interaction_report import (
+    PerformanceInteractionReport,
+)
+from varats.experiments.base.time_workloads import TimeWorkloadsSynth
 from varats.experiments.vara.performance_interaction import (
     PerformanceInteractionExperiment,
+    PerformanceInteractionExperimentSynthetic,
 )
-from varats.jupyterhelper.file import load_performance_interaction_report
+from varats.jupyterhelper.file import (
+    load_performance_interaction_report,
+    load_mpr_wl_time_report_aggregate,
+    load_mpr_performance_interaction_report,
+)
 from varats.mapping.commit_map import get_commit_map, CommitMap
 from varats.mapping.configuration_map import ConfigurationMap
 from varats.paper.case_study import CaseStudy
@@ -46,8 +55,8 @@ class ConfusionMatrixData(tp.TypedDict):
 
 def is_regression(
     performance_data: pd.DataFrame,
-    old_rev: ShortCommitHash,
-    new_rev: ShortCommitHash,
+    old_rev: tp.Union[ShortCommitHash, str],
+    new_rev: tp.Union[ShortCommitHash, str],
     configs: ConfigurationMap,
     threshold: float,
     min_diff: float = 1
@@ -73,8 +82,18 @@ def is_regression(
     """
     num_regressions = 0
     for cid in configs.ids():
-        old_vals = ast.literal_eval(performance_data.loc[cid, old_rev])
-        new_vals = ast.literal_eval(performance_data.loc[cid, new_rev])
+        old_vals_raw = performance_data.loc[cid, old_rev]
+        new_vals_raw = performance_data.loc[cid, new_rev]
+
+        if isinstance(old_vals_raw, str):
+            old_vals = ast.literal_eval(old_vals_raw)
+        else:
+            old_vals = old_vals_raw
+
+        if isinstance(new_vals_raw, str):
+            new_vals = ast.literal_eval(new_vals_raw)
+        else:
+            new_vals = new_vals_raw
 
         std_old = np.std(old_vals)
         std_new = np.std(new_vals)
@@ -250,6 +269,221 @@ class PerformanceRegressionClassification(
     def generate(self) -> tp.List[Table]:
         return [
             PerformanceRegressionClassificationTable(
+                self.table_config, **self.table_kwargs
+            )
+        ]
+
+
+def load_synth_baseline_data(
+    case_study: CaseStudy, config_ids: tp.List[int]
+) -> pd.DataFrame:
+    project_name = case_study.project_name
+
+    data: tp.List[tp.Dict[str, tp.Any]] = []
+
+    for config_id in config_ids:
+        time_report_files = get_processed_revisions_files(
+            project_name,
+            TimeWorkloadsSynth,
+            file_name_filter=get_case_study_file_name_filter(case_study),
+            only_newest=True,
+            config_id=config_id
+        )
+
+        if not time_report_files:
+            LOG.warning(
+                f"No baseline report found for {project_name}:{config_id}, skipping."
+            )
+            continue
+
+        assert len(time_report_files) == 1
+        time_report = load_mpr_wl_time_report_aggregate(time_report_files[0])
+
+        baseline_report = time_report.get_baseline_report()
+        assert len(baseline_report.workload_names()) == 1
+        workload = next(iter(baseline_report.workload_names()))
+        data.append({
+            "revision":
+                "base",
+            "config_id":
+                config_id,
+            "wall_clock_time":
+                baseline_report.measurements_wall_clock_time(workload)
+        })
+
+        for patch_name in time_report.get_patch_names():
+            patched_report = time_report.get_report_for_patch(patch_name)
+            assert len(patched_report.workload_names()) == 1
+            workload = next(iter(patched_report.workload_names()))
+            data.append({
+                "revision":
+                    patch_name,
+                "config_id":
+                    config_id,
+                "wall_clock_time":
+                    patched_report.measurements_wall_clock_time(workload)
+            })
+
+    return pd.DataFrame.from_records(data)
+
+
+def load_synth_perf_inter_reports(
+    case_study: CaseStudy
+) -> tp.Dict[str, PerformanceInteractionReport]:
+    project_name = case_study.project_name
+
+    report_files = get_processed_revisions_files(
+        project_name,
+        PerformanceInteractionExperimentSynthetic,
+        file_name_filter=get_case_study_file_name_filter(case_study),
+        only_newest=True
+    )
+
+    if not report_files:
+        LOG.warning(
+            f"No performance interaction report found for {project_name}, skipping."
+        )
+        return {}
+
+    assert len(report_files) == 1
+    report = load_mpr_performance_interaction_report(report_files[0])
+
+    report_dict: tp.Dict[str:PerformanceInteractionReport] = {}
+    report_dict["base"] = report.get_baseline_report()
+    for patch_name in report.get_patch_names():
+        report_dict[patch_name] = report.get_report_for_patch(patch_name)
+
+    return report_dict
+
+
+class PerformanceRegressionClassificationTableSynth(
+    Table, table_name="perf_reg_synth"
+):
+    """Table for performance regression classification analysis for synthetic
+    case studies."""
+
+    def tabulate(self, table_format: TableFormat, wrap_table: bool) -> str:
+        thresholds = self.table_kwargs["threshold"]  # % diff
+        min_diff = self.table_kwargs["sigma"]  # times std
+
+        case_studies = get_loaded_paper_config().get_all_case_studies()
+
+        data: tp.List[pd.DataFrame] = []
+        for case_study in case_studies:
+            project_name = case_study.project_name
+
+            configs = load_configuration_map_for_case_study(
+                get_paper_config(), case_study, PlainCommandlineConfiguration
+            )
+
+            performance_data = load_synth_baseline_data(
+                case_study, configs.ids()
+            )
+
+            if performance_data.empty:
+                continue
+
+            revisions = performance_data["revision"].unique().tolist()
+            revisions.remove("base")
+            performance_data = performance_data.pivot(
+                index="config_id", columns="revision", values="wall_clock_time"
+            )
+
+            perf_inter_reports = load_synth_perf_inter_reports(case_study)
+
+            threshold_data: tp.Dict[
+                float,
+                ConfusionMatrixData] = defaultdict(lambda: defaultdict(list))
+            predicted = 0
+
+            for old_rev, new_rev in [
+                ("base", patch_name) for patch_name in revisions
+            ]:
+                report = perf_inter_reports.get(new_rev, None)
+
+                if (
+                    old_rev not in performance_data.columns or
+                    new_rev not in performance_data.columns
+                ):
+                    continue
+
+                if report is not None and report.performance_interactions:
+                    predicted += 1
+
+                for threshold in thresholds:
+                    current_data = threshold_data[threshold]
+
+                    is_reg, _ = is_regression(
+                        performance_data, old_rev, new_rev, configs, threshold,
+                        min_diff
+                    )
+
+                    if is_reg:
+                        current_data["actual_positive_values"].append(new_rev)
+                    else:
+                        current_data["actual_negative_values"].append(new_rev)
+
+                    # performance interaction classification
+                    if report is not None and report.performance_interactions:
+                        current_data["predicted_positive_values"].append(
+                            new_rev
+                        )
+                    else:
+                        current_data["predicted_negative_values"].append(
+                            new_rev
+                        )
+
+            cs_data: tp.Dict[tp.Any,
+                             tp.Any] = {("Project",): [project_name],
+                                        ("Revisions",): [len(revisions) + 1],
+                                        ("Predicted",): [predicted]}
+
+            for threshold in thresholds:
+                current_data = threshold_data[threshold]
+                confusion_matrix = ConfusionMatrix(
+                    current_data["actual_positive_values"],
+                    current_data["actual_negative_values"],
+                    current_data["predicted_positive_values"],
+                    current_data["predicted_negative_values"],
+                )
+
+                threshold_key = f"Threshold = {threshold}"
+                cs_data[(threshold_key, "Base")] = [confusion_matrix.P]
+                # cs_data[(threshold_key, "Pred")] = [confusion_matrix.PP]
+                cs_data[(threshold_key, "Rec.")] = [confusion_matrix.recall()]
+                cs_data[(threshold_key, "Prec.")
+                       ] = [confusion_matrix.precision()]
+
+            cs_df = pd.DataFrame.from_dict(cs_data)
+            cs_df.set_index("Project", inplace=True)
+            data.append(cs_df)
+
+        df = pd.concat(data).sort_index()
+
+        style = df.style
+        kwargs: tp.Dict[str, tp.Any] = {}
+        if table_format.is_latex():
+            kwargs["hrules"] = True
+            kwargs["column_format"] = "lrr" + "|rrr" * len(thresholds)
+            kwargs["multicol_align"] = "c"
+            style.format(precision=2, thousands=r"\,")
+
+        return dataframe_to_table(
+            df, table_format, style, wrap_table, wrap_landscape=True, **kwargs
+        )
+
+
+class PerformanceRegressionClassificationSynth(
+    TableGenerator,
+    generator_name="perf-reg-synth",
+    options=[OPTIONAL_THRESHOLDS, OPTIONAL_SIGMA]
+):
+    """Generates a table that does a precision/recall analysis for performance
+    regression detection for multiple thresholds."""
+
+    def generate(self) -> tp.List[Table]:
+        return [
+            PerformanceRegressionClassificationTableSynth(
                 self.table_config, **self.table_kwargs
             )
         ]
