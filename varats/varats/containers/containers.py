7fe64ee1a0d7362e69d4e3b41d538c5790958026
Sebastian Böhm <boehmseb@cs.uni-saarland.de>
2024-06-05 10:22:08 +0200

    Add missing dependency to debian 12 base image

--- varats/varats/containers/containers.py
+++ varats/varats/containers/containers.py
@@ -252,7 +252,7 @@ _BASE_IMAGES: tp.Dict[ImageBase, tp.Callable[[StageBuilder], None]] = {
                  'software-properties-common', 'musl-dev', 'git', 'gcc',
                  'libgit2-dev', 'libffi-dev', 'libyaml-dev', 'graphviz-dev',
                  'python3', 'python3-pip', 'python3-virtualenv', 'clang',
-                 'lld', 'time'))
+                 'lld', 'libc++-dev', 'libc++abi-dev', 'time'))
 }
 
 _STAGE_LAYERS: tp.Dict[ImageStage,
