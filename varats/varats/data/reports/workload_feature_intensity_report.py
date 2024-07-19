7d6713a9db01e80b3dbf4aace81f454eacf84659
Lukas Abelt <git@login.luabelt.de>
2024-07-19 13:13:20 +0200

    * Move feature region extraction to workload feature intensity report

--- varats/varats/data/reports/workload_feature_intensity_report.py
+++ varats/varats/data/reports/workload_feature_intensity_report.py
@@ -1,11 +1,17 @@
+import logging
 import typing as tp
 from collections import defaultdict
 from pathlib import Path
 from tempfile import TemporaryDirectory
 from zipfile import ZipFile
 
+from varats.data.databases.feature_perf_precision_database import (
+    get_interactions_from_fr_string,
+)
 from varats.report.report import BaseReport
-from varats.report.tef_report import TEFReport
+from varats.report.tef_report import TEFReport, TraceEvent, TraceEventType
+
+LOG = logging.getLogger(__name__)
 
 
 class WorkloadFeatureIntensityReport(
@@ -13,6 +19,66 @@ class WorkloadFeatureIntensityReport(
 ):
     """Report that aggregates the feature intensities for different binaries and
     workloads."""
+
+    def __extract_workload_name_from_report(self, report: TEFReport) -> str:
+        return report.filename.filename.split("/")[-1].split("_")[2]
+
+    def __get_feature_regions_from_tef_report(
+        tef_report: TEFReport,
+    ) -> tp.Dict[tp.Tuple[str, tp.Tuple[int, ...]], int]:
+        """Extract feature regions from a TEFReport."""
+        open_events: tp.List[TraceEvent] = []
+
+        feature_intensities: tp.Dict[tp.Tuple[str, tp.Tuple[int, ...]],
+                                     int] = defaultdict(int)
+
+        def get_matching_event(
+            open_events: tp.List[TraceEvent], closing_event: TraceEvent
+        ) -> tp.Optional[TraceEvent]:
+            for event in open_events:
+                if (
+                    event.uuid == closing_event.uuid and
+                    event.pid == closing_event.pid and
+                    event.tid == closing_event.tid
+                ):
+                    open_events.remove(event)
+                    return event
+
+            LOG.debug(
+                f"Could not find matching start for Event {repr(closing_event)}."
+            )
+
+            return None
+
+        found_missing_open_event = False
+        for trace_event in tef_report.trace_events:
+            if trace_event.category == "Feature":
+                if trace_event.event_type == TraceEventType.DURATION_EVENT_BEGIN:
+                    # insert event at the top of the list
+                    open_events.insert(0, trace_event)
+                elif trace_event.event_type == TraceEventType.DURATION_EVENT_END:
+                    opening_event = get_matching_event(open_events, trace_event)
+                    if not opening_event:
+                        found_missing_open_event = True
+                        continue
+
+                    interaction_names = [event.name for event in open_events]
+                    region_ids = tuple([event.uuid for event in open_events])
+
+                    interaction_string = get_interactions_from_fr_string(
+                        ",".join(interaction_names + [trace_event.name])
+                    )
+
+                    feature_intensities[(interaction_string, region_ids)] += 1
+
+        if open_events:
+            LOG.error("Not all events have been correctly closed.")
+            LOG.debug(f"Events = {open_events}.")
+
+        if found_missing_open_event:
+            LOG.error("Not all events have been correctly opened.")
+
+        return feature_intensities
 
     def __init__(self, path: Path):
         super().__init__(path)
@@ -43,6 +109,16 @@ class WorkloadFeatureIntensityReport(
         # Extract workloads from report file names
         # Report filenames are in format "feature_intensity_<workload>_0.json"
         return [
-            report.filename.filename.split("/")[-1].split("_")[2]
+            self.__extract_workload_name_from_report(report)
             for report in self.__reports[binary]
         ]
+
+    def reports_for_binary(self, binary: str) -> tp.Dict[str, TEFReport]:
+        return {
+            self.__extract_workload_name_from_report(report): report
+            for report in self.__reports[binary]
+        }
+
+    def intensities_for_binary(self,
+                               binary: str) -> tp.Dict[str, tp.Dict[str, int]]:
+        pass
--- varats/varats/tables/workload_feature_intensity.py
+++ varats/varats/tables/workload_feature_intensity.py
@@ -1,12 +1,5 @@
-import logging
 import typing as tp
-from collections import defaultdict
 
-import click
-
-from varats.data.databases.feature_perf_precision_database import (
-    get_interactions_from_fr_string,
-)
 from varats.data.reports.workload_feature_intensity_report import (
     WorkloadFeatureIntensityReport,
 )
@@ -16,75 +9,9 @@ from varats.experiments.vara.workload_feature_intensity import (
 from varats.paper.case_study import CaseStudy
 from varats.paper.paper_config import get_loaded_paper_config
 from varats.paper_mgmt.case_study import get_case_study_file_name_filter
-from varats.report.tef_report import (
-    TEFReportAggregate,
-    TEFReport,
-    TraceEvent,
-    TraceEventType,
-)
 from varats.revision.revisions import get_processed_revisions_files
 from varats.table.table import Table
 from varats.table.tables import TableFormat, TableGenerator
-
-LOG = logging.getLogger(__name__)
-
-
-def get_feature_regions_from_tef_report(
-    tef_report: TEFReport,
-) -> tp.Dict[tp.Tuple[str, tp.Tuple[int, ...]], int]:
-    """Extract feature regions from a TEFReport."""
-    open_events: tp.List[TraceEvent] = []
-
-    feature_intensities: tp.Dict[tp.Tuple[str, tp.Tuple[int, ...]],
-                                 int] = defaultdict(int)
-
-    def get_matching_event(
-        open_events: tp.List[TraceEvent], closing_event: TraceEvent
-    ) -> tp.Optional[TraceEvent]:
-        for event in open_events:
-            if (
-                event.uuid == closing_event.uuid and
-                event.pid == closing_event.pid and
-                event.tid == closing_event.tid
-            ):
-                open_events.remove(event)
-                return event
-
-        LOG.debug(
-            f"Could not find matching start for Event {repr(closing_event)}."
-        )
-
-        return None
-
-    found_missing_open_event = False
-    for trace_event in tef_report.trace_events:
-        if trace_event.category == "Feature":
-            if trace_event.event_type == TraceEventType.DURATION_EVENT_BEGIN:
-                # insert event at the top of the list
-                open_events.insert(0, trace_event)
-            elif trace_event.event_type == TraceEventType.DURATION_EVENT_END:
-                opening_event = get_matching_event(open_events, trace_event)
-                if not opening_event:
-                    found_missing_open_event = True
-                    continue
-
-                interaction_names = [event.name for event in open_events]
-                region_ids = tuple([event.uuid for event in open_events])
-
-                interaction_string = get_interactions_from_fr_string(
-                    ",".join(interaction_names + [trace_event.name])
-                )
-
-                feature_intensities[(interaction_string, region_ids)] += 1
-
-    if open_events:
-        LOG.error("Not all events have been correctly closed.")
-        LOG.debug(f"Events = {open_events}.")
-
-    if found_missing_open_event:
-        LOG.error("Not all events have been correctly opened.")
-
-    return feature_intensities
 
 
 class WorkloadIntensityTable(Table, table_name="workload_intensity"):
