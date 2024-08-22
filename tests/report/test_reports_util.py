22cb4cdc409203a4a2f8e311868ef17ecbb16b39
Lukas Abelt <git@login.luabelt.de>
2024-08-22 10:47:18 +0200

    * Finished tests for reports util

--- tests/report/test_reports_util.py
+++ tests/report/test_reports_util.py
@@ -11,19 +11,52 @@ class TestFeatureReportUtils(unittest.TestCase):
     """Tests utility functions for feature reports."""
 
     def test_extract_single_feature(self) -> None:
-        self.assertTrue(False)
+        self.assertEqual({"feature1"},
+                         extract_feature_region_set_from_string("FR(feature1)"))
+        self.assertEqual({"A"}, extract_feature_region_set_from_string("FR(A)"))
 
     def test_extract_two_features(self) -> None:
-        self.assertTrue(False)
+        self.assertEqual({
+            "feature1", "feature2"
+        }, extract_feature_region_set_from_string("FR(feature1,feature2)"))
+        self.assertEqual({"A", "B"},
+                         extract_feature_region_set_from_string("FR(A,B)"))
 
     def test_extract_multiple_features(self) -> None:
-        self.assertTrue(False)
+        self.assertEqual({"feature1", "feature2", "feature3"},
+                         extract_feature_region_set_from_string(
+                             "FR(feature1,feature2,feature3)"
+                         ))
+        self.assertEqual({"A", "B", "C"},
+                         extract_feature_region_set_from_string("FR(A,B,C)"))
+        self.assertEqual({
+            "A", "B", "C", "D", "E", "F", "G", "H"
+        }, extract_feature_region_set_from_string("FR(A,B,C,D,E,F,G,H)"))
 
     def test_single_feature_to_string(self) -> None:
-        self.assertTrue(False)
+        self.assertEqual(
+            "FR(feature1)", feature_region_string_from_set({"feature1"})
+        )
+        self.assertEqual("FR(A)", feature_region_string_from_set({"A"}))
 
     def test_two_features_to_string(self) -> None:
-        self.assertTrue(False)
+        self.assertEqual(
+            "FR(feature1,feature2)",
+            feature_region_string_from_set({"feature1", "feature2"})
+        )
+        self.assertEqual("FR(A,B)", feature_region_string_from_set({"A", "B"}))
 
     def test_multiple_features_to_string(self) -> None:
-        self.assertTrue(False)
+        self.assertEqual(
+            "FR(feature1,feature2,feature3)",
+            feature_region_string_from_set({"feature1", "feature2", "feature3"})
+        )
+        self.assertEqual(
+            "FR(A,B,C)", feature_region_string_from_set({"A", "B", "C"})
+        )
+        self.assertEqual(
+            "FR(A,B,C,D,E,F,G,H)",
+            feature_region_string_from_set({
+                "A", "B", "C", "D", "E", "F", "G", "H"
+            })
+        )
--- varats/varats/data/reports/reports_util.py
+++ varats/varats/data/reports/reports_util.py
@@ -9,7 +9,7 @@ def feature_region_string_from_set(feature_region_set: tp.Iterable[str]) -> str:
     @param feature_region_set: The feature region set to convert.
     @return: The string representation of the feature region set.
     """
-    return "FR(" + ",".join(feature_region_set) + ")"
+    return "FR(" + ",".join(sorted(feature_region_set)) + ")"
 
 
 def extract_feature_region_set_from_string(
