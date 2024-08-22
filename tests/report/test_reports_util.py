6d95ac14af90adf5eda95c72a77c6ddcb7603cad
Lukas Abelt <git@login.luabelt.de>
2024-08-22 10:38:20 +0200

    * Added test skeleton for report utils

---
+++ tests/report/test_reports_util.py
@@ -0,0 +1,29 @@
+"""Tests that the report utility functions work as expected."""
+import unittest
+
+from varats.data.reports.reports_util import (
+    feature_region_string_from_set,
+    extract_feature_region_set_from_string,
+)
+
+
+class TestFeatureReportUtils(unittest.TestCase):
+    """Tests utility functions for feature reports."""
+
+    def test_extract_single_feature(self) -> None:
+        self.assertTrue(False)
+
+    def test_extract_two_features(self) -> None:
+        self.assertTrue(False)
+
+    def test_extract_multiple_features(self) -> None:
+        self.assertTrue(False)
+
+    def test_single_feature_to_string(self) -> None:
+        self.assertTrue(False)
+
+    def test_two_features_to_string(self) -> None:
+        self.assertTrue(False)
+
+    def test_multiple_features_to_string(self) -> None:
+        self.assertTrue(False)
