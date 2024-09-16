f4f3653b09dbcd8d568e8eabdcef7d2ca69e684c
Sebastian Böhm <boehmseb@cs.uni-saarland.de>
2024-09-16 14:42:43 +0200

    Update HotFunctionsReport

--- varats-core/varats/report/hot_functions_report.py
+++ varats-core/varats/report/hot_functions_report.py
@@ -1,8 +1,9 @@
 import typing as tp
+from collections import defaultdict
 from dataclasses import dataclass
 from pathlib import Path
 
-from pandas import read_csv
+import pandas as pd
 
 from varats.experiment.workload_util import WorkloadSpecificReportAggregate
 from varats.report.report import BaseReport, ReportAggregate
@@ -14,6 +15,12 @@ class XRayFunctionWrapper:
     count: int
     sum_time: float
     self_time: float
+
+    def __str__(self) -> str:
+        return f"{self.name}[count={self.count}, sum={self.sum_time}, self={self.self_time}]"
+
+    def __repr__(self) -> str:
+        return str(self)
 
 
 def _ns_to_seconds(ns: int) -> float:
@@ -31,7 +38,28 @@ class HotFunctionReport(BaseReport, shorthand="HFR", file_type=".csv"):
 
     def __init__(self, path: Path) -> None:
         super().__init__(path)
-        self.__function_data = read_csv(path)
+        self.__function_data = pd.read_csv(path)
+
+    @staticmethod
+    def __create_function_wrapper(
+        function_data: pd.Series
+    ) -> XRayFunctionWrapper:
+        return XRayFunctionWrapper(
+            name=function_data["function"],
+            count=function_data["count"],
+            sum_time=_ns_to_seconds(function_data["sum"]),
+            self_time=_ns_to_seconds(function_data["self"])
+        )
+
+    @property
+    def total_time(self) -> float:
+        return _ns_to_seconds(self.__function_data["sum"].max())
+
+    def function(self, function_name: str) -> XRayFunctionWrapper:
+        return self.__create_function_wrapper(
+            self.__function_data[self.__function_data["function"] ==
+                                 function_name]
+        )
 
     def top_n_functions(self, limit: int = 10) -> tp.List[XRayFunctionWrapper]:
         """Determines the `n` hottest functions in which the most time was
@@ -40,12 +68,8 @@ class HotFunctionReport(BaseReport, shorthand="HFR", file_type=".csv"):
             by='self', ascending=False, inplace=True
         )
         return [
-            XRayFunctionWrapper(
-                name=row["function"],
-                count=row['count'],
-                sum_time=_ns_to_seconds(row["sum"]),
-                self_time=_ns_to_seconds(row["self"])
-            ) for _, row in self.__function_data.head(limit).iterrows()
+            self.__create_function_wrapper(row)
+            for _, row in self.__function_data.head(limit).iterrows()
         ]
 
     def hot_functions(self, threshold: int = 2) -> tp.List[XRayFunctionWrapper]:
@@ -75,12 +99,7 @@ class HotFunctionReport(BaseReport, shorthand="HFR", file_type=".csv"):
             self_time_cutoff = (total_time_tracked * threshold) / 100
 
         return [
-            XRayFunctionWrapper(
-                name=row["function"],
-                count=row['count'],
-                sum_time=_ns_to_seconds(row["sum"]),
-                self_time=_ns_to_seconds(row["self"])
-            )
+            self.__create_function_wrapper(row)
             for _, row in self.__function_data.iterrows()
             if row["self"] > self_time_cutoff
         ]
@@ -98,6 +117,9 @@ class WLHotFunctionAggregate(
     def __init__(self, path: Path) -> None:
         super().__init__(path, HotFunctionReport)
 
+    def total_times(self, workload_name: str) -> tp.List[float]:
+        return [report.total_time for report in self.reports(workload_name)]
+
     def dump_all_reports(self) -> None:
         """Dumps the contents of all loaded hot functions reports."""
         for wl_name in self.workload_names():
@@ -105,16 +127,26 @@ class WLHotFunctionAggregate(
                 report.print_full_dump()
 
     def hot_functions_per_workload(
-        self, threshold: int = 2
-    ) -> tp.Dict[str, tp.List[XRayFunctionWrapper]]:
+        self,
+        threshold: int = 2
+    ) -> tp.Dict[str, tp.Dict[str, tp.List[XRayFunctionWrapper]]]:
         """
         Args:
             threshold: min percentage a function needs as self time to count as hot
         """
-        res: tp.Dict[str, tp.List[XRayFunctionWrapper]] = {}
+        res: tp.Dict[str, tp.Dict[str, tp.List[XRayFunctionWrapper]]] = {}
         for wl_name in self.workload_names():
-            # TODO: repetition handling
-            for report in self.reports(wl_name):
-                res[wl_name] = report.hot_functions(threshold=threshold)
+            wl_hot_funcs: tp.Dict[
+                str, tp.List[XRayFunctionWrapper]] = defaultdict(list)
+            wl_reports = self.reports(wl_name)
+            for report in wl_reports:
+                for func in report.hot_functions(threshold=threshold):
+                    wl_hot_funcs[func.name].append(func)
+
+            # only consider hot functions that are hot in all repetitions
+            res[wl_name] = {}
+            for func_name, funcs in wl_hot_funcs.items():
+                # if len(funcs) == len(wl_reports):
+                res[wl_name][func_name] = funcs
 
         return res
