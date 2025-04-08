b523eff8e9aae952a76804aa1d1e2940bb5971b1
Lukas Abelt <git@login.luabelt.de>
2025-04-08 11:14:20 +0200

    * Apply fixes for mypy and pylint

--- varats-core/varats/experiment/steps/combinators.py
+++ varats-core/varats/experiment/steps/combinators.py
@@ -1,3 +1,4 @@
+"""Collection of combinator experiment steps."""
 import textwrap
 import typing as tp
 from pathlib import Path
@@ -13,8 +14,8 @@ def _run_with_output_if_necessary(
 ) -> StepResult:
     if isinstance(step, OutputFolderStep):
         return step.call_with_output_folder(tmp_dir)
-    else:
-        return step()
+
+    return step()
 
 
 class IfThenElse(OutputFolderStep):
@@ -28,7 +29,7 @@ class IfThenElse(OutputFolderStep):
         self,
         project: Project,
         condition: ProjectStep,
-        valid_results: tp.List[StepResult] = None,
+        valid_results: tp.Optional[tp.List[StepResult]] = None,
         then_step: tp.Optional[ProjectStep] = None,
         else_step: tp.Optional[ProjectStep] = None
     ) -> None:
@@ -36,14 +37,18 @@ class IfThenElse(OutputFolderStep):
         self.__condition = condition
         self.__then_step = then_step
         self.__else_step = else_step
-        self.__valid_results = valid_results
-        if self.__valid_results is None:
+        if valid_results is None:
             self.__valid_results = [StepResult.OK, StepResult.CAN_CONTINUE]
+        else:
+            self.__valid_results = valid_results
 
     def call_with_output_folder(self, tmp_dir: Path) -> StepResult:
         result = _run_with_output_if_necessary(tmp_dir, self.__condition)
 
-        run_step = self.__then_step if result in self.__valid_results else self.__else_step
+        if result in self.__valid_results:
+            run_step = self.__then_step
+        else:
+            run_step = self.__else_step
 
         if not run_step:
             return result
@@ -53,7 +58,10 @@ class IfThenElse(OutputFolderStep):
     def __call__(self) -> StepResult:
         result = self.__condition()
 
-        run_step = self.__then_step if result == StepResult.OK or result == StepResult.CAN_CONTINUE else self.__else_step
+        if result in self.__valid_results:
+            run_step = self.__then_step
+        else:
+            run_step = self.__else_step
 
         if not run_step:
             return result
@@ -61,19 +69,19 @@ class IfThenElse(OutputFolderStep):
         return run_step()
 
     def __str__(self, indent: int = 0) -> str:
-        outstr = textwrap.indent(f"* If: ", indent * " ") + "\n"
+        outstr = textwrap.indent("* If: ", indent * " ") + "\n"
         outstr += textwrap.indent(
             str(self.__condition), (indent + 2) * " "
         ) + "\n"
 
         if self.__then_step:
-            outstr += textwrap.indent(f"* Then: ", indent * " ") + "\n"
+            outstr += textwrap.indent("* Then: ", indent * " ") + "\n"
             outstr += textwrap.indent(
                 str(self.__then_step), (indent + 2) * " "
             ) + "\n"
 
         if self.__else_step:
-            outstr += textwrap.indent(f"* Else: ", indent * " ") + "\n"
+            outstr += textwrap.indent("* Else: ", indent * " ") + "\n"
             outstr += textwrap.indent(
                 str(self.__else_step), (indent + 2) * " "
             ) + "\n"
