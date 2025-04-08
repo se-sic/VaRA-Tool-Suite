bfe5c488cf8de7e2ff46671d81f2c9a9475ec9f1
Lukas Abelt <git@login.luabelt.de>
2025-04-08 10:39:41 +0200

    * Added suggestions from code review

--- varats-core/varats/experiment/steps/ifthenelse.py
+++ varats-core/varats/experiment/steps/ifthenelse.py
@@ -28,18 +28,22 @@ class IfThenElse(OutputFolderStep):
         self,
         project: Project,
         condition: ProjectStep,
-        if_step: tp.Optional[ProjectStep] = None,
+        valid_results: tp.List[StepResult] = None,
+        then_step: tp.Optional[ProjectStep] = None,
         else_step: tp.Optional[ProjectStep] = None
     ) -> None:
         super().__init__(project)
         self.__condition = condition
-        self.__if_step = if_step
+        self.__then_step = then_step
         self.__else_step = else_step
+        self.__valid_results = valid_results
+        if self.__valid_results is None:
+            self.__valid_results = [StepResult.OK, StepResult.CAN_CONTINUE]
 
     def call_with_output_folder(self, tmp_dir: Path) -> StepResult:
         result = _run_with_output_if_necessary(tmp_dir, self.__condition)
 
-        run_step = self.__if_step if result == StepResult.OK or result == StepResult.CAN_CONTINUE else self.__else_step
+        run_step = self.__then_step if result in self.__valid_results else self.__else_step
 
         if not run_step:
             return result
@@ -49,7 +53,7 @@ class IfThenElse(OutputFolderStep):
     def __call__(self) -> StepResult:
         result = self.__condition()
 
-        run_step = self.__if_step if result == StepResult.OK or result == StepResult.CAN_CONTINUE else self.__else_step
+        run_step = self.__then_step if result == StepResult.OK or result == StepResult.CAN_CONTINUE else self.__else_step
 
         if not run_step:
             return result
@@ -61,12 +65,16 @@ class IfThenElse(OutputFolderStep):
         outstr += textwrap.indent(
             str(self.__condition), (indent + 2) * " "
         ) + "\n"
-        outstr += textwrap.indent(f"* Then: ", indent * " ") + "\n"
-        outstr += textwrap.indent(
-            str(self.__if_step), (indent + 2) * " "
-        ) + "\n"
-        outstr += textwrap.indent(f"* Else: ", indent * " ") + "\n"
-        outstr += textwrap.indent(
-            str(self.__else_step), (indent + 2) * " "
-        ) + "\n"
+
+        if self.__then_step:
+            outstr += textwrap.indent(f"* Then: ", indent * " ") + "\n"
+            outstr += textwrap.indent(
+                str(self.__then_step), (indent + 2) * " "
+            ) + "\n"
+
+        if self.__else_step:
+            outstr += textwrap.indent(f"* Else: ", indent * " ") + "\n"
+            outstr += textwrap.indent(
+                str(self.__else_step), (indent + 2) * " "
+            ) + "\n"
         return outstr
