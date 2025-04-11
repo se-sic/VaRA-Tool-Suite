8e3df806383357b2903d8f5ab6e800dc63c6ef1b
Johannes Weissmann <s8jsweis@stud.uni-saarland.de>
2025-04-11 15:48:47 +0200

    adds an option which enables the analysis pass on VaRA's side (I forgot to commit this change like a year ago.)

--- varats/varats/experiments/vara/architecture_report_experiment.py
+++ varats/varats/experiments/vara/architecture_report_experiment.py
@@ -51,9 +51,8 @@ class ArchitectureAnalysis(actions.ProjectStep):  # type: ignore
             )
 
             opt_params = [
-                "--enable-new-pm=0",
-                f"-vara-report-outfile={result_file}",
-                # TODO an option which enables the analysis pass on VaRA's side
+                "--enable-new-pm=0", f"-vara-report-outfile={result_file}",
+                "-vara-AD", "-vara-AR",
                 get_cached_bc_file_path(
                     self.project, binary,
                     [BCFileExtensions.NO_OPT, BCFileExtensions.ARCH]
