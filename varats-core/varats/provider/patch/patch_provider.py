a697a826b6c47dbfa37cea221db7e04e444119b8
Lukas Abelt <git@login.luabelt.de>
2025-05-20 14:23:13 +0200

    - Minor fix in patch provider

--- varats-core/varats/provider/patch/patch_provider.py
+++ varats-core/varats/provider/patch/patch_provider.py
@@ -241,7 +241,8 @@ class Patch:
         if project_step:
             # Generate a random name for the patch file
             rendered_path = (
-                project_step.project.builddir / f"self.shortname-{uuid.uuid4()}"
+                project_step.project.builddir /
+                f"{self.shortname}-{uuid.uuid4()}"
             )
             with open(str(rendered_path), "wb") as f:
                 f.write(rendered.encode())
