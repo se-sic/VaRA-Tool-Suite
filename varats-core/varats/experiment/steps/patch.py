d3a59fccb34ea7c3581c9352c504d8004c3c266d
Lukas Abelt <git@login.luabelt.de>
2025-07-18 14:05:39 +0200

    - Add possibility to add a rendered name for patches with arguments

--- varats-core/varats/experiment/steps/patch.py
+++ varats-core/varats/experiment/steps/patch.py
@@ -33,7 +33,7 @@ class ApplyPatch(actions.ProjectStep):
     def __call__(self) -> StepResult:
         self.status = StepResult.OK
         print(
-            f"Applying {self.__patch.shortname} to "
+            f"Applying {self.__patch.rendered_name(self.__arguments)} to "
             f"{self.project.source_of_primary}"
         )
 
@@ -41,7 +41,7 @@ class ApplyPatch(actions.ProjectStep):
             patch_path = self.__patch.render(self, **self.__arguments)
         except TemplateError:
             print(
-                f"Failed to render patch {self.__patch.shortname} "
+                f"Failed to render patch {self.__patch.rendered_name(self.__arguments)} "
                 f"with arguments:"
                 f" {_build_args_string(**self.__arguments)}"
             )
