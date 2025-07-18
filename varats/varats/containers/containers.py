60157e5401ac0b0c96cf12886f35d13cbf9179e1
Sebastian Böhm <boehmseb@cs.uni-saarland.de>
2025-07-18 10:06:39 +0200

    container: setup basic git config.

--- varats/varats/containers/containers.py
+++ varats/varats/containers/containers.py
@@ -252,7 +252,10 @@ _BASE_IMAGES: tp.Dict[ImageBase, tp.Callable[[StageBuilder], None]] = {
                  'software-properties-common', 'musl-dev', 'git', 'gcc',
                  'libgit2-dev', 'libffi-dev', 'libyaml-dev', 'graphviz-dev',
                  'python3', 'python3-pip', 'python3-virtualenv', 'clang',
-                 'lld', 'libc++-dev', 'libc++abi-dev', 'time', 'linux-perf'))
+                 'lld', 'libc++-dev', 'libc++abi-dev', 'time', 'linux-perf')
+            # setup git config
+            .run('git', 'config', '--global', 'user.name', 'VaRA-Tool-Suite')
+            .run('git', 'config', '--global', 'user.email', 'varats@container'))
 }
 
 _STAGE_LAYERS: tp.Dict[ImageStage,
