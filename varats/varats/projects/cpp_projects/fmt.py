38b675a8eeb5e52764a8b435542d4f1b59d613ba
Sebastian Böhm <boehmseb@cs.uni-saarland.de>
2025-07-21 20:26:07 +0200

    Adds fmt as a new project.

---
+++ varats/varats/projects/cpp_projects/fmt.py
@@ -0,0 +1,72 @@
+"""Project file for fmt."""
+import typing as tp
+
+import benchbuild as bb
+from benchbuild.utils.cmd import make, cmake
+from benchbuild.utils.settings import get_number_of_jobs
+from plumbum import local
+
+from varats.containers.containers import get_base_image, ImageBase
+from varats.paper.paper_config import PaperConfigSpecificGit
+from varats.project.project_domain import ProjectDomains
+from varats.project.project_util import (
+    ProjectBinaryWrapper,
+    BinaryType,
+    get_local_project_repo,
+    verify_binaries,
+    RevisionBinaryMap,
+)
+from varats.project.varats_project import VProject
+from varats.utils.git_util import ShortCommitHash
+from varats.utils.settings import bb_cfg
+
+
+class Fmt(VProject):
+    """A modern formatting library."""
+
+    NAME = 'fmt'
+    GROUP = 'cpp_projects'
+    DOMAIN = ProjectDomains.CPP_LIBRARY
+
+    SOURCE = [
+        PaperConfigSpecificGit(
+            project_name="fmt",
+            remote="https://github.com/fmtlib/fmt.git",
+            local="fmt",
+            refspec="origin/HEAD",
+            limit=None,
+            shallow=False
+        )
+    ]
+
+    CONTAINER = get_base_image(ImageBase.DEBIAN_12)
+
+    @staticmethod
+    def binaries_for_revision(
+        revision: ShortCommitHash
+    ) -> tp.List[ProjectBinaryWrapper]:
+        binary_map = RevisionBinaryMap(get_local_project_repo(Fmt.NAME))
+
+        binary_map.specify_binary('build/libfmt.a', BinaryType.STATIC_LIBRARY)
+
+        return binary_map[revision]
+
+    def run_tests(self) -> None:
+        pass
+
+    def compile(self) -> None:
+        """Compile the project."""
+        fmt_source = local.path(self.primary_source)
+
+        c_compiler = bb.compiler.cc(self)
+        cxx_compiler = bb.compiler.cxx(self)
+        build_folder = fmt_source / "build"
+        build_folder.mkdir()
+        with local.cwd(build_folder):
+            with local.env(CC=str(c_compiler), CXX=str(cxx_compiler)):
+                bb.watch(cmake)("..")
+
+            bb.watch(make)("-j", get_number_of_jobs(bb_cfg()))
+
+        with local.cwd(fmt_source):
+            verify_binaries(self)
