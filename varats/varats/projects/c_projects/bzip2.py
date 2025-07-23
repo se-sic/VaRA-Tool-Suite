87f91fb297ed3f99b833ca776b0cbb9a301d5b49
Gabrian Giovano Tanasal <gabriantanasal@gmail.com>
2025-07-23 22:09:24 +0200

    bzip2 build_tests fix from "make tests" to only "make" so it also compile the program

--- varats/varats/projects/c_projects/bzip2.py
+++ varats/varats/projects/c_projects/bzip2.py
@@ -201,7 +201,7 @@ class Bzip2(VProject):
         bzip2_version_source = local.path(self.source_of_primary)
 
         with local.cwd(bzip2_version_source / "build"):
-            bb.watch(make)("test")
+            bb.watch(make)
 
     def get_test_names(self) -> tp.Iterable[str]:
         """Get the test names."""
