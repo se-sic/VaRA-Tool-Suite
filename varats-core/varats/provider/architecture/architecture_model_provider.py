fd42b9d94ba40e29551403ee124a13238c1d1150
Nerum <nerumlp@gmail.com>
2025-07-31 12:41:13 +0200

    add proper architecture model provider

--- varats-core/varats/provider/architecture/architecture_model_provider.py
+++ varats-core/varats/provider/architecture/architecture_model_provider.py
@@ -1,6 +1,9 @@
 import typing as tp
+from pathlib import Path
 
+import benchbuild as bb
 from benchbuild.project import Project
+from benchbuild.source.base import target_prefix
 
 from varats.provider.provider import Provider
 from varats.utils.filesystem_util import lock_file
@@ -8,6 +11,8 @@ from varats.utils.filesystem_util import lock_file
 
 class ArchitectureModelProvider(Provider):
     """Provider for accessing project related ArchitectureModels."""
+
+    am_repository = "https://github.com/Sinerum/ArchitectureModels.git"
 
     @classmethod
     def create_provider_for_project(
@@ -36,14 +41,46 @@ class ArchitectureModelProvider(Provider):
             "All usages should be covered by the project specific provider."
         )
 
-    def get_architecture_model_path(self, project: Project) -> str:
+    def get_architecture_model_path(
+        self,
+        # Currently, unused until pascals impl is ready
+        revision: str  # pylint: disable=W0613
+    ) -> tp.Optional[Path]:
         """
-        Get the path to the architecture model for the given project.
+        Get the path to a architecture model for a specific `revision` that
+        describes the architectures of a project and their relationships. In
+        case that no architecture model exists `None` is returned.
 
         Args:
-            project: The project to get the architecture model for.
+            revision: of the project, specifying for which state of the project
+                      the architecture model needs to be valid.
 
-        Returns:
-            The path to the architecture model.
+        Returns: a path to the corresponding architecture model
         """
-        return f"/home/simon/Workspace/Uni/Vara/vara/example.yaml"
+        project_name = self.project.NAME.lower()
+
+        fully_qualified_am_name = "ArchitectureModel"
+
+        for project_dir in self._get_architecture_model_repository_path(
+        ).iterdir():
+            if project_dir.name.lower() == project_name:
+                for poss_am_file in project_dir.iterdir():
+                    if poss_am_file.stem == fully_qualified_am_name:
+                        return poss_am_file
+
+        return None
+
+    @staticmethod
+    def _get_architecture_model_repository_path() -> Path:
+        fm_source = bb.source.Git(
+            remote=ArchitectureModelProvider.am_repository,
+            local="ArchitectureModels",
+            refspec="origin/HEAD",
+            limit=1,
+        )
+
+        lock_path = Path(target_prefix()) / "am_provider.lock"
+        with lock_file(lock_path):
+            fm_source.fetch()
+
+        return Path(Path(target_prefix()) / fm_source.local)
