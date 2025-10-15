4e02507240bcab62376a88a4fafebc1ebfe9ccdc
Gabrian Giovano Tanasal <gabriantanasal@gmail.com>
2025-10-15 13:23:28 +0200

    trying out update_all_submodules

--- varats/varats/projects/cpp_projects/hyteg.py
+++ varats/varats/projects/cpp_projects/hyteg.py
@@ -8,6 +8,7 @@ import benchbuild as bb
 from benchbuild.command import WorkloadSet, SourceRoot
 from benchbuild.utils.cmd import ninja, cmake, mkdir
 from benchbuild.utils.revision_ranges import SingleRevision
+from github.Repository import Repository
 from plumbum import local
 
 from varats.experiment.workload_util import WorkloadCategory, RSBinary
@@ -23,7 +24,7 @@ from varats.project.sources import FeatureSource
 from varats.project.varats_command import VCommand
 from varats.project.varats_project import VProject
 from varats.utils.git_commands import update_all_submodules
-from varats.utils.git_util import ShortCommitHash
+from varats.utils.git_util import ShortCommitHash, RepositoryHandle
 from varats.utils.settings import bb_cfg
 from varats.utils.testsuite_utils import (
     TestResult,
@@ -162,7 +163,9 @@ class HyTeg(VProject):
 
         mkdir("-p", hyteg_source / "build")
 
-        update_all_submodules(hyteg_source, recursive=True, init=True)
+        update_all_submodules(
+            RepositoryHandle(hyteg_source), recursive=True, init=True
+        )
         # with local.cwd(hyteg_source):
         #     local["git"]["submodule"]("update", "--init", "--recursive")
 
