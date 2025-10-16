1ff7993011734669e86a56a8b78f705077844df8
Gabrian Giovano Tanasal <gabriantanasal@gmail.com>
2025-10-16 20:37:42 +0200

    adding exclude test functionality to fast_downward

--- varats/varats/projects/cpp_projects/fast_downward.py
+++ varats/varats/projects/cpp_projects/fast_downward.py
@@ -129,6 +129,11 @@ class FastDownward(VProject, ReleaseProviderHook):
         # to be installed.
         # Since this is usually not the case, we skip this test
         test_runner = pytest["-k", "not test_commandline_args"]
+        if tests_to_exclude:
+            exclude_regex = ' and '.join([
+                f"'not {re.escape(name)}'" for name in tests_to_exclude
+            ])
+            test_runner = test_runner["-k", f"not ({exclude_regex})"]
 
         if test_report_path:
             test_runner = test_runner["--junitxml", test_report_path]
