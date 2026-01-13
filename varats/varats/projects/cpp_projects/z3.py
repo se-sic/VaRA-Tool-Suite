105c68b9be3d44f2779f08478ae0fe734bf6350c
Gabrian Giovano Tanasal <gabriantanasal@gmail.com>
2026-01-13 13:09:20 +0100

    z3 update run_testsuite

--- varats/varats/projects/cpp_projects/z3.py
+++ varats/varats/projects/cpp_projects/z3.py
@@ -147,7 +147,7 @@ class Z3(VProject, ReleaseProviderHook):
     ) -> tp.Optional[tp.Dict[str, TestResult]]:
         z3_source = Path(self.source_of(self.primary_source))
         if not tests_to_run:
-            all_tests = self.get_test_names()
+            tests_to_run = self.get_test_names()
         results: tp.Dict[str, TestResult] = {}
         if test_report_path:
             aggregated_results = test_report_path / "aggregated_test_results.zip"
