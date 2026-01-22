0961fea3b3721c017ff3b35bcc9e1a4c31499bfd
Gabrian Giovano Tanasal <gabriantanasal@gmail.com>
2026-01-22 12:40:52 +0100

    adding example workload (untested)

--- varats/varats/projects/c_projects/x264.py
+++ varats/varats/projects/c_projects/x264.py
@@ -3,12 +3,14 @@ import typing as tp
 from pathlib import Path
 
 import benchbuild as bb
+from benchbuild.command import SourceRoot, WorkloadSet
 from benchbuild.utils.cmd import make
 from benchbuild.utils.revision_ranges import block_revisions, GoodBadSubgraph
 from benchbuild.utils.settings import get_number_of_jobs
 from plumbum import local
 
 from varats.containers.containers import get_base_image, ImageBase
+from varats.experiment.workload_util import RSBinary, WorkloadCategory
 from varats.paper.paper_config import PaperConfigSpecificGit
 from varats.project.project_domain import ProjectDomains
 from varats.project.project_util import (
@@ -17,8 +19,9 @@ from varats.project.project_util import (
     BinaryType,
     verify_binaries,
     RevisionBinaryMap,
+    HTTP7z,
 )
-from varats.project.varats_project import VProject
+from varats.project.varats_project import VProject, VCommand
 from varats.utils.git_util import ShortCommitHash, get_all_revisions_between
 from varats.utils.settings import bb_cfg
 from varats.utils.testsuite_utils import (
@@ -49,10 +52,48 @@ class X264(VProject):
                 limit=None,
                 shallow=False
             )
-        )
+        ),
+        HTTP7z(
+            local="Bosphorus_1920x1080_120fps_420_8bit_YUV_Y4M",
+            remote={
+                "1.0":
+                    "http://ultravideo.cs.tut.fi/video/"
+                    "Bosphorus_1920x1080_120fps_420_8bit_YUV_Y4M.7z"
+            }
+        ),
+        HTTP7z(
+            local="Bosphorus_3840x2160_120fps_420_8bit_YUV_Y4M",
+            remote={
+                "1.0":
+                    "http://ultravideo.cs.tut.fi/video/"
+                    "Bosphorus_3840x2160_120fps_420_8bit_YUV_Y4M.7z"
+            }
+        ),
     ]
 
-    CONTAINER = get_base_image(ImageBase.DEBIAN_10)
+    WORKLOADS = {
+        WorkloadSet(WorkloadCategory.EXAMPLE): [
+            VCommand(
+                SourceRoot("x264") / RSBinary("x264"),
+                "--preset medium",
+                "--crf 24",
+                "--frames 300",
+                "--threads auto",
+                # Use output_param to ensure input file
+                # gets appended after all arguments.
+                output_param=["{output}"],
+                output=SourceRoot(
+                    "Bosphorus_1920x1080_120fps_420_8bit_YUV_Y4M/Bosphorus_1920x1080_120fps_420_8bit_YUV_Y4M.y4m"
+                ),
+                label="1080-medium-preset",
+                creates=[
+                    "Bosphorus_1920x1080_120fps_420_8bit_YUV_Y4M/Bosphorus_1920x1080_120fps_420_8bit_YUV_Y4M.y4m"
+                ]
+            )
+        ]
+    }
+
+    CONTAINER = get_base_image(ImageBase.DEBIAN_12)
 
     @staticmethod
     def binaries_for_revision(
@@ -129,7 +170,6 @@ class X264(VProject):
                     configure_flags.append("--extra-ldflags=\"-static\"")
                 bb.watch(local["./configure"])(configure_flags)
 
-    # Do i just create my own test?
     def get_test_names(self) -> tp.Iterable[str]:
         pass
 
@@ -146,3 +186,8 @@ class X264(VProject):
         tests_to_exclude: tp.Optional[tp.Iterable[str]] = None
     ) -> tp.Optional[tp.Dict[str, TestResult]]:
         pass
+
+
+# http://www.phoronix-test-suite.com/benchmark-files/x264-git-20220222.tar.bz2 700 ish kb
+# http://ultravideo.cs.tut.fi/video/Bosphorus_1920x1080_120fps_420_8bit_YUV_Y4M.7z 670 mb
+# http://ultravideo.cs.tut.fi/video/Bosphorus_3840x2160_120fps_420_8bit_YUV_Y4M.7z 2.8 gb
