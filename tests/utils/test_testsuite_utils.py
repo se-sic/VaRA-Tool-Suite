d9b300be86bef6eefe8ad61af4a9ced3e2cc8fe8
Gabrian Giovano Tanasal <gabriantanasal@gmail.com>
2026-01-29 08:26:39 +0100

    fixing test_ctest_get_test_names()

--- tests/utils/test_testsuite_utils.py
+++ tests/utils/test_testsuite_utils.py
@@ -1,6 +1,9 @@
 import benchbuild as bb
+from benchbuild.utils.cmd import cmake, make, mkdir
+from benchbuild.utils.settings import get_number_of_jobs
 
 from tests.helper_utils import TEST_INPUTS_DIR
+from varats.utils.settings import bb_cfg
 from varats.utils.testsuite_utils import *
 
 
@@ -18,6 +21,14 @@ def test_parse_junit_xml() -> None:
 def test_ctest_get_test_names() -> None:
     test_report_path = TEST_INPUTS_DIR / "ctest_get_names"
     build_dir = test_report_path / "build"
+
+    mkdir(build_dir, "-p")
+    clang = bb.compiler.cc(test_report_path)
+    cxx = bb.compiler.cxx(test_report_path)
+    with local.cwd(build_dir):
+        with local.env(CC=str(clang), CXX=str(cxx)):
+            bb.watch(cmake)("-G", "Unix Makefiles", "..")
+        bb.watch(make)("-j", get_number_of_jobs(bb_cfg()))
     test_names = ctest_get_test_names(build_dir)
 
     expected_test_names = ["time", "fail", "timeout"]
