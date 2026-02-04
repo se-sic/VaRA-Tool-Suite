c0eb4b0f1672c1eadd95f0d0e67dfdb7dacb5016
Lukas Abelt <git@login.luabelt.de>
2026-02-04 12:31:01 +0100

    - Move benchbase protocol to separate file

---
+++ varats/varats/experiments/hidden_config/database_utils.py
@@ -0,0 +1,36 @@
+from typing_extensions import runtime_checkable, Protocol
+
+from varats.project.project_util import ProjectBinaryWrapper
+
+
+@runtime_checkable
+class SupportsBenchbase(Protocol):
+
+    def get_database_name(self) -> str:
+        """Get the name of the database associated with this project."""
+        ...
+
+    def get_benchbase_profile_name(self) -> str:
+        """Get the BenchBase profile name for this project."""
+        ...
+
+    def get_database_connection_string(self) -> str:
+        """Get the connection string for the database associated with this
+        project."""
+        ...
+
+    def database_binary(self) -> ProjectBinaryWrapper:
+        """Get the binary used to interact with the database associated with
+        this project."""
+        ...
+
+    def start_database_server(self) -> None:
+        """Start the database server."""
+        ...
+
+    def stop_database_server(self) -> None:
+        """Stop the database server."""
+        ...
+
+    def render_workload_config(self, workload: str, configuration):
+        ...
