1b63e94dff34273d931365d6fde1c7843de7f1e6
ReebaAslam <35392562+ReebaAslam@users.noreply.github.com>
2026-04-11 19:04:57 +0500

    Apply suggestion from @ReebaAslam

--- varats/varats/tools/driver_run.py
+++ varats/varats/tools/driver_run.py
@@ -15,7 +15,6 @@ from subprocess import PIPE
 
 import click
 import jinja2
-from benchbuild.environments.adapters.common import run_tee
 from benchbuild.utils.cmd import benchbuild, sbatch
 from benchbuild.utils.settings import to_yaml
 from plumbum import local
