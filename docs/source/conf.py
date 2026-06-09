4c5d18f813c45e848e744baec152eb907b008aa3
Sebastian Böhm <boehmseb@cs.uni-saarland.de>
2026-06-09 16:48:06 +0200

    Be more specific with sys.path.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,9 @@ import benchbuild.utils
 
 __version__ = metadata.version('varats')
 
-sys.path.insert(0, os.path.abspath('../../'))
+sys.path.append(os.path.abspath('../../varats-core'))
+sys.path.append(os.path.abspath('../../varats'))
+sys.path.append(os.path.abspath('../../tests'))
 
 # pylint: skip-file
 project = 'VaRA'
