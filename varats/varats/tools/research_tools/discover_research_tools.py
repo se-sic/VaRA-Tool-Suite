f3acee594ac0c609db6e6e68ef836f1edf42b20e
Reeba Aslam <reas00001@gitlab.cs.uni-saarland.de>
2026-06-26 10:28:44 +0200

    maded phasar non-abstract

--- varats/varats/tools/research_tools/discover_research_tools.py
+++ varats/varats/tools/research_tools/discover_research_tools.py
@@ -3,7 +3,6 @@
 from pathlib import Path
 
 import varats.utils.external_source_handling as esh
-from varats.tools.research_tools.research_tool import ResearchTool
 from varats.utils.settings import vara_cfg
 
 
@@ -11,16 +10,11 @@ def initialize_research_tools() -> None:
     """Initialize builtin and external research tools."""
     # Import builtin tools to trigger automatic registration via
     # __init_subclass__
-    from varats.tools.research_tools.phasar import Phasar  # noqa: PLC0415
+    from varats.tools.research_tools.phasar import Phasar  # noqa: PLC0415, F401
     from varats.tools.research_tools.szz_unleashed import (  # noqa: PLC0415,F401
         SZZUnleashed,
     )
     from varats.tools.research_tools.vara import VaRA  # noqa: PLC0415, F401
-
-    # Phasar is abstract (has unimplemented methods),
-    # so __init_subclass__ skips it.
-    # Registering it manually.
-    ResearchTool.REGISTRY["phasar"] = Phasar
 
     # Load external research tools from registered repositories
     extra_sources = vara_cfg()['external_source_repositories'].value
