e024b43c2ea99d6f3d6e361d023cd7196d7291f2
Sebastian Böhm <boehmseb@cs.uni-saarland.de>
2026-07-21 10:35:47 +0200

    containers: install zip in base image.

--- varats/varats/containers/containers.py
+++ varats/varats/containers/containers.py
@@ -12,36 +12,42 @@ from benchbuild.environments import bootstrap
 from benchbuild.environments.adapters.common import buildah_version
 from benchbuild.environments.domain.commands import (
     CreateImage,
-    fs_compliant_name,
-    ExportImage,
     DeleteImage,
+    ExportImage,
     RunProjectContainer,
+    fs_compliant_name,
 )
 from benchbuild.environments.domain.declarative import (
-    add_benchbuild_layers,
     ContainerImage,
+    add_benchbuild_layers,
 )
-from benchbuild.utils.settings import to_yaml, get_number_of_jobs
+from benchbuild.utils.settings import get_number_of_jobs, to_yaml
 from plumbum import local
 
 from varats.tools.research_tools.research_tool import (
-    Distro,
     ContainerInstallable,
+    Distro,
 )
-from varats.tools.research_tools.vara_manager import BuildType
 from varats.tools.tool_util import get_research_tool
-from varats.utils.settings import bb_cfg, vara_cfg, save_bb_config
+from varats.utils.settings import bb_cfg, save_bb_config, vara_cfg
+
+if tp.TYPE_CHECKING:
+    from types import TracebackType
+
+    from varats.tools.research_tools.vara_manager import BuildType
 
 LOG = logging.getLogger(__name__)
 
 
 class ImageBase(Enum):
     """Container image bases that can be used by projects."""
+
     DEBIAN_10 = (Distro.DEBIAN, 10)
     DEBIAN_11 = (Distro.DEBIAN, 11)
     DEBIAN_12 = (Distro.DEBIAN, 12)
 
-    def __init__(self, distro: Distro, version_number: int):
+    def __init__(self, distro: Distro, version_number: int) -> None:
+        """Creates a base image object."""
         self.__distro = distro
         self.__version_number = version_number
 
@@ -58,6 +64,7 @@ class ImageBase(Enum):
 
 class ImageStage(Enum):
     """The stages that make up a base image."""
+
     STAGE_00_BASE = 00
     STAGE_10_VARATS = 10
     STAGE_20_TOOL = 20
@@ -65,7 +72,7 @@ class ImageStage(Enum):
     STAGE_31_CONFIG_DEV = 31
 
 
-class StageBuilder():
+class StageBuilder:
     """
     Context manager for creating a base image stage.
 
@@ -81,7 +88,10 @@ class StageBuilder():
     bb_root = Path("/app/")
     """BenchBuild root inside the container."""
 
-    def __init__(self, base: ImageBase, stage: ImageStage, image_name: str):
+    def __init__(
+        self, base: ImageBase, stage: ImageStage, image_name: str
+    ) -> None:
+        """Creates a StageBuilder context manager."""
         self.__base = base
         self.__stage = stage
         self.__layers = ContainerImage()
@@ -90,11 +100,16 @@ class StageBuilder():
         self.__tmp_dir = TemporaryDirectory()
 
     def __enter__(self) -> 'StageBuilder':
+        """Opens the stage builder for configuration."""
         return self
 
     def __exit__(
-        self, exc_type: tp.Any, exc_val: tp.Any, exc_tb: tp.Any
+        self,
+        exc_type: type[BaseException] | None,
+        exc_value: BaseException | None,
+        traceback: 'TracebackType | None',
     ) -> None:
+        """Builds the image from this stage builder."""
         bootstrap.bus()(CreateImage(self.image_name, self.layers))
 
         if self.__tmp_dir:
@@ -159,7 +174,7 @@ def _create_stage_00_base_layers(stage_builder: StageBuilder) -> None:
     _BASE_IMAGES[stage_builder.base](stage_builder)
     _setup_venv(stage_builder)
 
-    if (research_tool := _get_installable_research_tool()):
+    if research_tool := _get_installable_research_tool():
         research_tool.container_install_dependencies(stage_builder)
 
 
@@ -171,13 +186,13 @@ def _create_stage_10_varats_layers(stage_builder: StageBuilder) -> None:
 
 
 def _create_stage_20_tool_layers(stage_builder: StageBuilder) -> None:
-    if (research_tool := _get_installable_research_tool()):
+    if research_tool := _get_installable_research_tool():
         research_tool.container_install_tool(stage_builder)
 
 
 def _create_stage_30_config_layers(stage_builder: StageBuilder) -> None:
-    env: tp.Dict[str, tp.List[str]] = {}
-    if (research_tool := _get_installable_research_tool()):
+    env: dict[str, list[str]] = {}
+    if research_tool := _get_installable_research_tool():
         env = research_tool.container_tool_env(stage_builder)
 
     _add_vara_config(stage_builder)
@@ -191,7 +206,7 @@ def _create_stage_31_config_dev_layers(stage_builder: StageBuilder) -> None:
 
 
 def _create_layers_helper(
-    create_layers: tp.Callable[[StageBuilder], tp.Any]
+    create_layers: tp.Callable[[StageBuilder], tp.Any],
 ) -> tp.Callable[[StageBuilder], None]:
 
     def wrapped(stage_builder: StageBuilder) -> None:
@@ -201,7 +216,7 @@ def _create_layers_helper(
 
 
 # yapf: disable
-_BASE_IMAGES: tp.Dict[ImageBase, tp.Callable[[StageBuilder], None]] = {
+_BASE_IMAGES: dict[ImageBase, tp.Callable[[StageBuilder], None]] = {
     ImageBase.DEBIAN_10:
         _create_layers_helper(lambda ctx: ctx.layers
             .from_("docker.io/library/debian:10")
@@ -256,7 +271,7 @@ _BASE_IMAGES: tp.Dict[ImageBase, tp.Callable[[StageBuilder], None]] = {
                  'gcc', 'libgit2-dev', 'libffi-dev', 'libyaml-dev',
                  'graphviz-dev', 'python3', 'python3-pip', 'python3-virtualenv',
                  'libc++-dev', 'libc++abi-dev', 'libtbb12',
-                 'time', 'linux-perf')
+                 'time', 'linux-perf', 'zip')
             # install llvm 20
             .run('wget', 'https://apt.llvm.org/llvm.sh')
             .run('chmod', '+x', './llvm.sh')
@@ -275,7 +290,7 @@ _BASE_IMAGES: tp.Dict[ImageBase, tp.Callable[[StageBuilder], None]] = {
             .run('git', 'config', '--global', 'user.email', 'varats@container'))
 }
 
-_STAGE_LAYERS: tp.Dict[ImageStage,
+_STAGE_LAYERS: dict[ImageStage,
                        tp.Callable[[StageBuilder], None]] = {
     ImageStage.STAGE_00_BASE:       _create_stage_00_base_layers,
     ImageStage.STAGE_10_VARATS:     _create_stage_10_varats_layers,
@@ -284,13 +299,13 @@ _STAGE_LAYERS: tp.Dict[ImageStage,
     ImageStage.STAGE_31_CONFIG_DEV: _create_stage_31_config_dev_layers
 }
 
-_BASE_IMAGE_STAGES: tp.List[ImageStage] = [
+_BASE_IMAGE_STAGES: list[ImageStage] = [
     ImageStage.STAGE_00_BASE,
     ImageStage.STAGE_10_VARATS,
     ImageStage.STAGE_20_TOOL,
     ImageStage.STAGE_30_CONFIG
 ]
-_DEV_IMAGE_STAGES: tp.List[ImageStage] = [
+_DEV_IMAGE_STAGES: list[ImageStage] = [
     ImageStage.STAGE_00_BASE,
     ImageStage.STAGE_10_VARATS,
     ImageStage.STAGE_31_CONFIG_DEV,
@@ -299,20 +314,23 @@ _DEV_IMAGE_STAGES: tp.List[ImageStage] = [
 
 
 def _create_container_image(
-    base: ImageBase, stage: ImageStage, stages: tp.List[ImageStage],
-    image_name: tp.Callable[[ImageStage], str], force_rebuild: bool
+    base: ImageBase,
+    stage: ImageStage,
+    stages: list[ImageStage],
+    image_name: tp.Callable[[ImageStage], str],
+    force_rebuild: bool,
 ) -> str:
     """
-    Build/update a base image for the given image base and the current research
-    tool.
+    Build a base image for the given image base and the current research tool.
 
-    Only rebuild the given stage and subsequent stages.
+    Only rebuilds the given stage and subsequent stages.
 
     Args:
         base:  the image base
         stage: the image stage to create/update
         stages: a list of stages the complete image stack consists of
         image_name: a function that returns the image's name for a given stage
+        force_rebuild: forces a rebuild of the complete base image
     Returns:
         the name of the final container image
     """
@@ -338,8 +356,8 @@ def _create_container_image(
     return name
 
 
-def _get_installable_research_tool() -> tp.Optional[ContainerInstallable]:
-    if (configured_research_tool := vara_cfg()["container"]["research_tool"]):
+def _get_installable_research_tool() -> ContainerInstallable | None:
+    if configured_research_tool := vara_cfg()["container"]["research_tool"]:
         research_tool = get_research_tool(str(configured_research_tool))
         if isinstance(research_tool, ContainerInstallable):
             return research_tool
@@ -349,16 +367,17 @@ def _get_installable_research_tool() -> tp.Optional[ContainerInstallable]:
 def _unset_varats_source_mount(image_context: StageBuilder) -> None:
     mounts = bb_cfg()["container"]["mounts"].value
     mounts[:] = [
-        mount for mount in mounts
+        mount
+        for mount in mounts
         if mount[1] != str(image_context.varats_source_mount_target)
     ]
     save_bb_config()
 
 
 def _set_varats_source_mount(image_context: StageBuilder, mnt_src: str) -> None:
-    bb_cfg()["container"]["mounts"].value[:] += [[
-        mnt_src, str(image_context.varats_source_mount_target)
-    ]]
+    bb_cfg()["container"]["mounts"].value[:] += [
+        [mnt_src, str(image_context.varats_source_mount_target)]
+    ]
     save_bb_config()
 
 
@@ -427,32 +446,32 @@ def _add_vara_config(image_context: StageBuilder) -> None:
 
 
 def _add_benchbuild_config(
-    image_context: StageBuilder, env: tp.Dict[str, tp.List[str]]
+    image_context: StageBuilder, env: dict[str, list[str]]
 ) -> None:
-    bb_env: tp.Dict[str, tp.List[str]] = defaultdict(list)
+    bb_env: dict[str, list[str]] = defaultdict(list)
     for key, value in env.items():
         bb_env[key].extend(value)
 
     # copy libraries to image if LD_LIBRARY_PATH is set
-    if "LD_LIBRARY_PATH" in bb_cfg()["env"].value.keys():
+    if "LD_LIBRARY_PATH" in bb_cfg()["env"].value:
         image_context.layers.copy_(
             bb_cfg()["env"].value["LD_LIBRARY_PATH"],
-            str(image_context.varats_root / "libs")
+            str(image_context.varats_root / "libs"),
         )
-        bb_env["LD_LIBRARY_PATH"].extend([
-            str(image_context.varats_root / "libs")
-        ])
+        bb_env["LD_LIBRARY_PATH"].extend(
+            [str(image_context.varats_root / "libs")]
+        )
 
     # set BB config via env vars
     image_context.layers.env(
         BB_VARATS_OUTFILE=str(image_context.varats_root / "results"),
         BB_VARATS_RESULT=str(image_context.varats_root / "BC_files"),
         BB_JOBS=str(bb_cfg()["jobs"]),
-        BB_ENV=to_yaml(dict(bb_env))
+        BB_ENV=to_yaml(dict(bb_env)),
     )
 
 
-def create_dev_image(base: ImageBase, build_type: BuildType) -> str:
+def create_dev_image(base: ImageBase, build_type: 'BuildType') -> str:
     """
     Build a dev image for the given image base and research tool.
 
@@ -494,7 +513,7 @@ def create_base_image(
 def create_base_images(
     images: tp.Iterable[ImageBase] = ImageBase,
     stage: ImageStage = _BASE_IMAGE_STAGES[0],
-    force_rebuild: bool = False
+    force_rebuild: bool = False,
 ) -> None:
     """Builds all base images for the current research tool."""
     for base in images:
@@ -538,7 +557,7 @@ def get_image_name(
 
 
 def get_dev_image_name(
-    base: ImageBase, stage: ImageStage, build_type: BuildType
+    base: ImageBase, stage: ImageStage, build_type: 'BuildType'
 ) -> str:
     """
     Get the name for a dev-container image.
@@ -555,8 +574,10 @@ def get_dev_image_name(
 
 
 def _delete_container_image(
-    base: ImageBase, stage: ImageStage, stages: tp.List[ImageStage],
-    image_name: tp.Callable[[ImageStage], str]
+    base: ImageBase,
+    stage: ImageStage,
+    stages: list[ImageStage],
+    image_name: tp.Callable[[ImageStage], str],
 ) -> None:
     """
     Delete a base image.
@@ -580,7 +601,7 @@ def _delete_container_image(
 
 def delete_base_images(
     images: tp.Iterable[ImageBase] = ImageBase,
-    first_stage: ImageStage = _BASE_IMAGE_STAGES[0]
+    first_stage: ImageStage = _BASE_IMAGE_STAGES[0],
 ) -> None:
     """
     Deletes the selected base images.
@@ -601,9 +622,9 @@ def delete_base_images(
 
 
 def delete_dev_images(
-    build_type: BuildType,
+    build_type: 'BuildType',
     images: tp.Iterable[ImageBase] = ImageBase,
-    first_stage: ImageStage = _DEV_IMAGE_STAGES[0]
+    first_stage: ImageStage = _DEV_IMAGE_STAGES[0],
 ) -> None:
     """
     Deletes the selected dev images.
@@ -646,8 +667,11 @@ def export_base_images(images: tp.Iterable[ImageBase] = ImageBase) -> None:
 
 
 def run_container(
-    image_tag: str, container_name: str, build_dir: tp.Optional[str],
-    tmp_dir: tp.Optional[str], args: tp.Sequence[str]
+    image_tag: str,
+    container_name: str,
+    build_dir: str | None,
+    tmp_dir: str | None,
+    args: tp.Sequence[str],
 ) -> None:
     """
     Run a podman container.
